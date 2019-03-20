38416779cd634581518207ee9f77b33f6464fd0b
TRBC <therealbitcoin.club@protonmail.com>
2019-03-20 20:43:08 +0100

    jajaja

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,268 @@
-// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
-// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
-// to center the text within the [Scaffold] and the [FloatingActionButton] is
-// centered and docked within the [BottomAppBar] using
-// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
-// connected to a callback that increments a counter.
-
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+class AnimatedListSample extends StatefulWidget {
+  @override
+  _AnimatedListSampleState createState() => _AnimatedListSampleState();
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _AnimatedListSampleState extends State<AnimatedListSample> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  ListModel<Merchant> _list;
+  int _selectedItem;
+  int _nextItem; // The next item inserted when the user presses the '+' button.
+  final dio = new Dio(); // for http requests
+  List<Merchant> names = new List<Merchant>(); // names we get from API
+
+  void _getNames() async {
+    final response = await dio.get('https://realbitcoinclub.firebaseapp.com/places8.json');
+    ListModel<Merchant> tempList = ListModel<Merchant>(
+      listKey: _listKey,
+      removedItemBuilder: _buildRemovedItem,
+    );
+
+    //List<Merchant> tempList = new List<Merchant>();
+
+    for (int i = 0; i < response.data.length; i++) {
+      Merchant m2 = Merchant.fromJson(response.data[i]);
+      //tempList.add(m2);
+      tempList.insert(_list.length, m2);
+      print(response.data[i].toString());
+    }
+
+    setState(() {
+      _list = tempList;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _list = ListModel<Merchant>(
+      listKey: _listKey,
+     removedItemBuilder: _buildRemovedItem,
+    );
+    _nextItem = 3;
+    _getNames();
+  }
+
+  // Used to build list items that haven't been removed.
+  Widget _buildItem(
+      BuildContext context, int index, Animation<double> animation) {
+    return CardItem(
+      animation: animation,
+      item: _list[index],
+      selected: _selectedItem == _list[index],
+      onTap: () {
+        setState(() {
+          //_selectedItem = _selectedItem == _list[index] ? null : _list[index];
+        });
+      },
+    );
+  }
+
+  // Used to build an item after it has been removed from the list. This method is
+  // needed because a removed item remains  visible until its animation has
+  // completed (even though it's gone as far this ListModel is concerned).
+  // The widget will be used by the [AnimatedListState.removeItem] method's
+  // [AnimatedListRemovedItemBuilder] parameter.
+  Widget _buildRemovedItem(
+      Merchant item, BuildContext context, Animation<double> animation) {
+    return CardItem(
+      animation: animation,
+      item: item,
+      selected: false,
+      // No gesture detector here: we don't want removed items to be interactive.
+    );
+  }
+
+  // Insert the "next item" into the list model.
+  void _insert() {
+    //final int index =
+    //_selectedItem == null ? _list.length : _list.indexOf(_selectedItem);
+    //_list.insert(index, _nextItem++);
+  }
+
+  // Remove the selected item from the list model.
+  void _remove() {
+    if (_selectedItem != null) {
+      //_list.removeAt(_list.indexOf(_selectedItem));
+      setState(() {
+        _selectedItem = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Code Sample for material.Scaffold',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _count = 0;
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sample Code'),
-      ),
-      body: Center(
-        child: Image.network("https://realbitcoinclub.firebaseapp.com/img/app/pizzapirat.gif"),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AnimatedList'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_circle),
+              onPressed: _insert,
+              tooltip: 'insert a new item',
+            ),
+            IconButton(
+              icon: const Icon(Icons.remove_circle),
+              onPressed: _remove,
+              tooltip: 'remove the selected item',
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: AnimatedList(
+            key: _listKey,
+            initialItemCount: _list.length,
+            itemBuilder: _buildItem,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-              _count++;
-            }),
-        tooltip: 'Increment Counter',
-        child: Icon(Icons.restaurant),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
+}
+
+/// Keeps a Dart List in sync with an AnimatedList.
+///
+/// The [insert] and [removeAt] methods apply to both the internal list and the
+/// animated list that belongs to [listKey].
+///
+/// This class only exposes as much of the Dart List API as is needed by the
+/// sample app. More list methods are easily added, however methods that mutate the
+/// list must make the same changes to the animated list in terms of
+/// [AnimatedListState.insertItem] and [AnimatedList.removeItem].
+class ListModel<E> {
+  ListModel({
+    @required this.listKey,
+    @required this.removedItemBuilder,
+    Iterable<E> initialItems,
+  })  : assert(listKey != null),
+        assert(removedItemBuilder != null),
+        _items = List<E>.from(initialItems ?? <E>[]);
+
+  final GlobalKey<AnimatedListState> listKey;
+  final dynamic removedItemBuilder;
+  final List<E> _items;
+
+  AnimatedListState get _animatedList => listKey.currentState;
+
+  void insert(int index, E item) {
+    _items.insert(index, item);
+    _animatedList.insertItem(index);
+  }
+
+  E removeAt(int index) {
+    final E removedItem = _items.removeAt(index);
+    if (removedItem != null) {
+      _animatedList.removeItem(index,
+              (BuildContext context, Animation<double> animation) {
+            return removedItemBuilder(removedItem, context, animation);
+          });
+    }
+    return removedItem;
+  }
+
+  int get length => _items.length;
+
+  E operator [](int index) => _items[index];
+
+  int indexOf(E item) => _items.indexOf(item);
+}
+
+/// Displays its integer item as 'item N' on a Card whose color is based on
+/// the item's value. The text is displayed in bright green if selected is true.
+/// This widget's height is based on the animation parameter, it varies
+/// from 0 to 128 as the animation varies from 0.0 to 1.0.
+class CardItem extends StatelessWidget {
+  const CardItem(
+      {Key key,
+        @required this.animation,
+        this.onTap,
+        @required this.item,
+        this.selected: false})
+      : assert(animation != null),
+        assert(item != null),
+        assert(selected != null),
+        super(key: key);
+
+  final Animation<double> animation;
+  final VoidCallback onTap;
+  final Merchant item;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    //Merchant m2 = Merchant.fromJson(jsonDecode(item.toString()));
+    TextStyle textStyle = Theme.of(context).textTheme.display1;
+    if (selected)
+      textStyle = textStyle.copyWith(color: Colors.lightGreenAccent[400]);
+    return Padding(
+      padding: const EdgeInsets.all(1.0),
+      child: SizeTransition(
+        axis: Axis.vertical,
+        sizeFactor: animation,
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: onTap,
+          child: SizedBox(
+            height: 128.0,
+            child: Card(
+              color: Colors.primaries[11],
+              child: Center(
+                child: Text(item.name, style: textStyle),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//{"p":"trbc", "x":"41.406595", "y":"2.16655","n":"TRBC - The Real Bitcoin Club", "t":"99","c":"3","s":"5.0", "d":"3", "a":"0,1,2,34", "l":"Barcelona, Spain, Europe"}
+
+class Merchant {
+  String id;
+  String x;
+  String y;
+  String name;
+  String type;
+  String reviewCount;
+  String reviewStars;
+  String discount;
+  String tags;
+  String location;
+
+  Merchant(this.id, this.x, this.y, this.name, this.type, this.reviewCount, this.reviewStars, this.discount, this.tags, this.location);
+
+  // named constructor
+  Merchant.fromJson(Map<String, dynamic> json)
+      : id = json['p'],
+        x= json['x'],
+        y = json['y'],
+        name = json['n'],
+        type = json['t'],
+        reviewCount = json['c'],
+        reviewStars = json['s'],
+        discount = json['d'],
+        tags = json['a'],
+        location = json['l'];
+
+  // method
+  /*Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'age': age,
+    };
+  }*/
+
+}
+
+void main() {
+  runApp(AnimatedListSample());
 }
