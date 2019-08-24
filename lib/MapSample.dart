1507c0d4aa0c1c07ee737d3d32eee178c8dcd08d
TRBC <therealbitcoin.club@protonmail.com>
2019-08-24 15:42:09 +0200

    remove print

--- lib/MapSample.dart
+++ lib/MapSample.dart
@@ -143,7 +143,6 @@ class MapSampleState extends State<MapSample> {
   }
 
   InfoWindow buildInfoWindow(Merchant merchant) {
-    print(merchant.name);
     return InfoWindow(
         title: merchant.name +
             ": " +
