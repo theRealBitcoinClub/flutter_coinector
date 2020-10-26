8801618ddc413b05ea7b9a68849086c49c339a31
TRBC <therealbitcoin.club@protonmail.com>
2020-10-26 02:01:24 -0400

    activate web specific snackbar before release

--- lib/Snackbars.dart
+++ lib/Snackbars.dart
@@ -2,8 +2,12 @@ import 'package:Coinector/Dialogs.dart';
 import 'package:Coinector/InternetConnectivityChecker.dart';
 import 'package:Coinector/translator.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:platform_detect/platform_detect.dart';
+
+import 'UrlLauncher.dart';
 
 class Snackbars {
   static void _showSnackBar(
@@ -94,7 +98,6 @@ class Snackbars {
   }
 
   static void showSnackBarPlayStore(scaffoldKey, ctx) {
-/*
     //THIS METHOD IS ONLY FOR THE WEB APP, in case that someone opens the web app with a mobile phone
     if (!kIsWeb) return;
     String appstore = "Android";
@@ -123,6 +126,5 @@ class Snackbars {
       //UNSUPPORTED ERROR
       //debugPrint(e);
     }
- */
   }
 }
