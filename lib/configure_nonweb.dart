39129a2d339009056b3c1ed1297a2de87f85cd0d
TRBC <therealbitcoin.club@protonmail.com>
2020-11-14 17:09:20 -0400

    ugrade packages, prepare mobile release 3.9.8

--- lib/configure_nonweb.dart
+++ lib/configure_nonweb.dart
@@ -1,3 +1,4 @@
-void configureApp() {
+/*void configureApp() {
   // No-op.
 }
+*/
--- lib/configure_web.dart
+++ lib/configure_web.dart
@@ -1,5 +1,6 @@
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+/*import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void configureApp() {
   setUrlStrategy(PathUrlStrategy());
 }
+*/
