19895d7c054d5218472c164bc4536eff29e5f0b1
TRBC <therealbitcoin.club@protonmail.com>
2020-11-15 05:01:13 -0400

    update pubspec libs, add firebase and onesignal

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,6 +6,8 @@
 import 'dart:ui';
 
 import 'package:connectivity_for_web/connectivity_for_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -15,6 +17,8 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   ConnectivityPlugin.registerWith(registry.registrarFor(ConnectivityPlugin));
+  FirebaseAnalyticsWeb.registerWith(registry.registrarFor(FirebaseAnalyticsWeb));
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   FluttertoastWebPlugin.registerWith(registry.registrarFor(FluttertoastWebPlugin));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
