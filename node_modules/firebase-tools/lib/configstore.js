af89bf3a5055e004cdf385457a416cfd32336bbf
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2021-12-19 05:51:36 -0400

    REMOVE FIREBASE AND NODE MODULES

--- node_modules/firebase-tools/lib/configstore.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var Configstore = require("configstore");
-var pkg = require("../package.json");
-module.exports = new Configstore(pkg.name);
-//# sourceMappingURL=configstore.js.map
--- node_modules/firebase-tools/lib/firestore/indexes-spec.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=indexes-spec.js.map
