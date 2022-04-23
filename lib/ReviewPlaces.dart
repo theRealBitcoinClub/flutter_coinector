b153e1b38c1c1316d47dc06ddb2f46170aa0ff71
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-04-22 22:39:45 -0400

    asia, new zealand, hawai, korea, philip, indo, thailand, india

--- lib/ReviewPlaces.dart
+++ lib/ReviewPlaces.dart
@@ -1053,203 +1053,246 @@ class ReviewPlaces {
     },
     {
       'value': 1,
-      'label': "",
+      'label':
+          "Aspire Club, Soi Sukhumvit 23, Klongtoey Nue, Watthana, Bangkok, Thailand ",
     },
     {
       'value': 2,
-      'label': "",
+      'label':
+          "The Sport Corner Sukhumvit Rd., Klongtoey Khlong Toei, Thailand Bangkok 10110, Thailand ",
     },
     {
       'value': 3,
-      'label': "",
+      'label':
+          "Cute Corner Cuisine, Sukhumvit Soi 18, Khlong Toei, Bangkok, Thailand ",
     },
     {
       'value': 4,
-      'label': "",
+      'label':
+          "Cute Concept constant beauty, Sukhumvit Soi 18, Khlong Toei, Bangkok, Thailand ",
     },
     {
       'value': 5,
-      'label': "",
+      'label':
+          "Panini MAFIA, Soi Ekkamai 5, Klongton Nua, Watthana, Bangkok, Thailand ",
     },
     {
       'value': 6,
-      'label': "",
+      'label':
+          "Sole Mio Akkhara Phat Alley, Khlong Tan Nuea, Watthana, Bangkok, Thailand ",
     },
     {
       'value': 7,
-      'label': "",
+      'label':
+          "UnionSPACE, Sukhumvit 61 Alley, Khlong Tan Nuea, Watthana, Bangkok, Thailand ",
     },
     {
       'value': 8,
-      'label': "",
+      'label':
+          "LBD™ - La Bombona Diving™, MAIN CENTER, Ko Tao, Koh Tao Surat Thani, Thailand ",
     },
     {
       'value': 9,
-      'label': "",
+      'label':
+          "Drift by Big Blue, Ko Tao, Ko Pha-ngan District, Surat Thani, Thailand ",
     },
     {
       'value': 10,
-      'label': "",
+      'label':
+          "Buddha Cafe, Ko Pha-ngan Sub-district, Ko Pha-ngan District, Surat Thani, Thailand ",
     },
     {
       'value': 11,
-      'label': "",
+      'label':
+          "BEACHUB, Ko Pha-ngan Sub-district, Ko Pha-ngan District, Surat Thani 84280, Thailand ",
     },
     {
       'value': 12,
-      'label': "",
+      'label':
+          "Bliss Villas, Koh Phangan, Ko Pha-ngan District, Surat Thani, Thailand ",
     },
     {
       'value': 13,
-      'label': "",
+      'label':
+          "One Love Dome, Ko Pha-ngan Sub-district, Ko Pha-ngan District, Surat Thani 84280, Thailand ",
     },
     {
       'value': 14,
-      'label': "",
+      'label':
+          "Out back Bar & Restaurant, Ban Tai, Kohphangan, Surat Thani, Thailand ",
     },
     {
       'value': 15,
-      'label': "",
+      'label':
+          "500Rai Floating Resort, รัช ช ประภา Khao Phang, Ban Ta Khun District, Surat Thani, Thailand ",
     },
     {
       'value': 16,
-      'label': "",
+      'label':
+          "Drinks & Co Phuket, Choeng Thale, Thalang District, Phuket, Thailand ",
     },
     {
       'value': 17,
-      'label': "",
+      'label':
+          "Siam Supper Club, Lagoon Rd, Tambon Cherngtalay Thalang District, Phuket 83110, Thailand ",
     },
     {
       'value': 18,
-      'label': "",
+      'label':
+          "Harrys Restaurant, Bar & Hotel, Patong Beach, Phuket Thawewong Road, Pa Tong, Kathu District, Phuket, Thailand ",
     },
     {
       'value': 19,
-      'label': "",
+      'label':
+          "Santosa, Patak Road, Karon, Mueang Phuket District, Phuket, Thailand ",
     },
     {
       'value': 20,
-      'label': "",
+      'label':
+          "Kata On Sea Taina Road, Karon, Mueang Phuket District, Phuket, Thailand ",
     },
     {
       'value': 21,
-      'label': "",
+      'label':
+          "Flip Side, Wiset Road, Rawai, Mueang Phuket District, Phuket, Thailand ",
     },
     {
       'value': 22,
-      'label': "",
+      'label':
+          "AYAMCHON สาขา Central Festival Hatyai Kanjanavanich Road, Kho Hong, Hat Yai District, Songkhla, Thailand ",
     },
     {
       'value': 23,
-      'label': "",
+      'label':
+          "AYAMCHON Charoen Pradit Road, Rusamilae, Mueang Pattani District, Pattani, Thailand ",
     },
     {
       'value': 24,
-      'label': "",
+      'label': "AYAMCHON Talubo, Mueang Pattani District, Pattani, Thailand",
     },
     {
       'value': 25,
-      'label': "",
+      'label':
+          "AYAMCHON สาขา Central Festival Hatyai Kanjanavanich Road, Kho Hong, Hat Yai District, Songkhla, Thailand",
     },
     {
       'value': 26,
-      'label': "",
+      'label':
+          "AYAMCHON 42 Bang Nak, Mueang Narathiwat District, Narathiwat, Thailand",
     },
     {
       'value': 27,
-      'label': "",
+      'label':
+          "SAIL TECHNOLOGIES OU, Jalan Bukit Marak, Kota Bharu, Kelantan, Malaysia ",
     },
     {
       'value': 28,
-      'label': "",
+      'label':
+          "VIVO MALAYSIA I PHONE COMMUNICATIONS ENTERPRISE, Jalan Kuala Kangsar, Taman Tasek Damai, Ipoh, Perak, Malaysia ",
     },
     {
       'value': 29,
-      'label': "",
+      'label':
+          "HANGOUT @ IPOH, Jalan Sultan Idris Shah, Taman Jubilee, Ipoh, Perak, Malaysia ",
     },
     {
       'value': 30,
-      'label': "",
+      'label':
+          "Home Appliances - XAM MAX, Jalan SS 21/60, Damansara Utama, Petaling Jaya, Selangor, Malaysia ",
     },
     {
       'value': 31,
-      'label': "",
+      'label':
+          "Antara Hardware Home Centre Sdn Bhd 安泰五金, Taman Putra Sulaiman, Kuala Lumpur, Selangor, Malaysia ",
     },
     {
       'value': 32,
-      'label': "",
+      'label':
+          "Studio One Zero One / 101 Studios, Jalan SS 3/39, Ss 3, 47300 Petaling Jaya, Selangor, Malaysia ",
     },
     {
       'value': 33,
-      'label': "",
+      'label':
+          "Nk Freight (M) Sdn. Bhd., Jalan 9 Kosmoplek, Bandar Baru Salak Tinggi, Sepang, Selangor, Malaysia ",
     },
     {
       'value': 34,
-      'label': "",
+      'label':
+          "Saudagar Durian, Jalan Tambak Paya / Ayer Molek, Kampung Tambak Paya, 75460 Ayer Molek, Melaka, Malaysia ",
     },
     {
       'value': 35,
-      'label': "",
+      'label': "Hougang Street 11, El Gaming Pte. Ltd., Singapore ",
     },
     {
       'value': 36,
-      'label': "",
+      'label': "Cove Drive, Sip N Sail, Sentosa Cove, Singapore ",
     },
     {
       'value': 37,
-      'label': "",
+      'label': "Robinson Road, Ducatus Cafe Singapore, Singapore ",
     },
     {
       'value': 38,
-      'label': "",
+      'label': "Telok Kurau Road, Meat And Green, Singapore ",
     },
     {
       'value': 39,
-      'label': "",
+      'label':
+          "Raffles Boulevard, Black Knight Hotpot, Nihon Street, Singapore ",
     },
     {
       'value': 40,
-      'label': "",
+      'label': "Hotel, CÉ LA VI Singapore, Singapore ",
     },
     {
       'value': 41,
-      'label': "",
+      'label': "RAJEX.ID, Lampaseh Kota, Banda Aceh City, Aceh, Indonesia ",
     },
     {
       'value': 42,
-      'label': "",
+      'label':
+          "Mie Pangsit Bunda Juli Patok 6, Jalan Raya Bireuen - Takengon, Bireun Meunasah Capa, Bireuen Regency, Aceh, Indonesia",
     },
     {
       'value': 43,
-      'label': "",
+      'label':
+          "TOP RELOAD SUKSES, Jalan Pendowo, Bukit Batrem, Kota Dumai, Riau, Indonesia ",
     },
     {
       'value': 44,
-      'label': "",
+      'label':
+          "Tapung Hulu Motor, Suka Ramai, Kampar Regency, Riau, Indonesia ",
     },
     {
       'value': 45,
-      'label': "",
+      'label':
+          "PT.NUSANTARA BANGKA JAYA, Jl. RE. Martadinata, Opas Indah, Pangkal Pinang City, Bangka Belitung Islands, Indonesia ",
     },
     {
       'value': 46,
-      'label': "",
+      'label':
+          "Distro NgabreetRY, Link. Tegal malang, Warnasari, Kec. Jombang, Kota Cilegon, Banten 42411, Indonesia ",
     },
     {
       'value': 47,
-      'label': "",
+      'label':
+          "NEOSIS - Pembuatan Website dan Jasa Design, RT.9/RW.11, East Cengkareng, West Jakarta City, Jakarta, Indonesia",
     },
     {
       'value': 48,
-      'label': "",
+      'label':
+          "Bitcoins Crypto, Jl. Jambu II, Depok Jaya, Depok City, West Java, Indonesia ",
     },
     {
       'value': 49,
-      'label': "",
+      'label':
+          "Wisata Budidaya Jahe, Leuit Jahe, Pasir Lame Kubang, Cikadu, West Bandung Regency, West Java, Indonesia",
     },
     {
       'value': 50,
-      'label': "",
+      'label':
+          "BTC Fashion Mall, Jalan Doktor Djunjunan, Pajajaran, Bandung City, West Java, Indonesia ",
     }
   ];
 
@@ -1260,187 +1303,215 @@ class ReviewPlaces {
     },
     {
       'value': 1,
-      'label': "",
+      'label':
+          "Bandung Web Agency, Padasuka, Bandung City, West Java, Indonesia ",
     },
     {
       'value': 2,
-      'label': "",
+      'label':
+          "RS Gunung Jati, Jalan Kesambi, Kesambi, Kota Cirebon, Jawa Barat, Indonesia ",
     },
     {
       'value': 3,
-      'label': "",
+      'label':
+          "KRAVEFORMORE, Pradahkalikendal, Surabaya City, East Java, Indonesia ",
     },
     {
       'value': 4,
-      'label': "",
+      'label':
+          "iTech Ubud - Apple Repair and Service, Jalan Arjuna, Ubud, Gianyar, Gianyar Regency, Bali, Indonesia ",
     },
     {
       'value': 5,
-      'label': "",
+      'label':
+          "iTech Seminyak - Apple Repair and Service - Android Repair, Jalan Kunti I, Seminyak, Badung Regency, Bali, Indonesia ",
     },
     {
       'value': 6,
-      'label': "",
+      'label':
+          "M2M Catfish, Jalan Tanah Mas, Baamang Hulu, East Kotawaringin Regency, Central Kalimantan, Indonesia ",
     },
     {
       'value': 7,
-      'label': "",
+      'label':
+          "Rose Bakeshop, Roxas St, Barangay 4, San Francisco, Agusan del Sur, Philippines",
     },
     {
       'value': 8,
-      'label': "",
+      'label': "Arc's Pizza Hub Poblacion 3, Hamtic, Antique ",
     },
     {
       'value': 9,
-      'label': "",
+      'label':
+          "Jollibee Cadiz, Villena Street, Cadiz City, Negros Occidental, Philippines ",
     },
     {
       'value': 10,
-      'label': "",
+      'label':
+          "9 Starts Sari-Sari Store And Snack House, Naga, Camarines Sur, Philippines",
     },
     {
       'value': 11,
-      'label': "",
+      'label':
+          "Elly Jean Store, National Highway, Anilao, Iloilo, Philippines ",
     },
     {
       'value': 12,
-      'label': "",
+      'label': "Gemma Store and Internet Cafe, Zarraga, Iloilo, Philippines ",
     },
     {
       'value': 13,
-      'label': "",
+      'label':
+          "ARC's Pizza Hub, R Javier Street, Hamtic, Antique, Philippines ",
     },
     {
       'value': 14,
-      'label': "",
+      'label':
+          "ERRAVILLE CCTV, Blumentritt Street, Naga, Camarines Sur, Philippines ",
     },
     {
       'value': 15,
-      'label': "",
+      'label':
+          "Bestra Aircon & Refrigeration Trading, Blumentritt Street, Naga, Camarines Sur, Philippines ",
     },
     {
       'value': 16,
-      'label': "",
+      'label': "Parmar COOP, Pateros, Metro Manila, Philippines ",
     },
     {
       'value': 17,
-      'label': "",
+      'label':
+          "Odfjell Ship Management Phils. Inc., Malate, Manila, Metro Manila, Philippines ",
     },
     {
       'value': 18,
-      'label': "",
+      'label':
+          "Bitcoin.com, Benita Street, Gagalangin tondo, Manila, Metro Manila, Philippines ",
     },
     {
       'value': 19,
-      'label': "",
+      'label': "Lone rider club, Marilao, Bulacan, Philippines ",
     },
     {
       'value': 20,
-      'label': "",
+      'label': "台灣高雄市左營區重立路502巷8vermore Yoga & English ",
     },
     {
       'value': 21,
-      'label': "",
+      'label':
+          "Taiwan, Tainan City, Guiren District, Section 1st, Zhongshan Road, Wow waffles 美味鬆餅の專門店-歸仁店 ",
     },
     {
       'value': 22,
-      'label': "",
+      'label':
+          "Taiwan, Tainan City, South District, Lane 21, Section 2, Jinhua Road, Wow waffles 美味鬆餅の專門店 ",
     },
     {
       'value': 23,
-      'label': "",
+      'label': "台灣台南市中西區金華路四段雙品香酥豬排 ",
     },
     {
       'value': 24,
-      'label': "",
+      'label':
+          "Crispy Barbers 男仕理髪廳 No.237, Sec 2, Ximen Rd, West Central Dist, Tainan, Taiwan ",
     },
     {
       'value': 25,
-      'label': "",
+      'label':
+          "Taiwan, Taichung City, South District, Lane 146th, Xuefu Road, 恆日1989 ",
     },
     {
       'value': 26,
-      'label': "",
+      'label': "台灣台中市西區精誠路UPH氫呼吸體驗館 臺中店 ",
     },
     {
       'value': 27,
-      'label': "",
+      'label':
+          "魂猴設計工作室 SoMonkey Design Studio, 金湖街 Su'ao Township, Yilan County, Taiwan ",
     },
     {
       'value': 28,
-      'label': "",
+      'label': "台灣台北市大安區忠孝東路三段于賓诊所 ",
     },
     {
       'value': 29,
-      'label': "",
+      'label': "台灣台北市萬華區中華路理工法律事務所 ",
     },
     {
       'value': 30,
-      'label': "",
+      'label': "台灣新北市板橋區陽明街十三人 Home 13 Café ",
     },
     {
       'value': 31,
-      'label': "",
+      'label':
+          "Taiwan, Taoyuan City, Taoyuan District, Fuguo Road, 嫲嫲Mama（炸烤） ",
     },
     {
       'value': 32,
-      'label': "",
+      'label':
+          "Duciduci, Supsongmaeul 1-ro, Ilsandong-gu, Goyang-si, Gyeonggi-do, South Korea ",
     },
     {
       'value': 33,
-      'label': "",
+      'label': "Ray & Dors, Kolonia, Pohnpei, Federated States of Micronesia",
     },
     {
       'value': 34,
-      'label': "",
+      'label': "Kaireva Beach House - Rarotonga, Rarotonga, Cook Islands ",
     },
     {
       'value': 35,
-      'label': "",
+      'label': "Mystic Closet Boutiques, Pahoa Village Road, Pāhoa, HI, USA ",
     },
     {
       'value': 36,
-      'label': "",
+      'label':
+          "Perfect Harmony Boutique & Tea Room, Keawe Street, Hilo, HI, USA ",
     },
     {
       'value': 37,
-      'label': "",
+      'label': "The Mystic Closet, Kamehameha Avenue, Hilo, HI, USA ",
     },
     {
       'value': 38,
-      'label': "",
+      'label': "Hands Down Massage Hawaii, Opelo Road, Waimea, HI, USA ",
     },
     {
       'value': 39,
-      'label': "",
+      'label': "The Mystic Closet, Kamehameha Avenue, Hilo, HI, USA ",
     },
     {
       'value': 40,
-      'label': "",
+      'label':
+          "GasmanQt McMillan Road, Arthurs Point, Queenstown, New Zealand ",
     },
     {
       'value': 41,
-      'label': "",
+      'label':
+          "Nutrient Depot 31 Birmingham Dr, Middleton, Christchurch 8024, New Zealand ",
     },
     {
       'value': 42,
-      'label': "",
+      'label': "Kuske - Individual Eyewear Hardy Street, Nelson, New Zealand ",
     },
     {
       'value': 43,
-      'label': "",
+      'label':
+          "3Design NZ Aerodrome Road, Hanger 1, Mount Maunganui, New Zealand ",
     },
     {
       'value': 44,
-      'label': "",
+      'label': "Z Energy Dublin Street, Whanganui, New Zealand ",
     },
     {
       'value': 45,
-      'label': "",
+      'label':
+          "Ray White Papamoa Domain Road, Papamoa Beach, Papamoa, New Zealand ",
     },
     {
       'value': 46,
-      'label': "",
+      'label':
+          "Gull Henderson Henderson Valley Road, Henderson, Auckland, New Zealand ",
     },
     {
       'value': 47,
