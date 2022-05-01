ddc17b603d3c3d84fe59f362b78c9ba0fc4e0008
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-05-01 12:03:46 -0400

    4.20.52 target android 32

--- lib/CoinectorWidgetList.dart
+++ lib/CoinectorWidgetList.dart
@@ -39,7 +39,7 @@ import 'TagBrands.dart';
 import 'TagCoinector.dart';
 import 'UrlLauncher.dart';
 
-const bool isManagerModeRelease = true;
+const bool isManagerModeRelease = false;
 
 class CoinectorWidget extends StatefulWidget {
   final String search;
