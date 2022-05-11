268e031713273d61ab7fd8eded96158167023f7f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-11 16:02:09 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_11_E_1652299329001.dart
@@ -0,0 +1,5 @@
+class AutoSuggestionsE { static final reviewedTitlesE = const {"Traki El Recreo - Caracas, Capital District",
+"china food ccs - las acacias, Caracas, Capital District",
+"Rest. Gran Yen - Avenida Quito, Caracas, Capital District",
+};
+}
