6a61067f6671e6690785b343eb284e4ada7eb0e1
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-05-11 16:39:08 -0400

    rename Panmoni to BCHLatam, add more brands

--- lib/TagBrands.dart
+++ lib/TagBrands.dart
@@ -27,7 +27,7 @@ class TagBrand {
       _brands.add(TagBrand(1, "APAY", "Anypay"));
       _brands.add(TagBrand(2, "GOC", "GoCrypto"));
       _brands.add(TagBrand(3, "TOW", "BCH City Townsville"));
-      _brands.add(TagBrand(4, "PAN", "Panmoni"));
+      _brands.add(TagBrand(4, "BCHLAT", "BCHLatam"));
       _brands.add(TagBrand(5, "BNB", "Binance"));
       _brands.add(TagBrand(6, "SAL", "Salamantex"));
       _brands.add(TagBrand(7, "CBUY", "CryptoBuyer"));
@@ -40,6 +40,10 @@ class TagBrand {
       _brands.add(TagBrand(14, "SAT", "Satoshi Angels"));
       _brands.add(TagBrand(15, "BCHH", "BCH House Vzla"));
       _brands.add(TagBrand(16, "BCHO", "BCH Oriente Vzla"));
+      _brands.add(TagBrand(17, "GIF", "Ryan Giffin"));
+      _brands.add(TagBrand(18, "FAL", "Marc Falzon"));
+      _brands.add(TagBrand(19, "SUN", "BCH Sunny"));
+      _brands.add(TagBrand(20, "NICK", "Nick Chase"));
     }
     return _brands;
   }
