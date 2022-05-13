69bad634e6c8860d13435e46e0c3fd503e5926a1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-13 18:44:29 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_13_REVIEW_1652481867980.dart
@@ -0,0 +1,4 @@
+class AutoSuggestionsREVIEW { static final reviewedTitlesREVIEW = const {"Paparazzi Pizzeria & Bar - corner of Dockyard Drive, English Harbor, Antigua and Barbuda",
+"Creole Antigua Tours - Jolly Harbour, Antigua and Barbuda",
+};
+}
