027e33eefedc38c1c2f0bf24db25da9c8e2f4cc8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-17 15:53:32 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_17_AM_1652817203594.dart
@@ -0,0 +1,5 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Anais Nails - JX3G+37R, Catia La Mar 1162, Vargas, Venezuela",
+"Ekko Barber Shop - Av. Universidad, Centro Comercial La Castilla, C. Guillermo José Schael, Caracas 1011, Distrito Capital, Venezuela",
+"Zona Servi - H2WX+7QF, C. Jefatura a Cristo, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
