06a4dc2dc914410c4a09bb3b9df7353ed4d2b273
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-18 17:12:58 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_18_AM_1652908374161.dart
@@ -0,0 +1,4 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Big Fashion Ccs - Avenida Sur 15, C. Guillermo José Schael, Caracas 1011, Distrito Capital, Venezuela",
+"Big Fashion Ccs - Avenida Sur 15, C. Guillermo José Schael, Caracas 1011, Distrito Capital, Venezuela",
+};
+}
