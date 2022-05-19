2df79293bcbb3b2e5120153c96f0fdec34319889
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-19 15:40:42 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_19_AM_1652989241087.dart
@@ -0,0 +1,4 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Tecno7Repair - Bellas Artes, Caracas 1011, Distrito Capital, Venezuela",
+"Goodstyleccs - Edif.san Luis parque Carabobo, Caracas, G32W+X96, Caracas 1011, Distrito Capital, Venezuela",
+};
+}
