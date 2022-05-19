504e0c3f7d1b73a91964369259e6a91a9638e8ba
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-19 14:39:26 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_19_AM_1652985565002.dart
@@ -0,0 +1,5 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Inversiones Valentina Bisutery C.A - Av. Universidad, Centro Comercial La Castilla, C. Guillermo José Schael, Caracas 1011, Distrito Capital, Venezuela",
+"Inversiones Sport Family Full C.A - G45V+XX5, Avenida Mohedano, Caracas 1060, Miranda, Venezuela",
+"7 Estelas - JX3F+7VF, Avenida Tacagua, Catia La Mar 1162, La Guaira, Venezuela",
+};
+}
