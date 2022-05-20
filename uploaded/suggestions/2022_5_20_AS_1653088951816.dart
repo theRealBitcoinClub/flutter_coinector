67c5266c2a2cd8ff5fb1f9a7d4f5bd4e0818fbe1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 19:22:34 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653088951816.dart
@@ -0,0 +1,7 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Tecnocelkc Ml-622 - J9RJ+72W, Avenida Las Delicias, Maracaibo 4005, Zulia, Venezuela",
+"Racarga-T - J9RJ+FPW, Av. 13 B, Maracaibo 4005, Zulia, Venezuela",
+"Cerrajería Industrial Mcbo - J9RH+MV2, Maracaibo 4005, Zulia, Venezuela",
+"Inversiones Carvajal Olivo, C.A. - Av. La Costanera, Edf. Palmar Este, PB, Local 1, Caraballeda 1165, La Guaira, Venezuela",
+"Panadería Ejército de Dios,C.A. - Calle Real de Maiquetía, de Cristo a Puente, Casa s/n, Local 4, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
