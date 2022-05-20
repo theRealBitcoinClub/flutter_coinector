807ee84661b7fade46de35ce9a4bdb4fac511936
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 07:57:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AM_1653047862053.dart
@@ -0,0 +1,10 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Comercializadora Don Apolinar Campos - Guanare Sector Cementerio Carrera 8 con calle 20 C.C. City Center p/b local 4, Guanare 3300, Portuguesa, Venezuela",
+"TODOTOYOTA, C.A. - Carrera 14 entre Calles 10 y 11 , Sector, Guanare 3350, Portuguesa, Venezuela",
+"Inversiones Chop 2017 - JX3G+36Q, Av. La Armada, Catia La Mar 1162, La Guaira, Venezuela",
+"Servicell - JX2G+W5C, Av. La Armada, Catia La Mar 1162, La Guaira, Venezuela",
+"Repuestos Tu Preferido - Entre calle 20 y 21 Av. Simón Bolívar, edificio Ferretería Venezuela p/b local 01. Sector El, Cementerio, Guanare 3350, Portuguesa, Venezuela",
+"Charcutería Capuchino C.A - WJ7R+4Q8, C. Roscio, San Juan de Los Morros 2301, Guárico, Venezuela",
+"Totimarket - 15 Avenida Las Delicias, Maracaibo 4005, Zulia, Venezuela",
+"Un Lugar Diferente para Comprar - Av. Padilla C. 93, Maracaibo 4001, Zulia, Venezuela",
+};
+}
