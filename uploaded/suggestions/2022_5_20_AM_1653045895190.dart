858d21361c7b6cddd6867c57297c577d08e53028
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 07:24:57 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AM_1653045895190.dart
@@ -0,0 +1,4 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Calzados Emanuel - JX2G+W3V, Av. La Armada, Catia La Mar 1162, La Guaira, Venezuela",
+"Bungalow - Av. Los Leones &amp; Calle Caroni, Urb. Fundalara, Edif. Centro Empresarial Barquisimeto, Barquisimeto 3001, Lara, Venezuela",
+};
+}
