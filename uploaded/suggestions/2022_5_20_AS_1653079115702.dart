77149cf6f14d0c8e425dca82e7d414f32123549b
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 16:38:38 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653079115702.dart
@@ -0,0 +1,3 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Kaska-Nueces - carrera 22, esquina con pasaje acueducto, casa #21-96. local #3, San Cristóbal 5001, Táchira, Venezuela",
+};
+}
