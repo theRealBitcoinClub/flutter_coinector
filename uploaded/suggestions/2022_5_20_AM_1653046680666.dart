bcfd5b188ba26c87ad6a4e42a7c2284c0bb77a2b
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 07:38:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AM_1653046680666.dart
@@ -0,0 +1,4 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Comercializadora Don Apolinar Campos - Guanare Sector Cementerio Carrera 8 con calle 20 C.C. City Center p/b local 4, Guanare 3300, Portuguesa, Venezuela",
+"TODOTOYOTA, C.A. - Carrera 14 entre Calles 10 y 11 , Sector, Guanare 3350, Portuguesa, Venezuela",
+};
+}
