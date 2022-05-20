fe29a940c51f05c37833a7464c603aee8de7444d
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 14:40:01 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653071999154.dart
@@ -0,0 +1,10 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Club Deportivo Las Nazareth, C.A. - Av. El ejército, Asociación Aorafanb, Local 4C, Catia La Mar 1162, La Guaira, Venezuela",
+"Inversiones Jehová El Rey, C.A. - Calle Libertador, Local 26 y 27, Mercado Cacique Maiquetia, Pasillo 1, Maiquetía 1161, La Guaira, Venezuela",
+"Inversiones Yheremy, C.A. - Calle Libertador, Mercado Cacique Maiquetia, Local 81 Pasillo 1, Maiquetía 1161, La Guaira, Venezuela",
+"ADAM SPORT, C.A. - Calle Jefatura a Cristo, Pasaje el Cristo, Local N ° 15, Maiquetía 1161, La Guaira, Venezuela",
+"MiniMarket Bodegon la Roca, C.A. - Calle Los Baños, Frente a la Plaza Lourdes, Local 11, Maiquetía 1162, La Guaira, Venezuela",
+"Licorería Anare, C.A. - Local N° 40, Avenida Principal de, Anare 1166, La Guaira, Venezuela",
+"Inversiones Marioly Brett, c.a. - Av. El Ejército, Proveeduría de la Reserva Activa, Nivel PB, JX26+4MR, Local 5, Catia La Mar 1162, La Guaira, Venezuela",
+"Inversiones My friend Paul, C.A. - Calle Libertador, Local 165, Mercado Cacique Maiquetia, Pasillo 2, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
