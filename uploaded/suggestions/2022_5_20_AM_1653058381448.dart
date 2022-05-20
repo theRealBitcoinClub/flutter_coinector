6cd960b198b17274324cd83f1d5e282bf51a845a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 10:53:03 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AM_1653058381448.dart
@@ -0,0 +1,5 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Inversiones I Store - H2VX+VFJ, Av. Soublette, Maiquetía 1161, La Guaira, Venezuela",
+"M&L2021 - 1060 C. Sorocaima, Maiquetía 1161, La Guaira, Venezuela",
+"Litoral Mall - H2WV+QM4, Av. Soublette, Maiquetía 1060, La Guaira, Venezuela",
+};
+}
