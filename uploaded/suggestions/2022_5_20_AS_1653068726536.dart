33afdce0c28125c2587a3699598a9dee51edc457
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 13:45:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653068726536.dart
@@ -0,0 +1,5 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Tienda Linda 2011, F.P. - Calle Libertador, Mercado Cacique Maiquetía, Pasillo 5, Local 467 y 468, Maiquetía 1161, La Guaira, Venezuela",
+"Inversiones Henriday, C.A. - Calle Libertador, local 516, Mercado Cacique Maiquetia, Pasillo 5, Maiquetía 1161, La Guaira, Venezuela",
+"Yessenia Montilla, F.P. - Calle Libertador, Mercado Cacique Maiquetia, Local 21 Pasillo 1, Maiquetía 1162, La Guaira, Venezuela",
+};
+}
