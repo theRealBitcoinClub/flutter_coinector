b36071b1ac0215e67db0d7f3041956cde738dc87
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 13:32:15 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653067933168.dart
@@ -0,0 +1,3 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Tienda Linda 2011, F.P. - Calle Libertador, Mercado Cacique Maiquetía, Pasillo 5, Local 467 y 468, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
