c3cb9cd0217cf861a6a74a02e508311326efcbba
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 19:09:37 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653088174908.dart
@@ -0,0 +1,4 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Tecnocelkc Ml-622 - J9RJ+72W, Avenida Las Delicias, Maracaibo 4005, Zulia, Venezuela",
+"Racarga-T - J9RJ+FPW, Av. 13 B, Maracaibo 4005, Zulia, Venezuela",
+};
+}
