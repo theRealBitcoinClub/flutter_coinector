3e9465e6030ed637cc4cc74f9571472ea67b392c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 16:42:03 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653079321135.dart
@@ -0,0 +1,5 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Kaska-Nueces - carrera 22, esquina con pasaje acueducto, casa #21-96. local #3, San Cristóbal 5001, Táchira, Venezuela",
+"Recol Repuestos - 5001 Calle 4 nro 9-21 concordia subiendo 100 metros del estadio Tachira, San Cristóbal 5001, Táchira, Venezuela",
+"Recol Repuestos - 5001 Calle 4 nro 9-21 concordia subiendo 100 metros del estadio Tachira, San Cristóbal 5001, Táchira, Venezuela",
+};
+}
