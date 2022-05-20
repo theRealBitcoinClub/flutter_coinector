9b874c9d9486a90460894038975435836256e199
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 10:17:20 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AM_1653056237996.dart
@@ -0,0 +1,11 @@
+class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Totimarket - 15 Avenida Las Delicias, Maracaibo 4005, Zulia, Venezuela",
+"JEOTCELL - A.V 15 Las Delicias, Con Calle 100, Maracaibo 4005, Zulia, Venezuela",
+"ALCANPO Cell Sucursal #2 - Av. 15 Las Delicias, Con Calle 100, Maracaibo 4005, Zulia, Venezuela",
+"Agrotienda - 5M5J+32J, Avenida, El Cují 3001, Lara, Venezuela",
+"DC Grand Bazaar - Av 15 Delicias, Calle 100, Av. Libertador, Maracaibo 4005, Zulia, Venezuela",
+"Optica Visión - Av 15 Las Delicias, Con Calle 100, Maracaibo 4005, Zulia, Venezuela",
+"DC Grand Bazaar - Av 15 Delicias, Calle 100, Av. Libertador, Maracaibo 4005, Zulia, Venezuela",
+"Comercializadora & Distribuidora Fabrimuebles Remolina C.a - Av José Antonio Paez, C.C Multiplaza El Paraíso, Nivel 6, Local C-12 y C-13, Urb, Zona Postal 1020, Caracas 1020, Distrito Capital, Venezuela",
+"Global Cell System - Av 15 Las Delicias, Con Calle 100, Maracaibo 4005, Zulia, Venezuela",
+};
+}
