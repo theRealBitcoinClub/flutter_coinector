f663a07d07c7ec21450a126d11a8efbd97afb1d0
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 16:10:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653077400179.dart
@@ -0,0 +1,5 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Mundo Repuestos Panda - MQWV+W5P, Guanarito 3354, Portuguesa, Venezuela",
+"Kau Kids - AV. UNDA ENTRE CARRERAS 6 Y 7, Centro Comercial LOS ANDENES, 3350, Av. Unda, Guanare 3350, Portuguesa, Venezuela",
+"Bubbles fresh - la guairita, local 67 Avenida Luis De Camoens, Caracas 1073, Miranda, Venezuela",
+};
+}
