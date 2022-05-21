cdf7989d2d82c01cc60406368f5be47438624315
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-20 20:01:57 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_20_AS_1653091315059.dart
@@ -0,0 +1,4 @@
+class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Barbería Unisex Nautilus II - Local N° 04, con Cristo a Puente, C. Real de Maiquetía, Maiquetía 1161, La Guaira, Venezuela",
+"Restaurant Cabo Kenedy, C.A - Av. Ppal. de Caribe C.C. Houston PB, Local 01, Caraballeda 1165, La Guaira, Venezuela",
+};
+}
