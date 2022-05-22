be8286c436ca1f32ac469d5f754b8cd7f33f6580
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-05-21 20:23:18 -0400

    remove duplicates in set

--- uploaded/suggestions/2022_5_19_AM_1652983912537.dart
+++ uploaded/suggestions/2022_5_19_AM_1652983912537.dart
@@ -1,7 +1,8 @@
-class AutoSuggestionsAM { static final reviewedTitlesAM = const {"Freddytattoo - JX2G+W45, Av. La Armada, Catia La Mar 1162, La Guaira, Venezuela",
-"Freddytattoo - JX2G+W45, Av. La Armada, Catia La Mar 1162, La Guaira, Venezuela",
-"Anais Nails - JX3G+37R, Catia La Mar 1162, Vargas, Venezuela",
-"Zona Servi - H2WX+7QF, C. Jefatura a Cristo, Maiquetía 1161, La Guaira, Venezuela",
-"Multiservicios Germany C.A. - Calle 14 De Febrero. Entre Calle Sol de Madrid &amp; Calle el Centro, Casa 12-A, Piso 2, Caracas 1030, Distrito Capital, Venezuela",
-};
+class AutoSuggestionsAM {
+  static final reviewedTitlesAM = const {
+    "Freddytattoo - JX2G+W45, Av. La Armada, Catia La Mar 1162, La Guaira, Venezuela",
+    "Anais Nails - JX3G+37R, Catia La Mar 1162, Vargas, Venezuela",
+    "Zona Servi - H2WX+7QF, C. Jefatura a Cristo, Maiquetía 1161, La Guaira, Venezuela",
+    "Multiservicios Germany C.A. - Calle 14 De Febrero. Entre Calle Sol de Madrid &amp; Calle el Centro, Casa 12-A, Piso 2, Caracas 1030, Distrito Capital, Venezuela",
+  };
 }
--- uploaded/suggestions/2022_5_20_AS_1653079491052.dart
+++ uploaded/suggestions/2022_5_20_AS_1653079491052.dart
@@ -1,6 +1,7 @@
-class AutoSuggestionsAS { static final reviewedTitlesAS = const {"Kaska-Nueces - carrera 22, esquina con pasaje acueducto, casa #21-96. local #3, San Cristóbal 5001, Táchira, Venezuela",
-"Recol Repuestos - 5001 Calle 4 nro 9-21 concordia subiendo 100 metros del estadio Tachira, San Cristóbal 5001, Táchira, Venezuela",
-"Recol Repuestos - 5001 Calle 4 nro 9-21 concordia subiendo 100 metros del estadio Tachira, San Cristóbal 5001, Táchira, Venezuela",
-"Pa' Que Leo - J532+33W, Caraballeda 1165, Vargas, Venezuela",
-};
+class AutoSuggestionsAS {
+  static final reviewedTitlesAS = const {
+    "Kaska-Nueces - carrera 22, esquina con pasaje acueducto, casa #21-96. local #3, San Cristóbal 5001, Táchira, Venezuela",
+    "Recol Repuestos - 5001 Calle 4 nro 9-21 concordia subiendo 100 metros del estadio Tachira, San Cristóbal 5001, Táchira, Venezuela",
+    "Pa' Que Leo - J532+33W, Caraballeda 1165, Vargas, Venezuela",
+  };
 }
