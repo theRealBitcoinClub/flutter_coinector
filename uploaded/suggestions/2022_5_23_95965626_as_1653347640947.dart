65c0b366bf60899e30accb1debca97dd2b802438
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-23 19:14:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_23_95965626_as_1653347640947.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions95965626 { static final reviewedTitles95965626 = const {"Saint-miguelacho- shoes zapateria deportiva - local 894, Avenida Bolivar, Mercado Bolivariano la Hoyada, pasillo Miranda con Ecuador, C. Los Ángeles, Caracas 1064, Distrito Capital, Venezuela",
+"Variedades JM14 - Centro Comercial Doral Barald av barald Local 279 PB Sector, Av. Baralt, Caracas 1014, Distrito Capital, Venezuela",
+"Miguelacho Shoes - G32R+R75, Caracas 1011, Capital District, Venezuela",
+"Pizza San Jose C.A. - Av. Bulevar Naiguatá, Centro Comercial Caribe, PB, Local N° 5, Urb, Caraballeda 1165, La Guaira, Venezuela",
+"Zapatos Miguelacho - Local 106, Avenida Bolívar, Mercado Bolivariano la Hoyada, Pasillo Miranda, Caracas 1011, Caracas 1011, Distrito Capital, Venezuela",
+"Nuva telo - Av. Baralt con esquina pedrera, C.C. Metrocenter, nivel Avenida, pasillo Medio, Caracas 1071, Av. Baralt, Caracas 1012, Miranda, Venezuela",
+"Zona Servi - H2WX+7QF, C. Jefatura a Cristo, Maiquetía 1161, La Guaira, Venezuela",
+"Tsunami Surfshop - H2WX+5X6, Maiquetía 1161, Vargas, Venezuela",
+};
+}
