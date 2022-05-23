0502c58ca09fa2c0913d63ce23391a6d01c65eaa
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-23 18:22:00 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_23_22186500_as_1653344517286.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions22186500 { static final reviewedTitles22186500 = const {"Iron Tech - J9RJ+P3M, Maracaibo 4005, Zulia, Venezuela",
+"Panaderia y Pastelería Tina C.A - Av. San Francisco &amp;, local 81 Avenida Francisco de Miranda, Caracas 1071, Miranda, Venezuela",
+};
+}
