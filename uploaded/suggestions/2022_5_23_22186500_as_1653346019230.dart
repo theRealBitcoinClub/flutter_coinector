ad316cfa375a6c7c84b6ffc87f1a71b2f80520f8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-23 18:47:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_23_22186500_as_1653346019230.dart
@@ -0,0 +1,12 @@
+class AutoSuggestions22186500 { static final reviewedTitles22186500 = const {"Iron Tech - J9RJ+P3M, Maracaibo 4005, Zulia, Venezuela",
+"Panaderia y Pastelería Tina C.A - Av. San Francisco &amp;, local 81 Avenida Francisco de Miranda, Caracas 1071, Miranda, Venezuela",
+"ALCANPO Mobile Store - Local 566, Urb.15 Delicias, C.C. Gran Bazar, Avenida Las Delicias, Maracaibo 4005, Zulia, Venezuela",
+"TELYCENT C.A. - 4005, Maracaibo 4005, Maracaibo 4001, Zulia, Venezuela",
+"Sensación Bellaroma - CC Grand Bazar, Av las Delicias frente al CC Cima Baracaibo, Maracaibo 4005, Maracaibo 4005, Zulia, Venezuela",
+"Inversiones Wapar - Mercado de Quinta Crespo, Local 100, Av. Baralt, Caracas 1014, Distrito Capital, Venezuela",
+"Diadema Group - Km 16, Carretera Panamericana. Centro Comercial Club de Campo, Nivel PB, Stand 02, Carr. Panamericana, San Antonio de Los Altos 1204, Miranda, Venezuela",
+"Kristy Cafe - F4XC+9XH, Caracas 1050, Capital District, Venezuela",
+"Ron&Jhon zapateria - Avenida Bolivar, Mercado Bolivariano la Hoyada, pasillo la Orchila, local 2887, Caracas, Caracas, Caracas 1040, Distrito Capital, Venezuela",
+"Peluquería Para Caballeros Ruzafa - C. Rbla. Baja, 13, 30800 Lorca, Murcia, Spain",
+};
+}
