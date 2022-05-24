5e479358449e6d9f494ee6fd5aee649a32bcab2d
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 18:40:06 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_8411019_as_1653432003187.dart
@@ -0,0 +1,13 @@
+class AutoSuggestions8411019 { static final reviewedTitles8411019 = const {"Inv Gabyjuan C.A - Av francisco solano edf pasaje concordia Planta baja local 3v, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiónes perlas perladas, c.a - Esq madrices a ibarras local c edf bergatin local pb Caracas, G34P+MWG, Caracas 1010, Distrito Capital, Venezuela",
+"Mundo Chocolate Reposteria - Esquina La Marrón, a una cuadra de la Av Urdaneta CC Doral Nivel Mezzanina M5 URB. Catedral Caracas, Caracas 1010, Distrito Capital, Venezuela",
+"Piñateria Mundo Chocolate 1260, C.A - Esquina La Marrón, Una cuadra antes de la Av. Urdaneta CC Doral Nivel Mezzanina Local M8- M9 URB, Caracas 1010, Distrito Capital, Venezuela",
+"Bodega Variedades Rchell y Samantha - Av Baralt CC Doral baralt Nivel Mezzanine Local 91 sector, Caracas 1010, Distrito Capital, Venezuela",
+"Variedades Rchell y Samantha Sucursal - Av Doralt Baralt entre las esquinas bucare A Carmen CC Doral baralt Nivel Mezzanine Local 87 urb, Caracas, Caracas 1014, Distrito Capital, Venezuela",
+"Bodega Variedades Rchell y Samantha - Av Baralt CC Doral baralt Nivel Mezzanine Local 91 sector, Caracas 1010, Distrito Capital, Venezuela",
+"Expoimpotmarhenzar2015 - F5HV+57Q, Av. Principal Las Vegas de Petare, Caracas 1073, Miranda, Venezuela",
+"Variedadesneomar - F5HV+87H, Av. Principal Las Vegas de Petare, Caracas 1073, Miranda, Venezuela",
+"Inversiones Andrews 2021, C.A - Av. Baralt, Locales 146 al 149, Cc Doral Baralt Esquina del Carmen a Buscarte, Caracas 1010, Av. Baralt, Caracas 1014, Distrito Capital, Venezuela",
+"INVERSIONES AURIGA, C.A. - F5HR+3V3 zona colonial de, C. La Paz, Caracas 1073, Miranda, Venezuela",
+};
+}
