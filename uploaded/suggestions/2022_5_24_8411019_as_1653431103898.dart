6ea524aea28cdfc22308b9f0b777fc96207e595e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 18:25:07 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_8411019_as_1653431103898.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions8411019 { static final reviewedTitles8411019 = const {"Inv Gabyjuan C.A - Av francisco solano edf pasaje concordia Planta baja local 3v, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiónes perlas perladas, c.a - Esq madrices a ibarras local c edf bergatin local pb Caracas, G34P+MWG, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
