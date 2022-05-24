82cd797abd813bd04dc45069f319470fae7ed6da
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 12:51:47 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_87508588_as_1653411103925.dart
@@ -0,0 +1,11 @@
+class AutoSuggestions87508588 { static final reviewedTitles87508588 = const {"Inversiones Las Julianas, C.A. - Calle Real de La Cañada, Bloque 15, Frente al, PB Local 13-04, Caracas 1030, Distrito Capital, Venezuela",
+"Kiosko EL OSO - HWVV+4WM, Avenida La Armada, Catia La Mar 1162, Vargas, Venezuela",
+"SagresMarket - 6 C. Negrín, Caracas 1050, Distrito Capital, Venezuela",
+"Comercial Barcelona,C.A - Calle Libertador, entre Calle Mare y Jefatura, Casa N° 1, PB, Frente al, Mercado Cacique Maiquetia, Maiquetía 1161, La Guaira, Venezuela",
+"Cocos y Cocadas El Trono de Odín - Av. san Martín con calle Mercado Municipal Oeste San Martín Local, 604-605 Sector, Caracas 1020, Distrito Capital, Venezuela",
+"Inversiones Gordo Pablo 2 C.A. - Centro comercial San Martin, locales 406-407. Caracas, Caracas, Caracas 1020, Distrito Capital, Venezuela",
+"Inversiones El Gordo Pablo 1. - Avenida principal, centro comercial San Martin, zona 22, local 513-514-515. Caracas 1020, Principal, Caracas 1020, Caracas 1020, Distrito Capital, Venezuela",
+"Viveres Pernia's 5-J - Av. san Martín edificio la araña_ planta baja local 36 37 y 38 urbanización, Caracas 1020, Distrito Capital, Venezuela",
+"Tu visión némesis C.A. - Avenida este, esquina Dr. Paul a Salvador de león, edificio 71 N.pb. L-11 La hoyada Caracas, Avenida E, Caracas 1040, Caracas 1012, Distrito Capital, Venezuela",
+};
+}
