acab83cf5651dd3a0e154c4a2fe74bb2c2b84b92
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 00:56:21 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_95224523_as_1653368178375.dart
@@ -0,0 +1,17 @@
+class AutoSuggestions95224523 { static final reviewedTitles95224523 = const {"Yarman fashion new store - G32R+Q8V, Caracas 1011, Capital District, Venezuela",
+"Vida's Bike comercializadora c.a - Av Sur 4 esquina de dolores a Mamey Local nro 170, sector, Caracas 1040, Distrito Capital, Venezuela",
+"INVERSIONES AEGON, C.A. - Esquina conde a carmelitas av sur 4 Cc minitiendas expomeca nivel pb local 57, Capitolio, Caracas 1010, Distrito Capital, Venezuela",
+"Tu ciervo Shoes Sport - Mercado Bolivariano La Hoyada, Pasillo Zulia, Local 208, Caracas 1011, Caracas 1011, Distrito Capital, Venezuela",
+"Variedades Mauped C.A - Los Chorritos, Av Principal, Local Nro 1, Principal, Tocuyito 2035, Carabobo, Venezuela",
+"Oxígenos cabriales C.A - Av. 74, calle 106, Centro Comercial Infra, PB, Local 10 Av. Don Julio Centeno, San Diego 2006, Carabobo, Venezuela",
+"Delicateses la Gloria de Dios - 4W8M+8QM, Tocuyito 2035, Carabobo, Venezuela",
+"Audicenter C.A. - Avenida universidad, centro comercial Metrocenter, local mp33. Caracas, Avenida Universidad, Caracas 1012, Distrito Capital, Venezuela",
+"Leicacel Service LS, C.A. - Esquina el conde, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones Biyetmar shoes - Mercado Bolivariano La Hoyada, Pasillo Portuguesa, Local 2345, Caracas 1011, Caracas 1011, Distrito Capital, Venezuela",
+"Distribuidora Najucar, C.A - Av. sur 4 esquinas Carmelitas a Conde CC expomeca 2000 Nivel Pasillo 1 Local 7 Urb, Av. Urdaneta, Caracas 1012, Distrito Capital, Venezuela",
+"Inversiones DGlass ropa interior - Av Bolívar, Mercado Bolivariano La Hoyada, Pasillo Falcón, Local 1566, Caracas 1011, Caracas 1011, Distrito Capital, Venezuela",
+"Representaciones Glorifer C.A. - Esa. Conde a Carmelitas, C.C. Expomeca 2000 Nivel PB, Local 77. Urb, Caracas 1020, Distrito Capital, Venezuela",
+"Los Sabores de Barbarita - Av San Martin, Mercado Municipal de San Martin Pasillo 24, Local 304 al lado de las, cocadas, Caracas 1020, Distrito Capital, Venezuela",
+"María Representaciones ropa interior 3000 C.A - Avenida Bolivar, Mercado Bolivariano la Hoyada, pasillo Falcón con ecuador, local 1566, Caracas, Caracas, Caracas 1011, Distrito Capital, Venezuela",
+};
+}
