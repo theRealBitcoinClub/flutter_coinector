3eb6f5228d9d177cd7314fb7363d776395bf9d3a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-23 20:22:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_23_38925541_as_1653351745468.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions38925541 { static final reviewedTitles38925541 = const {"CENTRO DIAGNOSTICO NATURISTA M. HILARION - 27W2+GHR, Guanare 3350, Portuguesa, Venezuela",
+"Representaciones Herdanys Fashion - Av. San Martin cc mercado san martin Zona 6 pasillo 10 y 11 Nivel PB local 317 y 341 Caracas, Caracas 1020, Distrito Capital, Venezuela",
+"Inversiónes Centrotienda, C.A. - Av. Sucre, entre Calle Principal de Altavista y Conexión con Autopista Caracas-La Guaira, Edif. La Fundación, PB Local 5, Caracas 1030, Distrito Capital, Venezuela",
+};
+}
