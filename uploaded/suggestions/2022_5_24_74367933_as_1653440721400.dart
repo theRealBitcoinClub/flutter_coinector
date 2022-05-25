98b15e5e6ebf40ec50ebcb94b2bba15d05e5f879
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 21:05:25 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_74367933_as_1653440721400.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions74367933 { static final reviewedTitles74367933 = const {"Peluquería MUNDO INFANTIL la florida - G42G+M4C, Avenida Juan Bautista Arismendi, Caracas 1050, Distrito Capital, Venezuela",
+"Carniceria y charcuteria LA ESPERANZA C.A - Esquinas de Conde a Piñango, al lado Prefectura de Catedral, Caracas 1012, Caracas 1012, Caracas 1012, Distrito Capital, Venezuela",
+};
+}
