bdbdeaa6b26b5f7ed24be89d7393cb61af2f7365
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 21:18:23 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_69739649_as_1653441499442.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions69739649 { static final reviewedTitles69739649 = const {"Continental Store - Esquina bolsa a Padre Sierra, edificio Alto, Continental, Caracas 1010, Distrito Capital, Venezuela",
+"GRUPO BAZAR MARKET, C.A. - Residencia La Hacienda, Avenida Veracruz, Nivel PB, Local 3, Caracas 1061, Miranda, Venezuela",
+"Kiosco El Universal - Av Principal del Bosque, Kiosco 5, con Blvr. de Sabana Grande, Caracas 1060, Miranda, Venezuela",
+};
+}
