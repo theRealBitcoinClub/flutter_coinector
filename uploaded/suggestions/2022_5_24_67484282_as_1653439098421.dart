bfc9ad6c5bb7e43bae877f2bee72343868374f83
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 20:38:22 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_67484282_as_1653439098421.dart
@@ -0,0 +1,12 @@
+class AutoSuggestions67484282 { static final reviewedTitles67484282 = const {"Melosa Café - Avenida Norte Av urdaneta de esquina de veroes a jesuitas Inicio boulevard Panteón Edificio San José Al lado de la Casa José Marti Caracas, Caracas 1010, Distrito Capital, Venezuela",
+"@Amarte2902 - F5HV+57X, Av. Principal Las Vegas de Petare, Caracas 1073, Miranda, Venezuela",
+"Arj soluciones - Centro comercial City Market, piso 2, local B334, Caracas, Caracas 1050, Distrito Capital, Venezuela",
+"Grupo tuservinet C.A. - Centro parque Carabobo, local L10;L11. Caracas, Avenida Universidad, Caracas 1011, Distrito Capital, Venezuela",
+"Kiosco El Rosal - Av. Tamanaco, Kiosco 13, Con Calle Mohedano, Caracas 1060, Miranda, Venezuela",
+"Radio Bar Ccs - Planinco, Av. Libertador, Caracas 1040, Distrito Capital, Venezuela",
+"La Barra de Chacao - Calle El Muñeco Con esquina, Avenida Francisco de Miranda, Caracas 1060, Miranda, Venezuela",
+"Electronic store compa y Stiven Vallejo - local 433, Avenida Abraham Lincoln, centro comercial City Market, nivel mezzanina, Caracas 1050, Distrito Capital, Venezuela",
+"Chinatown C.A. - Esquina El Conde, edificio Hotel El Conde, PB, locales 4 y 5, Caracas 1012, Distrito Capital, Venezuela",
+"ÁREA DE CAMPING - Centro Polo, Av. Caroní, Caracas 1080, Miranda, Venezuela",
+};
+}
