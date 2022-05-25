69477cc1036d834e8720fe48610957e9b07fe052
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-24 20:20:54 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_24_67484282_as_1653438050650.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions67484282 { static final reviewedTitles67484282 = const {"Melosa Café - Avenida Norte Av urdaneta de esquina de veroes a jesuitas Inicio boulevard Panteón Edificio San José Al lado de la Casa José Marti Caracas, Caracas 1010, Distrito Capital, Venezuela",
+"@Amarte2902 - F5HV+57X, Av. Principal Las Vegas de Petare, Caracas 1073, Miranda, Venezuela",
+"Arj soluciones - Centro comercial City Market, piso 2, local B334, Caracas, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
