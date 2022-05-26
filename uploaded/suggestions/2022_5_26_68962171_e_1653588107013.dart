c6af9c408bfa88e1332186b4d451a4d7baf7cb30
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 14:01:51 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_68962171_e_1653588107013.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions68962171 { static final reviewedTitles68962171 = const {"Bodega la 30 - Carrera 30 &amp; Calle 26 y 25, Barquisimeto 3001, Lara, Venezuela",
+"Centro de Economia Comunal Manuelita Saenz - C.C Manuelita Sáenz Nv. 4, Local #123, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
