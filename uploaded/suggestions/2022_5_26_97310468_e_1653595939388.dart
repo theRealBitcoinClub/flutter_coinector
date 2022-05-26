5c4dff553c14d592cd2291f8973c7d589cd25f11
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 16:12:24 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_97310468_e_1653595939388.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions97310468 { static final reviewedTitles97310468 = const {"11.11 Boutique - G46V+MR8, Avenida Transversal 10, Caracas 1060, Distrito Capital, Venezuela",
+"Pintacar Express - JX49+V7F, Calle Tacagua, Catia La Mar 1162, La Guaira, Venezuela",
+"Inversiones Oshun, C.A. - Calle libertador, Local 182, Mercado Cacique Maiquetia, Pasillo 3, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
