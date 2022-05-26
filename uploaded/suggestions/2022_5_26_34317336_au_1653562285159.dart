8cf636bf1988e3ccab894720aa7b57c014e0cff0
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 06:51:29 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_34317336_au_1653562285159.dart
@@ -0,0 +1,9 @@
+class AutoSuggestions34317336 { static final reviewedTitles34317336 = const {"Centro de Copiado Belen - JX3G+FF6, Catia La Mar 1162, Vargas, Venezuela",
+"MotoRespuestos El Peludo - Av José Antonio Paez, Resd Atlas, Local 02, Mun, Av. José Antonio Páez, Oeste 1021, Distrito Capital, Venezuela",
+"NIVEL 1 LOCAL 33 - G33P+HW9, Avenida Universidad, Caracas 1012, Distrito Capital, Venezuela",
+"Octopus Tattoo - F3V8+555, Caracas 1020, Capital District, Venezuela",
+"Blu-Ray Caracas - F4VV+R4M, Calle Páez, Caracas 1060, Distrito Capital, Venezuela",
+"Blu-Ray Caracas - F4VV+R4M, Calle Páez, Caracas 1060, Distrito Capital, Venezuela",
+"Time Game - Centro de la Economía Popular Manuela Sáenz, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
