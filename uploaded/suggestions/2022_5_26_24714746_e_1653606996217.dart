662ab9d4022e42f12db1b3212c6cdf462f39974e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 19:16:40 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_24714746_e_1653606996217.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions24714746 { static final reviewedTitles24714746 = const {"Mariangel Studio - Carrera 4 calle 2 y 3 frente a la Plaza Bolívar Tariba Centro comercial Los Daniel&#39;s local N- 11 P1, Táriba 5017, Táchira, Venezuela",
+"Haru Nails Studio - Carrera 4 calle 2 y 3 frente a la Plaza Bolívar Tariba Centro comercial Los Daniel&#39;s local N-13, Troncal 1, Táriba 5017, Táchira, Venezuela",
+"La Brachera In C.A. - CALLE PIAR, LOCAL 03 C. Montilla, Guacara 2015, Carabobo, Venezuela",
+};
+}
