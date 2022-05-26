6bb3f84ca9050847571501bc464cfb70494a734f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 15:53:58 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_16560007_e_1653594833597.dart
@@ -0,0 +1,11 @@
+class AutoSuggestions16560007 { static final reviewedTitles16560007 = const {"La caridad fresco market - Centro comercial galerías playa grande nivel 1 local 18, Maiquetía 1162, La Guaira, Venezuela",
+"Vaporhot vzla - Av Principal de Playa Grande, Catia La Mar 1162, La Guaira, Venezuela",
+"Ferretería Comercial Ramar - Quinta Nilsa N°31. Calle Real De Antimano, entre calle montalbancito, y, C. La Estación, Caracas 1100, Distrito Capital, Venezuela",
+"Distribuidora Olivo C.A - Carrera 25 &amp;, C. 33, Barquisimeto 3001, Lara, Venezuela",
+"Cassa Market - 37 Carrera 25, Barquisimeto 3001, Lara, Venezuela",
+"Dulces y algo más - Carrera 28 &amp;, Calle 25, Barquisimeto 3001, Lara, Venezuela",
+"Clínica Vitalis - Piso 10, oficina 03, Torre América, Avenida Venezuela, Caracas 1050, Distrito Capital, Venezuela",
+"Novedades Hello C.A. - Av. Principal de Propatria &amp; Av Simón Bolívar. Centro Comercial Propatria, nivel 2, local 26 A, Caracas 1030, Distrito Capital, Venezuela",
+"Frigususa - Urb. Playa Grande Av. Hotel Marriott, 1162, La Guaira, Venezuela",
+};
+}
