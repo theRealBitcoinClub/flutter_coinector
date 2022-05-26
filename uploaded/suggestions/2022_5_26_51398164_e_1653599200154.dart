43aaa12bff604969f82eee81571bc19f915ecfb9
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 17:06:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_51398164_e_1653599200154.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions51398164 { static final reviewedTitles51398164 = const {"Inversiones Jhoelins, C.A. - Calle Libertador, Local 406, Mercado Cacique Maiquetia, Pasillo 5, Maiquetía 1161, La Guaira, Venezuela",
+"Full Pizza - 27R4+2W6, Av. Unda, Guanare 3350, Portuguesa, Venezuela",
+"Café Plaza - Calle 16 Entre Carreras 4 Y 5 Edificio Don Armando local 02 Frente A La Plaza Bolivar, Guanare 3350, Portuguesa, Venezuela",
+"Inversiones Don Miguelacho - Av. Principal de los proceres entre calle 1 y 2, sector los Próceres, casa nro 30, local 01, Guanare 3350, Portuguesa, Venezuela",
+};
+}
