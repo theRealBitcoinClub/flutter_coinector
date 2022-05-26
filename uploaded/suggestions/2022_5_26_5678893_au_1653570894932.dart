8704d96f195a82d50713ce94d8c7f26315aba1ca
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 09:14:59 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_5678893_au_1653570894932.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions5678893 { static final reviewedTitles5678893 = const {"Las Vegas Atelier de Uñas - Chacaito, Av Casanova, Centro Comercial Único, PB Local 12, Caracas 1060, Miranda, Venezuela",
+"Vestidos Diosmary - Galerias La Frayette, Edificio Benedetti, PB, Local 66, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"El Gordo Burger 12 - F3P6+899, Caracas 1020, Capital District, Venezuela",
+};
+}
