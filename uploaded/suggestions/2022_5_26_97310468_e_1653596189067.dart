e3958803e008dad5c5de0d5de2ffef50d930c509
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 16:16:33 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_97310468_e_1653596189067.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions97310468 { static final reviewedTitles97310468 = const {"11.11 Boutique - G46V+MR8, Avenida Transversal 10, Caracas 1060, Distrito Capital, Venezuela",
+"Pintacar Express - JX49+V7F, Calle Tacagua, Catia La Mar 1162, La Guaira, Venezuela",
+"Inversiones Oshun, C.A. - Calle libertador, Local 182, Mercado Cacique Maiquetia, Pasillo 3, Maiquetía 1161, La Guaira, Venezuela",
+"Fruber 1111 - Calle Páez, entre Av Guaicaipuro y calle Elice. Edificio Caracas, local C. Chacao, Caracas 1080, C. Páez, Caracas 1060, Distrito Capital, Venezuela",
+"Tu Cuevita Repostera La Arenosa - Carrera 12 entre calles 14 y 15 local 1 casa 12-37, Sector, Guanare 3350, Portuguesa, Venezuela",
+};
+}
