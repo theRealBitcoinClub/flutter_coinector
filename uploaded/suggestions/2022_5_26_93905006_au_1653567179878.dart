6c17e14540f2677ef803c1c13fc5899d400e917d
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 08:13:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_93905006_au_1653567179878.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions93905006 { static final reviewedTitles93905006 = const {"Comercial Nilson Modas - JX2F+X4X, Catia La Mar 1162, Vargas, Venezuela",
+"Distribuidora Yuruba - JX3G+7F3, Catia La Mar 1162, Vargas, Venezuela",
+"Mari&Alex Inversiones - JX3G+7F3, Catia La Mar 1162, Vargas, Venezuela",
+"Servidetalles - JX3F+86W, Catia La Mar 1162, Vargas, Venezuela",
+};
+}
