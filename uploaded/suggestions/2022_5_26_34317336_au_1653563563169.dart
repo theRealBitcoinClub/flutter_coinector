122888c21bb046802330dc5ec065779e33e19594
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 07:12:47 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_34317336_au_1653563563169.dart
@@ -0,0 +1,15 @@
+class AutoSuggestions34317336 { static final reviewedTitles34317336 = const {"Centro de Copiado Belen - JX3G+FF6, Catia La Mar 1162, Vargas, Venezuela",
+"MotoRespuestos El Peludo - Av José Antonio Paez, Resd Atlas, Local 02, Mun, Av. José Antonio Páez, Oeste 1021, Distrito Capital, Venezuela",
+"NIVEL 1 LOCAL 33 - G33P+HW9, Avenida Universidad, Caracas 1012, Distrito Capital, Venezuela",
+"Octopus Tattoo - F3V8+555, Caracas 1020, Capital District, Venezuela",
+"Blu-Ray Caracas - F4VV+R4M, Calle Páez, Caracas 1060, Distrito Capital, Venezuela",
+"Blu-Ray Caracas - F4VV+R4M, Calle Páez, Caracas 1060, Distrito Capital, Venezuela",
+"Time Game - Centro de la Economía Popular Manuela Sáenz, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Global Cel C.A. - Centro Comercial Único, Blvr. de Sabana Grande, Caracas 1060, Distrito Capital, Venezuela",
+"Don Andres ferreteria - Carrera 12 con calle 18, edificio Don Andrés, p/b local 01, Guanare 3350, Portuguesa, Venezuela",
+"Sunset Coffee & Market - Residencias Playate, Av. del Hotel, Maiquetía 1162, La Guaira, Venezuela",
+"Vespa Shoes Store - 9 y 11, entre carrera, Muro de Los Lamentos, local 5 Av. Unda, Guanare 3350, Portuguesa, Venezuela",
+"Inversiones Tecnológicas Lemus C.A. - Carrera 13 entre calle 15 y 16 casa 15-40 local 1 Sector, cementerio, Guanare 3350, Portuguesa, Venezuela",
+"Team Gamer - Local C002, nivel sótano. Centro Parque Carabobo, Avenida Universidad, entre Av Sur 11, y, Avenida Sur 13, Caracas 1011, Distrito Capital, Venezuela",
+};
+}
