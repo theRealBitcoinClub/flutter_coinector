5f35815585cc9fd06e5213fdcabd616dcc9aa93c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-25 22:13:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_25_58854749_au_1653531203988.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions58854749 { static final reviewedTitles58854749 = const {"DISTRIBUIDORA LA JOYA 2000, C.A. - CALLE LA JOYA EDIFICIO LA JOYA PB, LOCAL A, Caracas 1060, Miranda, Venezuela",
+"El Criollito Sanchez - La peñita, calle 23 con carrera 3, casa nro 22-43, local 2, planta baja, Guanare 3350, Portuguesa, Venezuela",
+"YC One Detail - Avenida Urdaneta &amp; Avenida Norte 1, entre esquina Madrices e Ibarra, Local 002, Caracas 1012, Distrito Capital, Venezuela",
+"Acrílicos pintacatia ca - 5 Av. Balneario, Catia La Mar 1162, La Guaira, Venezuela",
+};
+}
