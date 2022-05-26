1ff2756fcc9b35a390c61292ad206c189d4fc3e9
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-26 16:47:20 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_26_20093911_e_1653598035962.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions20093911 { static final reviewedTitles20093911 = const {"Bodegón RMR - Av. Unda con, P/B, edificio Aura, local 05 Carrera 12, Guanare 3350, Portuguesa, Venezuela",
+"Strong Physical Center, C.A - Nivel Mezzanina, Centro Comercial Virgen Del Valle, Local LM-45 A, Av. Alirio Ugarte Pelayo, Via Viboral, Maturín 6201, Monagas, Venezuela",
+"Alimentos y Suministros Galax - Centro Comercial Comunitario Norte, Local 0809, Av. Alirio Ugarte Pelayo, Via Viboral, Maturín 6201, Monagas, Venezuela",
+};
+}
