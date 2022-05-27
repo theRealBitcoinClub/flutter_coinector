ddaac6e3fb46040b1c4ca2b4b4f3c5105979d56c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-27 13:09:14 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_27_48302421_e_1653671350677.dart
@@ -0,0 +1,14 @@
+class AutoSuggestions48302421 { static final reviewedTitles48302421 = const {"Mundo de Azúcar, f.p. - Av unda entre calle 9 y 8 C.C La arenosa nivel 1, local 39 Av. Unda, Guanare 3350, Portuguesa, Venezuela",
+"UPF Delicateses la Gloria de Dios II - Calle Arvelo Casa Nro 03, Tocuyito 2035, Carabobo, Venezuela",
+"AHOPE Style - Local A15 C. Rondon, Tocuyito 2035, Carabobo, Venezuela",
+"Inversiones Peña Nuñez 2015 C.A - Calle Arvelo local Nro 15, Tocuyito 2035, Carabobo, Venezuela",
+"Don Miguel Pizzería - Los Chorritos, Calle Valencia con 2da Calle, Local Nro A-1, Av. 109, Tocuyito 2035, Carabobo, Venezuela",
+"AHOPE Style - Local A15 C. Rondon, Tocuyito 2035, Carabobo, Venezuela",
+"Inversiones Alianza de Dios JL C.A - Los Chorritos, Calle Las Bateas con 42, local 110-20, Tocuyito 2035, Carabobo, Venezuela",
+"Víveres Guacara F.P - Calle Bolivar, entre Jacinto Lara, Local 109 C. Laurencio Silva, Guacara 2015, Carabobo, Venezuela",
+"Abasto A Dios JL - Local 110, Los Chorritos, 21 C. Las Bateas, Tocuyito 2035, Carabobo, Venezuela",
+"Liontech Sambil - Avenida Libertador, C.C. Sambil Caracas, nivel acuario, local AC-C49, Caracas, Av. Libertador, Caracas 1064, Distrito Capital, Venezuela",
+"Abasto Los Chorritos - Los Chorritos, Av Venezuela, Casa Nro 67, Sector Uslar Pietri, Tocuyito 2035, Carabobo, Venezuela",
+"La Coromotana C.A - Local N°1, Los Chorritos, Calle El Valencia, con Av. 110, Tocuyito 2035, Carabobo, Venezuela",
+};
+}
