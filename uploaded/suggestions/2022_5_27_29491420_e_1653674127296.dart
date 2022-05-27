a5b9cf4496d929827bd6f2a674f0e33494c6f1e8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-27 13:55:30 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_27_29491420_e_1653674127296.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions29491420 { static final reviewedTitles29491420 = const {"Keves Vision - Avenida Este 0, entre av. Sur 19 y 21. C.C. Parque Caracas, PB, local 2, Caracas 1011, Caracas 1011, Distrito Capital, Venezuela",
+"Frutimarket Ccs - Av Este 2, entre av. sur 21 y 23. Edif. Torre Canaima, PB, local 2 y 3, Caracas 1011, Distrito Capital, Venezuela",
+"Casa Hot Dog - Av. Universidad, casa N. 179-105. Sector, Av. Universidad, Naguanagua 2005, Carabobo, Venezuela",
+"Lucian's tech - C.C. Gran bazar, J9RH+FRP, Maracaibo 4001, Zulia, Venezuela",
+"Representaciones Sitec C.A. - Quinta Irlanda, Calle Nicanor Bolet Peraza, Caracas 1040, Distrito Capital, Venezuela",
+};
+}
