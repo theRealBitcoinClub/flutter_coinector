170e7bdbfd5d89ad0a80550e1dce80b050fc7fd8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-27 16:39:36 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_27_8889713_e_1653683973146.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions8889713 { static final reviewedTitles8889713 = const {"Krispy Donuts El Bosque - Av Las Delicias, Edificio Doña Clemencia, Piso P.B, Local 231, Maracay 2102, Aragua, Venezuela",
+"Krispy Donuts - E/S Circulo Militar, Local Nro, Zona Postal, S/N Avenida Las Delicias, Maracay 2102, Aragua, Venezuela",
+};
+}
