66bcbf4651fd518b78a539cbfb1d37c3762e2c26
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-05-27 12:49:38 -0400

    c

--- uploaded/suggestions/2022_5_25_25296375_au_1653485564886.dart
+++
@@ -1,5 +0,0 @@
-class AutoSuggestions25296375 {
-  static final reviewedTitles25296375 = const {
-    "El Rey de la Hamburguesa - JX26+J43, Av. Balneario, Catia La Mar 1162, La Guaira, Venezuela",
-  };
-}
--- uploaded/suggestions/2022_5_26_83711774_au_1653568027989.dart
+++
@@ -1,7 +0,0 @@
-class AutoSuggestions83711774 {
-  static final reviewedTitles83711774 = const {
-    "Caribe Phone - JX3F+95F, Catia La Mar 1162, Vargas, Venezuela",
-    "Frutería El Mercadito - Av Principal de Baruta, C5J4+923, Caracas 1083, Miranda, Venezuela",
-    "POSADA VILLA DEL CARMEN - Avenida principal de Playa Grande, Calle 1, Catia La Mar 1162, La Guaira, Venezuela",
-  };
-}
