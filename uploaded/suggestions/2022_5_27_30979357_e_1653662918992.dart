320daa4c1203baffa99d0121936696490099296b
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-27 10:48:42 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_27_30979357_e_1653662918992.dart
@@ -0,0 +1,17 @@
+class AutoSuggestions30979357 { static final reviewedTitles30979357 = const {"Tecnochia - 5ta avenida entre calles 8 y 9 CC Shopping Center Nivel PB Local 12, San Cristóbal 5001, Táchira, Venezuela",
+"Inversiones Darwin Rojas, C.A. - Calle Real de Maiquetia, Local 667, Mercado Cacique Maiquetia, Pasillo 11, Maiquetía 1162, La Guaira, Venezuela",
+"Inversiones Milca Torres 2021, C.A. - Calle Real de Maiquetia, Local 597, Mercado Cacique Maiquetia, Pasillo 11, Maiquetía 1161, La Guaira, Venezuela",
+"Inversiones M & M 2013, C.A. - Calle Real de Maiquetia, Local 689, Mercado Cacique Maiquetia, Pasillo 11, Maiquetía 1162, La Guaira, Venezuela",
+"Inversiones Josdavier 2017, C.A. - Calle Real de Maiquetia, Local 677, Mercado Cacique Maiquetia, Pasillo 12, Maiquetía 1162, La Guaira, Venezuela",
+"Inversiones Delgado Maiquel(K.D.F.2176) F.P. - Calle Real de Maiquetia, Local 631, Mercado Cacique Maiquetia, Pasillo Central, Maiquetía 1162, La Guaira, Venezuela",
+"Inversiones Orocristal, C.A. - Calle Real de Maiquetia, Local 478, Mercado Cacique Maiquetia, Pasillo 11, Maiquetía 1162, La Guaira, Venezuela",
+"Inversiones Carol, C.A. - Calle Libertador local 247, Mercado Cacique Maiquetia, Pasillo 3, Maiquetía 1162, La Guaira, Venezuela",
+"Ferreteria Arturo G - Carrera 6 con esquina calle 2 casa Nro 2-3, local 1 y 2, Guanare 3350, Portuguesa, Venezuela",
+"El Tinajero Hogar C.A. - Calle 11 entre carrera 5 y 6 Edificio El Tinajero P/B Local 1, Guanare 3350, Portuguesa, Venezuela",
+"Ricar2 House - Los Chorritos, Av Principal, Casa Nro 34, Principal, Tocuyito 2035, Carabobo, Venezuela",
+"BB Store Servicios, CA - AV. Alirio Ugarte Pelayo, Centro Comercial Virgen Del Valle, Local 47 Piso 1, Maturín 6201, Monagas, Venezuela",
+"XTREMO JULKAR 27, C.A - Local 41 Piso PB, Centro Comercial Virgen Del Valle, Av. Alirio Ugarte Pelayo, Maturín 6201, Monagas, Venezuela",
+"FRUTTY MANIA - Centro Comercial Virgen Del Valle, AV. Alirio Ugarte Pelayo, Local 53, Piso 1 Via Viboral, Maturín 6201, Monagas, Venezuela",
+"Sucursal CA BST FARMA - Zona Industrial Castillito Centro Comercial Termini PB local 2. Av. Don Julio Centeno San diego, Valencia 2006, Carabobo, Venezuela",
+};
+}
