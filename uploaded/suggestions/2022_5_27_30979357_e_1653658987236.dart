8fa6e9e2e97a2efc90ac97cf52c4292ce612f6cf
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-05-27 12:45:46 -0400

    clean

--- uploaded/suggestions/2022_5_27_30979357_e_1653658987236.dart
+++
@@ -1,6 +0,0 @@
-class AutoSuggestions30979357 { static final reviewedTitles30979357 = const {"Tecnochia - 5ta avenida entre calles 8 y 9 CC Shopping Center Nivel PB Local 12, San Cristóbal 5001, Táchira, Venezuela",
-"Inversiones Darwin Rojas, C.A. - Calle Real de Maiquetia, Local 667, Mercado Cacique Maiquetia, Pasillo 11, Maiquetía 1162, La Guaira, Venezuela",
-"Inversiones Milca Torres 2021, C.A. - Calle Real de Maiquetia, Local 597, Mercado Cacique Maiquetia, Pasillo 11, Maiquetía 1161, La Guaira, Venezuela",
-"Inversiones M & M 2013, C.A. - Calle Real de Maiquetia, Local 689, Mercado Cacique Maiquetia, Pasillo 11, Maiquetía 1162, La Guaira, Venezuela",
-};
-}
