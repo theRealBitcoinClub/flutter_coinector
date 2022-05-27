32b9040ad211fcc81ac37529ac44f9aa095e0543
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-05-26 22:36:46 -0400

    check duplicates AM

--- uploaded/suggestions/2022_5_18_AM_1652922175160.dart
+++
@@ -1,5 +0,0 @@
-class AutoSuggestionsAM {
-  static final reviewedTitlesAM = const {
-    "Trovas Ve - Av 40a entre calles 161 y, Calle 162, San Francisco 4004, Zulia, Venezuela",
-  };
-}
--- uploaded/suggestions/2022_5_23_50975583_as_1653351065053.dart
+++ uploaded/suggestions/2022_5_23_50975583_as_1653351065053.dart
@@ -2,7 +2,6 @@ class AutoSuggestions50975583 {
   static final reviewedTitles50975583 = const {
     "EJCA Electrónica, C.A. - Av Sur 2. Entre Av Lecuna y Av Oeste 12. Edif. Sur 2, PB, Local 1, Caracas 1014, Distrito Capital, Venezuela",
     "Floristeria Paraiso Galipan - H2WQ+J8V, C. Real de Pariata, Maiquetía 1161, La Guaira, Venezuela",
-    "Trovas Ve - Av 40a entre calles 161 y, Calle 162, San Francisco 4004, Zulia, Venezuela",
     "Comercial el Divino Niño - J458+3GR, Vía Teleférico, Macuto 1164, La Guaira, Venezuela",
     "Inversiones VFC - H2WX+VCC, C. Los Baños, Maiquetía 1161, La Guaira, Venezuela",
     "Electrónica Compusol - H3W2+HF2, C. Los Baños, Maiquetía 1161, La Guaira, Venezuela",
