b9a145377f6daadf974cb2f16ecfee929f6d7d62
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-27 15:21:42 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_27_70149163_e_1653679298369.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions70149163 { static final reviewedTitles70149163 = const {"Restaurante Mansión Oriental - Av. Principal de Caribe, Bulevar Naiguata, Parcela 38-35, Qta. Timotes, PB, Local 1, Caraballeda 1165, La Guaira, Venezuela",
+"Burger Grill, C.A. - Av. La Playa con, local 38 Calle El Hambre, Caraballeda 1165, La Guaira, Venezuela",
+"Inversiones Erlyn, C.A. - Calle libertador, Puesto 215, Mercado Cacique Maiquetia, Pasillo 3, Maiquetía 1162, La Guaira, Venezuela",
+"Inversiones Rubí Esmeralda, C.A. - Av. Soublette, local 463, Mercado Cacique Maiquetia, Pasillo 5, Maiquetia 1162, La Guaira, Venezuela",
+};
+}
