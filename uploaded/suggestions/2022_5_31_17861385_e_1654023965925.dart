cb570cb039d6ddc0c1c234749cd02ae96b5dd92a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 15:06:10 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_17861385_e_1654023965925.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions17861385 { static final reviewedTitles17861385 = const {"Canguro sede Urdaneta tienda mayorista - Av. Urdaneta, de Pelota a Punceres. Residencias 26. PB y piso 1. Caracas, 1020, Av. Urdaneta, Caracas 1010, Distrito Capital, Venezuela",
+"Canguro Sede Sabana Grande (Servicio Técnico) - Caracas - Bulevar de Sabana Grande, C.C Sabana Grande, Nivel Mezanina, Local 3, local 03 C. Villaflor, Caracas 1050, Distrito Capital, Venezuela",
+"Canguro sede city market 2 - Calle Villa Flor Centro Camercial City Market, Piso 3, Local 335 codigo postal 1050. Caracas, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Sandy, F.P. - Calle Libertador, Local 208, Mercado Cacique Maiquetia, Pasillo 3, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
