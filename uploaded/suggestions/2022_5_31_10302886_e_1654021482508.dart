fc1e0dffd14303e283014ad1358f6a2994579c29
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 14:24:46 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_10302886_e_1654021482508.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions10302886 { static final reviewedTitles10302886 = const {"Velvet The Beauty House - Avda Francisco de Miranda, Centro Lido Nivel Miranda Local M14, Caracas 1060, Distrito Capital, Venezuela",
+"Boutique Jessimar - Unnamed Road, Guanarito 3354, Guanarito 3354, Portuguesa, Venezuela",
+};
+}
