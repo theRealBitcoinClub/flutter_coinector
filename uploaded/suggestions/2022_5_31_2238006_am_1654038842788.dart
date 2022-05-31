b16564157fd3e24c45aa5d4521cb1691ea04ddd3
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 19:14:07 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_2238006_am_1654038842788.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions2238006 { static final reviewedTitles2238006 = const {"Inversiones Tocoquera 2021, C.A. - F3WJ+WQP, Caracas 1014, Capital District, Venezuela",
+"PC MANIA, C.A. - Av. Francisco de Miranda, Multicentro Empresarial del Este, Edif. Central Planta Baja, Local PB-24, Caracas 1060, Miranda, Venezuela",
+};
+}
