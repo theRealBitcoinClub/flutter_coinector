057235f8ada7467447943b1e65398d017a9a9ddc
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 16:00:29 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_85263027_e_1654027224997.dart
@@ -0,0 +1,11 @@
+class AutoSuggestions85263027 { static final reviewedTitles85263027 = const {"Lio's Store - Unnamed Road, Guanarito 3354, Guanarito 3354, Portuguesa, Venezuela",
+"La Tiendita GA Saavedra - calle 5 entre carreras 5 y 6, local 07. Barrio Madre Vieja, 3354, Guanarito 3354, Portuguesa, Venezuela",
+"D Vivas Shop - MQVR+QJ8, Guanarito 3354, Portuguesa, Venezuela",
+"Zapatería Jkamy Boutique segundo piso local 1 - Carrera 7 con Calle 18, C.C Zafiro, Nivel 2, Local 1, Guanare 3350, Portuguesa, Venezuela",
+"GUERRERO SERVICELL CA (GRAN BAZAR) - J9RJ+M36, Maracaibo 4005, Zulia, Venezuela",
+"Inversiones Barac 2013 - local 76-77, Mercado Bolivariano de la Hoyada, Caracas, Caracas 1011, Distrito Capital, Venezuela",
+"lola express - local 12 y 13, Edificio Martín, Av. San Martín, Caracas 1020, Distrito Capital, Venezuela",
+"Multiservicios Mundo Tecnologia C.A. - Local Comercial ubicado en av. 15 Delicias CC Bazar, Maracaibo 4001, Zulia, Venezuela",
+"JKAMY Boutique - Local 3, Carrera 7 con Calle 18, C.C Zafiro, Nivel PB, C. 18, Guanare 3350, Portuguesa, Venezuela",
+};
+}
