62fc85e1b0dd34b4b50b786704eb835775ce51f9
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 14:52:33 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_95134969_e_1654023149595.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions95134969 { static final reviewedTitles95134969 = const {"Canguro sede city market 1 - Calle Villa flor, centro comercial City market PB local 114 código postal 1050. Caracas, C. Villaflor, Caracas 1050, Distrito Capital, Venezuela",
+"Canguro sede city market 2 - Calle Villa Flor Centro Camercial City Market, Piso 3, Local 335 codigo postal 1050. Caracas, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
