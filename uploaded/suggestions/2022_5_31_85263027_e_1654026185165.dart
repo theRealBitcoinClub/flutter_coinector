5d76bddd0f19c8758e9256166456f999ae2c35c1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 15:43:09 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_85263027_e_1654026185165.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions85263027 { static final reviewedTitles85263027 = const {"Lio's Store - Unnamed Road, Guanarito 3354, Guanarito 3354, Portuguesa, Venezuela",
+"La Tiendita GA Saavedra - calle 5 entre carreras 5 y 6, local 07. Barrio Madre Vieja, 3354, Guanarito 3354, Portuguesa, Venezuela",
+};
+}
