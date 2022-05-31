d68c96caae4ba641b8800991eed71fd13517dabd
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 17:19:37 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_28714974_e_1654031972794.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions28714974 { static final reviewedTitles28714974 = const {"Agua Purificada y Algo Más... - Avenida Luis De Camoens, Caracas 1061, Distrito Capital, Venezuela",
+"Inversiones Jhoelins, C.A. - Calle Libertador, Local 406, Mercado Cacique Maiquetia, Pasillo 5, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
