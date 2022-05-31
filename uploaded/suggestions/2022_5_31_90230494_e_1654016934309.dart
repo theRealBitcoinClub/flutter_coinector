6e92c17f7febfb51f48ebb81a797b215510e01e5
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 13:08:58 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_90230494_e_1654016934309.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions90230494 { static final reviewedTitles90230494 = const {"Led-Computer - Av. 15 Las Delicias con calle 100, C.C. Gran Bazar, nivel 1, local 756, Maracaibo 4005, Zulia, Venezuela",
+"Diseños & creaciones Snarpy - Local 4, Av. Unda, barrio La Arenosa, esquina Carrera 7, C.C Cordillera, Nivel 1, Av. Unda, Guanare 3350, Portuguesa, Venezuela",
+"Evoualji - Barrio Cementerio, esquina Carrera 8 con Calle 20, C.C City Center, P/B, Local 1, Guanare 3350, Portuguesa, Venezuela",
+"Rio Celeste - G36G+M92, Avenida Sucre, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
