68728c485964d6c1202f9caa97be1ad748e6df83
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 18:47:08 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_66197448_am_1654037223680.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions66197448 { static final reviewedTitles66197448 = const {"Maison Plaza - F5W3+JQR, Caracas 1060, Miranda, Venezuela",
+"V58 Store City Market 4 - C.C City Market, Nivel PB, Local Arca, N° 456 Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Mr.ShopCcs - C.C. Metrocenter Pasaje Contemporáneo, Local MT08 Caracas, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
