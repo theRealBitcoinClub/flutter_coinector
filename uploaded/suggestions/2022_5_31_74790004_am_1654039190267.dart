9c76896c7e3ced669e64f711ef728b95bdb93134
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 19:19:54 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_74790004_am_1654039190267.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions74790004 { static final reviewedTitles74790004 = const {"Mini Market DS, C.A - J263+346, Maiquetía 1162, Vargas, Venezuela",
+"TecnoLion MerpoEste - Centro Comercial MerpoEste, C. Los Ángeles, Caracas 1064, Distrito Capital, Venezuela",
+};
+}
