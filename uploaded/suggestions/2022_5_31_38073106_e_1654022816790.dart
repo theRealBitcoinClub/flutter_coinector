5653e08ae456e0331a37ce5ba1883a0b87f1df76
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 14:47:01 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_38073106_e_1654022816790.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions38073106 { static final reviewedTitles38073106 = const {"Creaciones Decomari - J9RJ+Q5F, Maracaibo 4005, Zulia, Venezuela",
+"Gravity Games - Planta Baja, Centro Plaza La Candelaria, local 26 Av. Urdaneta, Caracas 1011, Distrito Capital, Venezuela",
+"Android Store - J9RH+6QX, Av. Libertador, Maracaibo 4005, Zulia, Venezuela",
+"Arepas Full Sabor - Av 15 Las Delicias Con Calle 100, Centro Comercial GRAN BAZAR, Nivel 2, Local 10, Maracaibo 4005, Zulia, Venezuela",
+"Inversiones Dey 98 ó 2013, F.P. - Calle Libertador, Mercado Cacique Maiquetia, Local 70 Pasillo 1, Maiquetía 1161, La Guaira, Venezuela",
+};
+}
