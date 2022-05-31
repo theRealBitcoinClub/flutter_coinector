df0675723cad3c7b58971e0ae15fe166bf83219e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-05-31 19:40:30 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_5_31_6830076_am_1654040425685.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions6830076 { static final reviewedTitles6830076 = const {"Lolar store - Centro comercial Merpoeste, piso 1, pasillo tigre, local 125. Chacao, Caracas 1060, Caracas, Caracas 1060, Distrito Capital, Venezuela",
+"Los Golfeados de Samuel - Merpoeste. Piso 2 Local 105 Nivel Los Andes, Pasillo Manatí, C. Chico Carrasquel, Caracas 1060, Miranda, Venezuela",
+"Inversiones Samar - Piso 3 ultimo piso nivel feria de comida Centro Comercial Merpoeste, Local 49 C. Los Ángeles, Caracas 1071, Miranda, Venezuela",
+};
+}
