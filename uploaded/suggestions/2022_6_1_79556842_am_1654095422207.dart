79c6670f9cbe443ef9ce42ac2a02a1757659829c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-01 10:57:06 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_1_79556842_am_1654095422207.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions79556842 { static final reviewedTitles79556842 = const {"Junqui Fresas Norkys Chacao - Local 9H. Planta Baja. Avenida Libertador con Calle Elice. Edificio Rudolf, Caracas 1060, Miranda, Venezuela",
+"DeliExpress Barrio Obrero - Barrio Obrero Carrera 21 Entre calle 10 y, Pasaje Acueducto, San Cristóbal 5001, Táchira, Venezuela",
+};
+}
