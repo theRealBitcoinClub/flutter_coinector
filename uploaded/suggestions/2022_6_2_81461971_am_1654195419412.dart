ec84c5ca4d3aafcec568478630119e27141d0ae3
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 14:43:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_81461971_am_1654195419412.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions81461971 { static final reviewedTitles81461971 = const {"Kiosco El Toldito Amarillo - Calle los angeles, frente al centro comercial Merpoeste, F4QV+QVJ, Caracas 1060, Distrito Capital, Venezuela",
+"Tasca Restaurant Madame Fru Fru - Multicentro Empresarial del Este, Av. Libertador, Caracas 1060, Miranda, Venezuela",
+};
+}
