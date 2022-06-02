e880b22a68ed600b8d8cceb00c33457e73ead224
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 10:44:36 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_48338639_am_1654181071225.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions48338639 { static final reviewedTitles48338639 = const {"Óptica Contact-o-Lent C.A - F5X3+54Q, Caracas 1060, Miranda, Venezuela",
+"Comercializadora BS 3000 - Avenida principal de Maripérez, Quinta Tibidabo, PB, frente a la Toyota. Caracas 1050, Caracas 1050, Distrito Capital, Venezuela",
+"Comercializadora BS 3000 - Avenida principal de Maripérez, Quinta Tibidabo, PB, frente a la Toyota. Caracas 1050, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
