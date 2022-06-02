63959da5b40b1f8c96a97aa9c0b707d3e89b0387
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 09:03:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_16419388_am_1654175003391.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions16419388 { static final reviewedTitles16419388 = const {"Candy Man Globos RL - Esquina Gradillas a San Jacinto Edif. Gradillas C Piso PB Local 2 Urb. Catedral Caracas, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones Haysali - G35R+636, Caracas 1010, Capital District, Venezuela",
+"4 EVER PHONE, C.A. - G34P+7WG, Avenida Sur 1, Caracas 1012, Distrito Capital, Venezuela",
+};
+}
