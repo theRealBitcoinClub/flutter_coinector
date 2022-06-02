4812c8a9e8da27f7f895f0524657fae3a610f7e9
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 11:48:14 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_66135728_am_1654184889790.dart
@@ -0,0 +1,14 @@
+class AutoSuggestions66135728 { static final reviewedTitles66135728 = const {"ZONA PARTS - Av. Principal de Maripérez, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Toyomen 360 - local 2, Av. Principal de Maripérez, entre 6ta y 9na transversal. Quinta Gloria PB, Av. Principal de Maripérez, Caracas 1050, Distrito Capital, Venezuela",
+"Agridulce Sabores - av. intercomunal del valle, con calle el palmar. Res, C. 19 De Abril, Caracas 1090, Distrito Capital, Venezuela",
+"Ferretería Mfwollmer - Av. Vollmer, Caracas 1011, Distrito Capital, Venezuela",
+"Quinservices 21 C.A. - Av. Wollmer, Centro Comercial Lord Center, Sotano 1 L-17, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones azriel 2020 c.a - avenida principal de camburito numero 410-a, Maracay 2102, Aragua, Venezuela",
+"Petalos Café - Av. Wollmer, CC Lord Center Nivel Feria, L-S1-40, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones The champions JN y GR - C.C. Sábana Grande, local MT20, 11 Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Rest. Casa Bermeo - G33X+J2C, Avenida Sur 17, Caracas 1011, Distrito Capital, Venezuela",
+"JN Global Celular - C.C.City Market, Bulevar de Sabana Grande, Nivel Boulevard, local 232, nível 4, Local MT20 Caracas 1050, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Dulceria el milagro de Theos - Edificio El Tejar y Mohedano, Avenida Lecuna, Caracas 1015, Distrito Capital, Venezuela",
+"Willmarket - Parque Central, Sótano 1, local 2Cs05, Caracas 1015, Caracas 1015, Caracas 1015, Distrito Capital, Venezuela",
+};
+}
