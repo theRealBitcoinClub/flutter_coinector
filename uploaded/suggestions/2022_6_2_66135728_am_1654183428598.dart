d8fda0408f0e42247778a737c17fcd00eacbce85
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 11:23:53 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_66135728_am_1654183428598.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions66135728 { static final reviewedTitles66135728 = const {"ZONA PARTS - Av. Principal de Maripérez, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Toyomen 360 - local 2, Av. Principal de Maripérez, entre 6ta y 9na transversal. Quinta Gloria PB, Av. Principal de Maripérez, Caracas 1050, Distrito Capital, Venezuela",
+"Agridulce Sabores - av. intercomunal del valle, con calle el palmar. Res, C. 19 De Abril, Caracas 1090, Distrito Capital, Venezuela",
+"Ferretería Mfwollmer - Av. Vollmer, Caracas 1011, Distrito Capital, Venezuela",
+};
+}
