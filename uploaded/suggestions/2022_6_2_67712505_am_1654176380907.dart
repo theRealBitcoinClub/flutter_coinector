afefc8a3047910f4899dfb21595e2563bd0c3cb4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 09:26:25 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_67712505_am_1654176380907.dart
@@ -0,0 +1,14 @@
+class AutoSuggestions67712505 { static final reviewedTitles67712505 = const {"Charcutería Fraga 2020, C.A - Calle Norte 7 Entre la esquina de Socorro Y San Ramón Edif. Torre Jadim Piso PB Local 49 Urb, Caracas 1010, Distrito Capital, Venezuela",
+"Charcuteria y delicateses Stuart Little, c.a. - av. fuerzas Armadas. entre esquinas San Enrique a San José. local 10 Caracas, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones Alexvall, C.A - Av. Fuerzas Armadas CC. Teleader Los Andes Nivel PB Local, G37V+P3W, Av. A-1, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones Mascotalandia - G36R+PQR, Caracas 1010, Capital District, Venezuela",
+"Inversiones Mascotalandia II, C.A - AV. Fuerzas Armadas Edif. Acre Piso PB Local Urb, G39P+7R5, Av. San José Del Ávila, Caracas 1010, Distrito Capital, Venezuela",
+"Comercial Santa Ana 2008, C.A - Av. Éste 2 Edif. Cabudare Piso PB Local Mezzanine Nro 1 Urb, Caracas 1010, Distrito Capital, Venezuela",
+"RDCELL 2019, C.A - Av Abraham Lincoln Con Calle Union Y Villaflor CC. City market Nivel PB Local 150 Urb, Caracas 1050, Distrito Capital, Venezuela",
+"Al Giorno C.A. - Sótano 1 Locales, Edificio El Tejar y Mohedano, 33 y 37 Avenida Lecuna, Caracas 1015, Distrito Capital, Venezuela",
+"Judibana 2010 C.A - Edificio El Tejar y Mohedano, Sótano 1, Local 2CS06, Avenida Lecuna, Caracas 1015, Distrito Capital, Venezuela",
+"Modas CV C.A. - Edificio El Tejar y Mohedano, Sótano 1, Local NCS, 08 Avenida Lecuna, Caracas 1015, Distrito Capital, Venezuela",
+"Luz Publicidad C.A. - Edificio El Tejar y Mohedano, Sótano 1, Local 2CS04, Avenida Lecuna, Caracas 1015, Distrito Capital, Venezuela",
+"Gigis Diseños Exclusivos - C.C Locatel, Esquina de Cruz a Candelito, Urb, G34V+4W6, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
