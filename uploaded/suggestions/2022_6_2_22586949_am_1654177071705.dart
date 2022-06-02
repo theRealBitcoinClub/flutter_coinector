f3902d79f55b2b3773d2826caf67f47d174fe5eb
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 09:37:56 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_22586949_am_1654177071705.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions22586949 { static final reviewedTitles22586949 = const {"ToyoMotors Malpica - Calle 5, San Cristóbal 5001, Táchira, Venezuela",
+"El Buen Sabor Andino - Esquina Miguelacho a Tracabordo Edif. Cachazo Piso PB Local Nro 12 Urbanización, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
