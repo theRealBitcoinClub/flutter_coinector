09ed220729ef3a31228bfb0c23e223617607c63f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 11:20:10 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_66135728_am_1654183205162.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions66135728 { static final reviewedTitles66135728 = const {"ZONA PARTS - Av. Principal de Maripérez, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Toyomen 360 - local 2, Av. Principal de Maripérez, entre 6ta y 9na transversal. Quinta Gloria PB, Av. Principal de Maripérez, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
