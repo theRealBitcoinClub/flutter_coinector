ad0ec8f102eb4c567edca0a2fe6002b3cdfefa53
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 10:12:20 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_20376832_am_1654179135803.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions20376832 { static final reviewedTitles20376832 = const {"Inversiones Olofidio Mary, C.A - Calle Las Flores Edif. Libertador 1 Torre B Local PB Urb, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Repuestos Orlando Motos - Calle 5, San Cristóbal 5001, Táchira, Venezuela",
+"AdriCar's - Calle 5, con carrera 5, San Cristóbal 5001, Táchira, Venezuela",
+"Bar y Restaurante El Meson De Cervantes - 8 Av. Urdaneta, Caracas 1010, Distrito Capital, Venezuela",
+"Proyectos Yaku Kiria Refill - Av. Fuerzas Armadas Esquina de Socorro Edif. Conjunto residencial El Dorado Piso PB Local 4-1 Urb, Caracas 1010, Distrito Capital, Venezuela",
+"Repuestos Viking Moto, C.A - Av. Norte Nueve Edif. San Onofre Piso PB Local 1 Sector Las Esquinas De Animas Caracas Caracas, Caracas 1030, Distrito Capital, Venezuela",
+"BRYGA 325748, C.A - C.C. Plaza La Candelaria PB Local 4 Esquina de Candilito a Cruz frente a La Plaza, Caracas 1010, Distrito Capital, Venezuela",
+"Víveres Deisy - Av. Sur 15 Local San Luis Nro 3C Y 4D Sector Entre las Esquinas de Peligro a, Edificio Alcabala, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
