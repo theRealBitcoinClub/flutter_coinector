8adc18b9b4e3a7a4f0330ca87bd5dbc20c964a7c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 15:58:50 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_93788555_am_1654199925040.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions93788555 { static final reviewedTitles93788555 = const {"Restaurant Royal - G42H+H54, C. Real de Chapellín, Caracas 1050, Distrito Capital, Venezuela",
+"Batstore.ve - Calle Villaflor con Union, CC City Market N- Plaza Local Kiosco K-178 Caracas, Caracas 1050, Distrito Capital, Venezuela",
+"La Merienda Express, C.A - Av Juan Bautista Arismendi, local 75A, con C. Negrín, Caracas 1050, Distrito Capital, Venezuela",
+"Kiosko Alexmar - Calle Arismendi Frente al CC, G42G+JP3, Caracas 1050, Distrito Capital, Venezuela",
+"Distribuidora Alfredroame - Av Andrés Bello entre las Palmas y Las acacias, Quinta Lilian, G43C+77G, Norte 12, Caracas 1060, Distrito Capital, Venezuela",
+"Abasto y Perfumería los Tres Casiques - Calle tercera Transversal Norte Quinta Coromoto PB Local 2 Urbanización Guaicaipuro, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
