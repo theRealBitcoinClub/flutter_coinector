67f1bea74884e82f71434ab5ff14a729b2fb4767
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 11:53:07 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_21513955_am_1654185181960.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions21513955 { static final reviewedTitles21513955 = const {"V58 STORE SAMBIL CARACAS - AV LIBERTADOR CC SAMBIL NIVEL ACUARIO LOCAL AC-C19SECTOR, Caracas 1064, Miranda, Venezuela",
+"Kaprichos Susy C.A - Edificio El Tejar y Mohedano, Nivel Sotano 1, Local NCS, 19 Avenida Lecuna, Caracas 1015, Distrito Capital, Venezuela",
+};
+}
