f9102f4be314b2ba87ae7934c8068b8c476b91bb
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 08:34:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_53704810_am_1654173239328.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions53704810 { static final reviewedTitles53704810 = const {"Oasis H²O - G434+Q2M, Transversal 2 Sur, Caracas 1050, Distrito Capital, Venezuela",
+"Home Clean JC - Avenida principal de Sarria, calle real Los Cortijos de Sarria, urbanización, local 10, Caracas 1050, Distrito Capital, Venezuela",
+"Plaza Bolívar Caracas - Edificio Casa del Profesor, Av. Las Acacias, Caracas 1041, Distrito Capital, Venezuela",
+"Game Play Centro - Av. Urdaneta, Torres a Veroes, Mini Centro Plaza Bolívar, Locales 89 y, 94 Av. Urdaneta, Caracas 1012, Distrito Capital, Venezuela",
+"Cindy's Studio - Parroquia Carlos Soublette Pariata. 2da calle los 2 cerritos, La Guaira 1161, La Guaira, Venezuela",
+"Restaurante Las Vegas II,C.A - Esq. De Torres A Veroes Edificio. 6 Piso PB parroquia Catedral Caracas, Caracas 1060, Distrito Capital, Venezuela",
+"El Papá de las Gorras, C.A - Esq. La Marron Edif. Galerias la Marron Piso PB Local 6 Urb. Pquia. Catedral Caracas, Caracas 1030, Distrito Capital, Venezuela",
+"Inversiones FASYMJ, C.A - Esquina Marrón a Madrices Edif. 38 PB Local 2 Urbanización Catedral Caracas, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
