c7a4de4f5b9db61d3f18b1ff507aea3571278541
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 15:04:42 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_81461971_am_1654196677645.dart
@@ -0,0 +1,16 @@
+class AutoSuggestions81461971 { static final reviewedTitles81461971 = const {"Kiosco El Toldito Amarillo - Calle los angeles, frente al centro comercial Merpoeste, F4QV+QVJ, Caracas 1060, Distrito Capital, Venezuela",
+"Tasca Restaurant Madame Fru Fru - Multicentro Empresarial del Este, Av. Libertador, Caracas 1060, Miranda, Venezuela",
+"Kiosco Elice - Av. Elice, Entre Libertador y, F4RV+8Q7, Avenida Francisco de Miranda, Caracas 1060, Distrito Capital, Venezuela",
+"BODEGON LA FORTALEZA & 2020 C.A FRUTERIA Y PANIFICADORA - 0102 Caracas, Caracas 0102, Distrito Capital, Venezuela",
+"ToyoFord - Calle5, San Cristóbal 5001, Táchira, Venezuela",
+"Punto y Coma - Quinta Soledad Calle principal de, G442+8HP, Caracas 1050, Distrito Capital, Venezuela",
+"Kiosco El Poeta - Av. Andres bello, G434+WFP, 4ta Transversal, Caracas 1050, Distrito Capital, Venezuela",
+"La Principal Renault F-1 C.A - San Cristobal 5001, Táchira, Venezuela",
+"SH GENERICOS Y MAS - QQ49+XXJ, Calle 4 Bis, San Cristóbal 5001, Táchira, Venezuela",
+"Tiendas Yaris - C.C MetroCenter, Caracas 1234, Distrito Capital, Venezuela",
+"Purificadora de agua alda - G422+WG4, Av Este 2, Caracas 1011, Distrito Capital, Venezuela",
+"Centro de Copiado Bargoni - Av. Norte 13, Local 27, Centro Comercial Candilito, con Av. Urdaneta, Caracas 1010, Distrito Capital, Venezuela",
+"restaurante mosoly - plaza local16 bp, Centro Comercial Galería, Av. Urdaneta, Caracas 1011, Distrito Capital, Venezuela",
+"Hamaca Cafe - Av. Avila con Urdaneta, CC Galerias Avila, Nivel Anauco, L-A41, Caracas 1011, Distrito Capital, Venezuela",
+};
+}
