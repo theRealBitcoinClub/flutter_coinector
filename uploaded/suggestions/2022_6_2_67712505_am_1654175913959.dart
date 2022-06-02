127417fa806b7038f31b564eb39604c71159c185
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 09:18:39 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_67712505_am_1654175913959.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions67712505 { static final reviewedTitles67712505 = const {"Charcutería Fraga 2020, C.A - Calle Norte 7 Entre la esquina de Socorro Y San Ramón Edif. Torre Jadim Piso PB Local 49 Urb, Caracas 1010, Distrito Capital, Venezuela",
+"Charcuteria y delicateses Stuart Little, c.a. - av. fuerzas Armadas. entre esquinas San Enrique a San José. local 10 Caracas, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones Alexvall, C.A - Av. Fuerzas Armadas CC. Teleader Los Andes Nivel PB Local, G37V+P3W, Av. A-1, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones Mascotalandia - G36R+PQR, Caracas 1010, Capital District, Venezuela",
+"Inversiones Mascotalandia II, C.A - AV. Fuerzas Armadas Edif. Acre Piso PB Local Urb, G39P+7R5, Av. San José Del Ávila, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
