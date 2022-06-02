317687c267f09f642431fc7934a9f5e8b611a2a9
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 16:26:59 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_24554457_am_1654201614760.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions24554457 { static final reviewedTitles24554457 = const {"171 Emergencia, C.A. - Av. Don Bosco, con 1ra Transv., Edif. 171 Emergencia CA,, Caracas 1050, Distrito Capital, Venezuela",
+"Hotel Royal Palace - F4WH+VPG, Calle Cantuara, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
