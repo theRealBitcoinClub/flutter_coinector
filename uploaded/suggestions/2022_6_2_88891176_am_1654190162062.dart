195cbe452d2fa4a001084773b8d9b8a78b92baa1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 13:16:07 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_88891176_am_1654190162062.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions88891176 { static final reviewedTitles88891176 = const {"Toner Xpres - Edificio San Martín, Caracas 1015, Distrito Capital, Venezuela",
+"Comercial Ponketa - Edificio San Martín, Caracas 1015, Distrito Capital, Venezuela",
+"Truco Café C.A - Edificio San Martín, Pasillo peatonal, local E, Caracas 1015, Caracas 1015, Caracas 1015, Distrito Capital, Venezuela",
+};
+}
