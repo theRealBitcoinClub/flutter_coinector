78eb7ae80c86d0e14c740a00692720457bf0dde7
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 08:16:42 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_42094977_am_1654172196957.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions42094977 { static final reviewedTitles42094977 = const {"Kanji Sushi Barquisimeto - 22 y 23 C. 15, Barquisimeto 3001, Lara, Venezuela",
+"Metrolandia center 18k C.A - Avenida universidad centro comercial metrocenter local pb-m4 Caracas, Caracas 1010, Distrito Capital, Venezuela",
+"Delicatessen Willson 2 - Planta Baja, Edificio Mary, local A, Av. Andrés Bello, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
