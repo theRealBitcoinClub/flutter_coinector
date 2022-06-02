e165d64ae7ca0b823212d4eb1733239186cf7cfd
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 08:27:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_53704810_am_1654172828404.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions53704810 { static final reviewedTitles53704810 = const {"Oasis H²O - G434+Q2M, Transversal 2 Sur, Caracas 1050, Distrito Capital, Venezuela",
+"Home Clean JC - Avenida principal de Sarria, calle real Los Cortijos de Sarria, urbanización, local 10, Caracas 1050, Distrito Capital, Venezuela",
+"Plaza Bolívar Caracas - Edificio Casa del Profesor, Av. Las Acacias, Caracas 1041, Distrito Capital, Venezuela",
+};
+}
