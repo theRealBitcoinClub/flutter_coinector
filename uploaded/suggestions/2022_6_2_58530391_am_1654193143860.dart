511d42c5ae460e9e4a5c5a73edeb381f87680bc6
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 14:05:49 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_58530391_am_1654193143860.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions58530391 { static final reviewedTitles58530391 = const {"Hotel Crillon Suites - Esquina, Av. Libertador, Caracas 1050, Distrito Capital, Venezuela",
+"Kartofell La Papota del Sabor, C.A - Planta baja, esquina Coromoto, Edificio Oramas, Local 6 Av. Casanova, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Todo Oficina Y Computación C.A. - Esquina de Candilito a Cruz de Candelaria, G34V+9W4, Caracas 1011, Distrito Capital, Venezuela",
+"Inversiones RRR - Esquina Paradero Av Venus Edif Venus Piso PB Local A, Urb. Terrazas de Guaicoco, Caracas 1010, Distrito Capital, Venezuela",
+"Dilybert Serv Cell - C.C. Plaza Candelaria, Nivel MZ Local 07-A ➗ Banco Provincial y C.C. Reservista Caracas, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
