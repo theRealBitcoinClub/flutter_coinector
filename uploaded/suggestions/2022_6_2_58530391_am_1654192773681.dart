c6114d5bf8c9ec7d2c52d0f76b276282e7b694c9
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 13:59:38 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_58530391_am_1654192773681.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions58530391 { static final reviewedTitles58530391 = const {"Hotel Crillon Suites - Esquina, Av. Libertador, Caracas 1050, Distrito Capital, Venezuela",
+"Kartofell La Papota del Sabor, C.A - Planta baja, esquina Coromoto, Edificio Oramas, Local 6 Av. Casanova, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
