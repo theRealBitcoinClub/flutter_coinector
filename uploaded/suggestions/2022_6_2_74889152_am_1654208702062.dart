9b0659595f3f751dfc9d98633f80dcb7077e8892
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 18:25:07 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_74889152_am_1654208702062.dart
@@ -0,0 +1,9 @@
+class AutoSuggestions74889152 { static final reviewedTitles74889152 = const {"Opti Ávila, C.A - Av Abraham Lincoln CC Sabana Grande Nivel PB Local MT-PB, 9 Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Bodemax 2019, C.A - Calle Negrín CC Mini Tiendas 199 Nivel PB Local 36 Urbanización, Caracas 1050, Distrito Capital, Venezuela",
+"El portal MiniMarket - Avenida Mohedano con Calle Urdaneta local gm-25, Mercado Municipal de Chacao, Caracas 1060, Miranda, Venezuela",
+"Suministros AFI C.A - 1070 Caracas, Caracas 1071, Distrito Capital, Venezuela",
+"Autorepuestos21 C.A - 1050 Caracas, Caracas 1050, Distrito Capital, Venezuela",
+"Moto Repuestos El Ruso - Mercado César Nieves piso 1 local 387 Catia la mar vargar, Catia La Mar 1162, La Guaira, Venezuela",
+"Kiosco Betania - Calle Villaflor, Entre Boulevard de Sabana Grande, y, Av. Casanova, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
