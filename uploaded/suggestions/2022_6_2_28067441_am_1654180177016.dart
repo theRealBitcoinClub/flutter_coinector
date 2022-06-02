0e28351c379aaac9848fbffb9694be9ff58d8de8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 10:29:42 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_28067441_am_1654180177016.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions28067441 { static final reviewedTitles28067441 = const {"Autopartes Hidalgo Todo Corsa - Calle 6 entre carrera 6 y prolongacion de la, 5ta. Av &quot;Francisco García de Hevia&quot;, San Cristóbal 5001, Táchira, Venezuela",
+"Luncheria Madame Fru Fru self service. - Centro empresarial del este, planta ingreso, local 2. Caracas 1060, Caracas 1060, Distrito Capital, Venezuela",
+};
+}
