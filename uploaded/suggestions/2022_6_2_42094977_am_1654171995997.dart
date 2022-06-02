c5955e22d3ec8931f628931ec61b91f4fef18f9b
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-02 08:13:21 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_2_42094977_am_1654171995997.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions42094977 { static final reviewedTitles42094977 = const {"Kanji Sushi Barquisimeto - 22 y 23 C. 15, Barquisimeto 3001, Lara, Venezuela",
+"Metrolandia center 18k C.A - Avenida universidad centro comercial metrocenter local pb-m4 Caracas, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
