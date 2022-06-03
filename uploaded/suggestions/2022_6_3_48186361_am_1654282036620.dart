6275282e67f6edf77a1d13d09c378b8211549b86
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 14:47:17 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_48186361_am_1654282036620.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions48186361 { static final reviewedTitles48186361 = const {"Soluciones ACJ 1938, C.A - https://www.facebook.com, Local 63, Soluciones-ACJ-CA, C. El Muñeco, Caracas 1060, Miranda, Venezuela",
+"Mercado Bolivariano de La Hoyada - mercado la hoyada, Caracas 1010, Distrito Capital, Venezuela",
+"Perros Calientes y Hamburguesas Ortega M - Entrada mercado Bolivariano La Hoyada, G33R+26R, Avenida Fuerzas Armadas, Caracas 1012, Distrito Capital, Venezuela",
+};
+}
