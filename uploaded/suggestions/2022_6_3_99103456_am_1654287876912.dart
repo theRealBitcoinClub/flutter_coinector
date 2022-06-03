ed66ef3299c599ad1217641837e26338af6533e5
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 16:24:37 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_99103456_am_1654287876912.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions99103456 { static final reviewedTitles99103456 = const {"N-veces Inversiones - Calle Villaflor con Union, CC City Market N, 163 Calle L, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Mepix C.A - C.C. City Market, piso 4, local 444 y, 416 Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Repuesto Cell OOC - C.C Sábana Grande, Nivel Mezanina, Local 04 Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Bluestore C.A - C.C City Market, Nivel 4, Local 441, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"IC Fix Servicio Técnico - C.C Sábana Grande, nivel sotano,local 25, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
