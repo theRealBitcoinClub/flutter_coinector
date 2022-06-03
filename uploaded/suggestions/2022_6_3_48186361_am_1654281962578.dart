7be87afcac90c1a2591ca605aca58c2be75f8a91
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 14:46:03 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_48186361_am_1654281962578.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions48186361 { static final reviewedTitles48186361 = const {"Soluciones ACJ 1938, C.A - https://www.facebook.com, Local 63, Soluciones-ACJ-CA, C. El Muñeco, Caracas 1060, Miranda, Venezuela",
+"Mercado Bolivariano de La Hoyada - mercado la hoyada, Caracas 1010, Distrito Capital, Venezuela",
+};
+}
