5a6caad09ae2a0b0658c35fa9394ab9b32d7d9d2
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 12:55:46 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_90266055_am_1654275345502.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions90266055 { static final reviewedTitles90266055 = const {"La Tostadita - G434+WVR, Av. Andrés Bello, Caracas 1050, Distrito Capital, Venezuela",
+"La Tostadita Luncheria - Andres bello / 4 y, Quinta Cisco L-1, 5 Transversal, Caracas 1050, Distrito Capital, Venezuela",
+"Pet house - Centro Comercial Bello Monte, Caracas 1041, Distrito Capital, Venezuela",
+};
+}
