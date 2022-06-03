5e31b900036d45e5c5f2b76bfc6e7c278333da54
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 16:17:29 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_9352509_am_1654287448769.dart
@@ -0,0 +1,9 @@
+class AutoSuggestions9352509 { static final reviewedTitles9352509 = const {"To The Games C.A - C.C City Market, Piso 4 Local 450 , Caracas 1050, Caracas 1050, Distrito Capital, Venezuela",
+"Bucare Deli-Burguers - Propatria 2da avenida. Entre calles 6 y 7 Caracas, Caracas 1030, Distrito Capital, Venezuela",
+"Espacios Kala´s - Edificio la piñata, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Lucky cell 2 - JX58+66P, Jl. Wonosobo - Purworejo, Tempelsari, Maduretno, Kec. Kalikajar, Kabupaten Wonosobo, Jawa Tengah 56372, Indonesia",
+"Coffe Party Ciber - C.C. del Este Nv. PB L-15 Urb, Caracas 1050, Distrito Capital, Venezuela",
+"Lucky Cell - Calle Villaflor con Union CC City Market Nivel Bazar P- B401 Caracas Calle Villaflor con Union CC City Market Nivel Bazar P- B225 Caracas Boulevard De Sabana Grande Minicentro 199, Local 35 y 36, Caracas 1050, Distrito Capital, Venezuela",
+"Centro Médico Louis - Petare, Calle Sucre. zona colonial, frente a la plaza Sucre casa #12 Caracas, Caracas 1070, Distrito Capital, Venezuela",
+};
+}
