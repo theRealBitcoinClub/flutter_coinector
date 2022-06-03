03c4ef0c28e716c4e08e9a0bbad5cf49d470717a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 15:53:48 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_9352509_am_1654286027551.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions9352509 { static final reviewedTitles9352509 = const {"To The Games C.A - C.C City Market, Piso 4 Local 450 , Caracas 1050, Caracas 1050, Distrito Capital, Venezuela",
+"Bucare Deli-Burguers - Propatria 2da avenida. Entre calles 6 y 7 Caracas, Caracas 1030, Distrito Capital, Venezuela",
+"Espacios Kala´s - Edificio la piñata, Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Lucky cell 2 - JX58+66P, Jl. Wonosobo - Purworejo, Tempelsari, Maduretno, Kec. Kalikajar, Kabupaten Wonosobo, Jawa Tengah 56372, Indonesia",
+};
+}
