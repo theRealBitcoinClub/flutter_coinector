3fd169dd64200c8e5daf5b7954e40055a6af1458
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 13:33:27 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_39555426_am_1654277607011.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions39555426 { static final reviewedTitles39555426 = const {"Romy stile - Calle el Samán con Calle Chico Carrasquel, C.C Merpoeste, Piso 1 Local H185, Caracas 1064, Miranda, Venezuela",
+"Inversiones Barbaramar, C.A. - Calle de Silencio a Jefatura, PB, Local 27-16, Maiquetía 1160, La Guaira, Venezuela",
+"Plaza C. Perfumería - Centro Comercial, Casa Bera Local PB022, Av. Urdaneta, Caracas 1010, Distrito Capital, Venezuela",
+"Inversiones Wildriana - Esquina Monroy, pasillo Táchira con ecuador, local 1721 y 1722, Caracas, Caracas, Caracas 1011, Distrito Capital, Venezuela",
+"Cosméticos Leis - Mercado bolivariano la hoyada, pasillo Anzoátegui, local 1999, Caracas, Caracas, Caracas 1011, Distrito Capital, Venezuela",
+};
+}
