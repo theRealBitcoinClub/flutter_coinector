f0f76b80e461916947c0fcf6e544b77f6b223798
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 08:42:14 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_96217557_am_1654260133725.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions96217557 { static final reviewedTitles96217557 = const {"Restaurante Doña Barbara - Local B, Edificio Ovidio, Av Francisco Solano Lopez, Caracas 1050, Distrito Capital, Venezuela",
+"Fashion View Óptica - 1071 Caracas, Caracas 1071, Miranda, Venezuela",
+};
+}
