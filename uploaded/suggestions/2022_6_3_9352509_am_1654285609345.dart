37609798fe70ab1dfa02612aac0d3e258ac908d0
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 15:46:50 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_9352509_am_1654285609345.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions9352509 { static final reviewedTitles9352509 = const {"To The Games C.A - C.C City Market, Piso 4 Local 450 , Caracas 1050, Caracas 1050, Distrito Capital, Venezuela",
+"Bucare Deli-Burguers - Propatria 2da avenida. Entre calles 6 y 7 Caracas, Caracas 1030, Distrito Capital, Venezuela",
+};
+}
