878f9e7befe5f56c8deccf7f1d769ce87bee338c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 17:34:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_98875053_am_1654292041965.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions98875053 { static final reviewedTitles98875053 = const {"Centro Medico Diagnóstico Sabana Grande - 11 Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"SimCardRental - Centro Ciudad Comercial Tamanaco nivel C1 Caracas, Caracas 1060, Distrito Capital, Venezuela",
+};
+}
