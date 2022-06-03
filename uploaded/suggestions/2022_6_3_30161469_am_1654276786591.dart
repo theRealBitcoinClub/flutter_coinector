cce4c6542e5825a341e550d69d81e23a1865e5f6
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 13:19:47 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_30161469_am_1654276786591.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions30161469 { static final reviewedTitles30161469 = const {"Bodegon Doña Paula, C.A - Esq. De Candilito Local Nro PB Urba La Candelaria, al Lado del Banco Vzla Caracas, G34V+2HF, Caracas 1010, Distrito Capital, Venezuela",
+"panaderia pasteleria la corona - Av. Jose Maria baralt, entre Av Oeste 6 y Av. Este Oeste 8 Local Nro 55-57 sector, Caracas 1020, Distrito Capital, Venezuela",
+"Celulares AM asociados - Calle San Antonio, reducto a Municipal, edificio 05, local PB 01, Caracas 1012, Caracas 1012, Distrito Capital, Venezuela",
+};
+}
