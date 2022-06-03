064301dc8f7e4d56bebede37f81c27125054516c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 14:51:15 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_54550283_am_1654282275140.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions54550283 { static final reviewedTitles54550283 = const {"Perros Calientes y Hamburguesas Ortega M - Entrada mercado Bolivariano La Hoyada, G33R+26R, Avenida Fuerzas Armadas, Caracas 1012, Distrito Capital, Venezuela",
+"ADVERSUM5 - C.C. Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Moder Cell AD 2021 - Centro Comercial City Market, nivel feria galerias, local 321, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
