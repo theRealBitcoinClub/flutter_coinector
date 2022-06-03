6a7c05c52d2142e44a8c5dee5ff5af4a02665f9e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 13:07:31 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_90266055_am_1654276050944.dart
@@ -0,0 +1,11 @@
+class AutoSuggestions90266055 { static final reviewedTitles90266055 = const {"La Tostadita - G434+WVR, Av. Andrés Bello, Caracas 1050, Distrito Capital, Venezuela",
+"La Tostadita Luncheria - Andres bello / 4 y, Quinta Cisco L-1, 5 Transversal, Caracas 1050, Distrito Capital, Venezuela",
+"Pet house - Centro Comercial Bello Monte, Caracas 1041, Distrito Capital, Venezuela",
+"Restaurante Da Tulio, C.A - Calle el colegio Edif. Hotel Luna luna, Planta baja, Local 1 Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Fergia's Studio's - 1071 Caracas, Caracas 1071, Miranda, Venezuela",
+"Mercería Tejeycrea - Centro Comercial Propatria, nivel 2, local A-26 Frente a panadería La Gran Sabana Caracas, Caracas 1000, Distrito Capital, Venezuela",
+"Jhony Car's Repuestos - C. 4 Bis, San Cristóbal 5001, Táchira, Venezuela",
+"Repuestos Cordillera - Repuestos Cordillera Calle 4 con Carrera 9, La Concordia Local N 9 - 15, San Cristóbal 5001, Táchira, Venezuela",
+"Sport London - Edificio Sport London, Esquina de Muñoz a Padre Sierra, Av Este 2, Caracas 1012, Distrito Capital, Venezuela",
+};
+}
