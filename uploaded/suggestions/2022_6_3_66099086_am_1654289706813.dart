e0749e7e836b45e9c886463d64c875500eb77665
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 16:55:07 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_66099086_am_1654289706813.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions66099086 { static final reviewedTitles66099086 = const {"Lion Tech Chacaito - Plaza Brion, Royal Palace, PB Local B, Chacaito Caracas, Caracas 1050, Distrito Capital, Venezuela",
+"La Mejor Paella de Caracas - Av. Casanova con 2da Av. de, Edif. Campanero, Caracas 1050, Distrito Capital, Venezuela",
+"MULTISERVICIOS VORTEX 1337 - Central de Campo Rico, Caracas 1073, Miranda, Venezuela",
+};
+}
