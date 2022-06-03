95eb5712ed8c0caa52466cb1c02d793f33ac2200
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-03 10:32:34 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_3_32372718_am_1654266753520.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions32372718 { static final reviewedTitles32372718 = const {"Clean Madel - Av. Principal de, Quinta 24, Av. Andrés Bello, Caracas 1050, Distrito Capital, Venezuela",
+"Inversiones Hega 21 - Av. Andres Bello, Cruce con Merida, Quinta MArile L-8, Caracas 1050, Distrito Capital, Venezuela",
+"Restaurante JG - Calle Villaflor, CC Sabana Grande, Nivel Feria L-40 Caracas, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
