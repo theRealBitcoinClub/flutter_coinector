1111bcc87055901633fa3eb371e2ef863a972c57
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-04 18:27:01 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_4_95763284_am_1654381620309.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions95763284 { static final reviewedTitles95763284 = const {"La Spezia Cafe - Av Francisco de Miranda, Centro Comercial Parque Cristal, nivel C1, local 12, Caracas 1060, Distrito Capital, Venezuela",
+"Bodegón Daroy - Av. 1, Edificio monte ulio, Caracas 1060, Distrito Capital, Venezuela",
+};
+}
