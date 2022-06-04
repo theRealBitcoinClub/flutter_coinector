fd286a7e6f7f51243653d27f061777e0091246ee
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-04 15:36:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_4_17578292_am_1654371372740.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions17578292 { static final reviewedTitles17578292 = const {"Especias Market - F4JQ+7MF, Caracas 1080, Miranda, Venezuela",
+"Especias Market - F4JQ+7MF, Caracas 1080, Miranda, Venezuela",
+"El Rey de los Enrollados - Av Francisco de -Miranda, Centro comercial El Marques, piso 1 local 217, Caracas 1071, Miranda, Venezuela",
+"Toromaima mini market - San Bernardino, Caracas 1011, Capital District, Venezuela",
+"Monumental Tours C.A - Torre Banvenez, Pb local 3, Caracas 1050, Distrito Capital, Venezuela",
+};
+}
