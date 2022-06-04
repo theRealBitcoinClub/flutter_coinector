4b7fb6d5faf967bc9fe490e42d73b0db59168fb7
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-04 14:59:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_4_77130810_am_1654369141354.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions77130810 { static final reviewedTitles77130810 = const {"Global Systems Store C.A - Caracas 1060, Miranda, Venezuela",
+"Arte Express 99 - F5W3+JPV, Caracas 1060, Miranda, Venezuela",
+"American Book Shop 2026 - Centro Plaza, Nivel Jardín CC3-10, Caracas 1069, Distrito Capital, Venezuela",
+"Essor Group - local 31, nivel jardin, centro comercial centro plaza, Avenida Francisco de Miranda, Caracas 1060, Miranda, Venezuela",
+"La Red Ciber Cafe - Calle Enrique Eraso Torre La Noria, piso 1 (adentro de la ULAC, Caracas 1060, Distrito Capital, Venezuela",
+"Calle A - C. A, Caracas 1071, Miranda, Venezuela",
+};
+}
