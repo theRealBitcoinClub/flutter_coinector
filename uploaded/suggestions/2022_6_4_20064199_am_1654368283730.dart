09dfff73eea7363baddf12a3ad17c05e372cb117
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-04 14:44:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_4_20064199_am_1654368283730.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions20064199 { static final reviewedTitles20064199 = const {"Kiosco Andres Bello - 3ra av con, frente a la plaza de, 2da Transversal, Caracas 1060, Distrito Capital, Venezuela",
+"Mayor de Alimentos DayRan - Local 5, Transversal 3, entre calle los higuerotes, Avenida Sta Isabel, Caracas 1040, Distrito Capital, Venezuela",
+"Inversiones exótica doris - local 183, Centro Comercial Mercado San Jorge, pasillo i, nivel PB, Caracas 1041, Av. Bogotá con Avenida de Capitan de Navío- Felipe, El Cementerio, Caracas 1040, Caracas 1040, Distrito Capital, Venezuela",
+};
+}
