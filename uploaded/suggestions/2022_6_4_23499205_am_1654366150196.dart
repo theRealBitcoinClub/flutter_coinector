419a1f56d11179607a47add163d9353c5dc051e0
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-04 14:09:11 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_4_23499205_am_1654366150196.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions23499205 { static final reviewedTitles23499205 = const {"SimCardRental - Centro Ciudad Comercial Tamanaco nivel C1 Caracas, Caracas 1060, Distrito Capital, Venezuela",
+"HABITACOM - Av. Ppal de Las Mercedes, Edif. Centro Summum, con C. Mucuchíes, Caracas 1060, Miranda, Venezuela",
+"Sushi One - Soler 5837, PalermoC1425 CABA, Argentina",
+"Café Sushi One 2017, C.A - Calle Urdaneta, Edif. Urdaneta Piso PB. Local n° 2 Sector, Caracas 1060, Distrito Capital, Venezuela",
+"HABITACOM - Av. Ppal de Las Mercedes, Edif. Centro Summum, con C. Mucuchíes, Caracas 1060, Miranda, Venezuela",
+"Café Sushi One 2017, C.A - Calle Urdaneta, Edif. Urdaneta Piso PB. Local n° 2 Sector, Caracas 1060, Distrito Capital, Venezuela",
+};
+}
