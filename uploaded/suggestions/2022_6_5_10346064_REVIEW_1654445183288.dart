ebd7125f10c1bdc1f9a60b75865e984cb064b76e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-05 12:06:24 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_5_10346064_REVIEW_1654445183288.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions10346064 { static final reviewedTitles10346064 = const {"Diamond Republic - 45C3+CPG, St John&#39;s, Antigua and Barbuda",
+"The Tailor's Daughter - 45C3+5QG, Redcliffe St, St John&#39;s, Antigua and Barbuda",
+"Creole Antigua Tours - Jolly Harbour, Antigua and Barbuda",
+"Ticchio Italian Food and Wine - Sir George Walter Hwy, St John&#39;s, Antigua and Barbuda",
+};
+}
