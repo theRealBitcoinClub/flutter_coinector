91e14ead5141c55be62399acbaa3aa5dbf98f1f1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-05 03:07:20 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_5_18750603_REVIEW_1654412839257.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions18750603 { static final reviewedTitles18750603 = const {"Spirits of the Caribbean - St, 45C3+CFP, John&#39;s, Antigua and Barbuda",
+"Twist Fitness - Lower RedCliff Street, Twist Mall, 3rd floor, St John&#39;s, Antigua and Barbuda",
+"TWIST Mall - St John&#39;s, Antigua and Barbuda",
+"Kon Tiki Bar and Grill - 544W+9X8, Dickenson Bay St, St John&#39;s, Antigua and Barbuda",
+};
+}
