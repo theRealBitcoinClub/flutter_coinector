0434b76c7999b5c7296a20febe62c50954bc03d8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-05 02:25:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_5_46119275_REVIEW_1654410303460.dart
@@ -0,0 +1,15 @@
+class AutoSuggestions46119275 { static final reviewedTitles46119275 = const {"TWIST Mall - St John&#39;s, Antigua and Barbuda",
+"Learning Ladder Preschool - 5569+W68, Cedar Grove, Antigua and Barbuda",
+"Axxess Marine - Comercial centre, next to Marine, English Harbour, Antigua and Barbuda",
+"M & M Service Station - 45FF+VP5, Piggotts, Antigua and Barbuda",
+"North Coast Hardware - 45J7+JQG, Friars Hill Road, St John&#39;s, Antigua and Barbuda",
+"Fort Medieval - West Coast Village, Antigua and Barbuda",
+"Clean Food'N Jooce - Antigua Avenue, English Harbour, Antigua and Barbuda",
+"North Coast Hardware - 45J7+JQG, Friars Hill Road, St John&#39;s, Antigua and Barbuda",
+"West Coast Village - West Coast Village Valley Road, Antigua and Barbuda",
+"Sheer Rocks - Ffryes Beach, Valley Road, St Johns, Antigua and Barbuda",
+"Clean Food'N Jooce - Antigua Avenue, English Harbour, Antigua and Barbuda",
+"West Coast Village - West Coast Village Valley Road, Antigua and Barbuda",
+"Catherines Café - 265F+FFJ, Piccadilly, Antigua and Barbuda",
+};
+}
