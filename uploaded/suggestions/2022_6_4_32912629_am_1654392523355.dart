285a85e92bdc8f0d01bd06927b143fb790b19d4e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-04 21:28:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_4_32912629_am_1654392523355.dart
@@ -0,0 +1,11 @@
+class AutoSuggestions32912629 { static final reviewedTitles32912629 = const {"Farmacia Parque Cristal - F5X4+6JX, Caracas,Los Palos Grandes 1060, Miranda, Venezuela",
+"Papelería Moderna - La concordia, barrio la castra, entre carreras 10 y 10 bis con calle 2 bis y calle 3, detrás de comercial la gran parada, San Cristóbal 5001, Táchira, Venezuela",
+"Perfumería el poder de la fe - Local-29, zona 71, Mercado de, Caracas 1020, Distrito Capital, Venezuela",
+"Marvic Style - Local 88-89. Calle Chico Carrasquel, Nivel PB, Pasillo Paují, Caracas 1064, Miranda, Venezuela",
+"Marvic Style - Local 88-89. Calle Chico Carrasquel, Nivel PB, Pasillo Paují, Caracas 1064, Miranda, Venezuela",
+"Basket Burguer - Centro comercial Merpoeste, nivel feria, local 50, Caracas 1060, Distrito Capital, Venezuela",
+"Romy Shop - Centro Comercial Merpoeste, P-1, L-189, Caracas 1060, Distrito Capital, Venezuela",
+"Sucursal Irowo Omaire y Asociados - Av Andrés Bello Mercado municipal de Guaicaipuro Piso Mezzanine local 72 Urb, Caracas 1050, Distrito Capital, Venezuela",
+"Nemesis ladiosa - Avenida este, esquina Dr Paul a Salvador de león, Edificio 71 Nivel PB, local 10. La hoyada Caracas 1040, Avenida E, Caracas 1020, Caracas 1012, Distrito Capital, Venezuela",
+};
+}
