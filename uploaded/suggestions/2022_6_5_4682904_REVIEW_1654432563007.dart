b42cfc8d039924d7da382aeeb79d71e2f4713788
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-05 08:36:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_5_4682904_REVIEW_1654432563007.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions4682904 { static final reviewedTitles4682904 = const {"Steph & Vlada’s Studio - Commercial centre, Jolly Harbour, Antigua and Barbuda",
+"The Cool Pool - West Coast Village Valley Road, Antigua and Barbuda",
+};
+}
