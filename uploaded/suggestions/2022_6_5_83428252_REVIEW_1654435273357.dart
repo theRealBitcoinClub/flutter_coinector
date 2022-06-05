4f86837879e3b08ea6c80fd637fc56875b04c770
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-05 09:21:14 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_5_83428252_REVIEW_1654435273357.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions83428252 { static final reviewedTitles83428252 = const {"5 Senses Restaurant - Dockyard Drive, Antigua and Barbuda",
+"The Vineyard - Friars Hill Road Jasmine Court, Suite No. 20, Antigua and Barbuda",
+"Clean Food'N Jooce - Antigua Avenue, English Harbour, Antigua and Barbuda",
+"Club House - 266H+GPG, Falmouth, Antigua and Barbuda",
+};
+}
