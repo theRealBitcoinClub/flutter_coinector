27b82f52a303fa56a037e716c3cd3244d3b58598
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 02:02:23 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_20829846_REVIEW_1654581741591.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions20829846 { static final reviewedTitles20829846 = const {"TRIXEL - Carrera 51B #80-58 Oficina 1203, Barranquilla, Atlántico, 080020, Colombia",
+"Industria Metalmecanica Lozano - 1-29 Zona Industrial Comditaca Ureña, C. 17, Ureña 5048, Táchira, Venezuela",
+"COOPERATIVA CELESTINA - 5X3M+3R6, Valencia 2001, Carabobo, Venezuela",
+};
+}
