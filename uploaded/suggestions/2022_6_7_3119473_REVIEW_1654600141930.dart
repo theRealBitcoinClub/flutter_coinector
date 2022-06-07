4594fc9b441c4570026e69ea577b1a5a30b5599c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 07:09:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_3119473_REVIEW_1654600141930.dart
@@ -0,0 +1,9 @@
+class AutoSuggestions3119473 { static final reviewedTitles3119473 = const {"Porlamar - W4VF+WJV, Porlamar 6301, Nueva Esparta, Venezuela",
+"CEINB PEDRO ELIAS GUTIERREZ - MQ79+R5M, Barinas 5213, Barinas, Venezuela",
+"Lodisca Maracaibo - Calle 110A Entre Av 19 y 19A CASA 19A-33 Sector Cerro Pelado Maracaibo, Venezuela, Maracaibo 4002, Zulia, Venezuela",
+"Tecnopapeles Group, C.A - PRWG+JGC, Calle Bermúdez, Maturín 6201, Monagas, Venezuela",
+"Torni Hogar TM, C.A. - Calle Bella Vista con Callejón Santander, Sector Campiña, La, La Sabanita 8001, Bolívar, Venezuela",
+"Simón Bolívar University - Sartenejas, Caracas, Miranda, Venezuela",
+"Auto Repuestos Glenda, c.a. - Segunda Avenida entre calles 7 y 8 Local 1 Sector Cantarrana, San Felipe 3201, Yaracuy, Venezuela",
+};
+}
