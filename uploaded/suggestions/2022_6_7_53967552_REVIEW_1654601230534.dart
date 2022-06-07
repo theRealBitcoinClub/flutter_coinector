951195ed662e3be9c0339c1b77644920a9876c9a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 07:27:12 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_53967552_REVIEW_1654601230534.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions53967552 { static final reviewedTitles53967552 = const {"Representaciones y Variedades JV Núñez - Av Sesquicentenario, Valencia 2001, Carabobo, Venezuela",
+"University of Carabobo - 7XGV+V8P, Naguanagua 2005, Carabobo, Venezuela",
+"Corporacion GalaIt.c.a - Avenida Libertador, Residencias Lidomar Plaza, Calle Panama, Caracas 1010, Distrito Capital, Venezuela",
+"Servicio Repacell - Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Servicio Repacell - Blvr. de Sabana Grande, Caracas 1050, Distrito Capital, Venezuela",
+"Bodca Bodegón - C.C. Alto Prado, Av. Los Próceres, Mérida 5101, Mérida, Venezuela",
+"ITELSA Repuestos JJM, C.A. - Avenida Este 16, Entre Esquinas de Palmita A Tablita, Casa 18, Caracas 1010, Distrito Capital, Venezuela",
+"La Pulperia Marketplace - El Dátil, 6320, Nueva Esparta, Venezuela",
+};
+}
