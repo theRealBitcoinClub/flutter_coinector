c07853b40e89fc4ffc0d8d19d50b9f933657ff06
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 17:02:17 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_40423630_REVIEW_1654635734919.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions40423630 { static final reviewedTitles40423630 = const {"Restaurant The Place - Strada Cetății 3a, Florești 407280, Romania",
+"German Market - Piața Mihai Viteazu nr. 17, Cluj-Napoca 400151, Romania",
+"Theta - Strada Iuliu Maniu 40/B, Satu Mare 440115, Romania",
+"Il Passo - Strada Alexandru Vaida Voevod nr. 53-55, Cluj-Napoca 400394, Romania",
+};
+}
