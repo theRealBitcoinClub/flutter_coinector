53525eb732267006b8f6c63c31626f76ea942a3f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 15:57:59 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_61129181_REVIEW_1654631877039.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions61129181 { static final reviewedTitles61129181 = const {"Krusty Krab Burgers - Bernolákova, 031 01 Liptovský Mikuláš, Slovakia",
+"Sedačky Phase - Trenčín - Belá oproti OC, NC Laugaricio, 911 01 Trenčín, Slovakia",
+"MM taxi a preprava - Slovenská 1538/10, 085 01 Bardejov, Slovakia",
+};
+}
