7920c9359b4e866f907f57d8c46d08f901688e2c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 09:23:15 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_40383361_REVIEW_1654608193580.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions40383361 { static final reviewedTitles40383361 = const {"Beluga House - Calle California, Caracas 1060, Miranda, Venezuela",
+"Grupo Zoom - Final Av. Principal, Qta. Grupo Zoom, Bello Campo, Caracas, F4RX+8HQ, Caracas 1060, Miranda, Venezuela",
+"Panadería Y Pastelería Perimetral 2013 C.A. - MQF5+35M, Carúpano 6150, Sucre, Venezuela",
+"Mookah Restaurant & Lounge - Av. Intercomunal La Trinidad - El Hatillo C.C. Trinidad Tepuy Caracas, Caracas 1081, Miranda, Venezuela",
+"Frenos y Silenciadores Maxituning C.A. - entre calles 30 y 31, 30-45 Av. Don Tulio Febres Cordero, Mérida 5101, Mérida, Venezuela",
+"Bodca Bodegón - C.C. Alto Prado, Av. Los Próceres, Mérida 5101, Mérida, Venezuela",
+"MT MARKETING - Calle Jesús María Patiño con Calle Ortega, Local 7. Porlamar, Isla de Margarita., Calle Ortega, Porlamar 6301, Nueva Esparta, Venezuela",
+"Algo Rico. Inversiones Jorge Madriz F.P - Calle Paez a una cuadra del Bco de Vzla, Tinaquillo 2209, Cojedes, Venezuela",
+};
+}
