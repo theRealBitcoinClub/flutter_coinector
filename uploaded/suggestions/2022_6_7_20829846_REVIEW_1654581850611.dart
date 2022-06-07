e7ee7a0a610d1694459015662c158f2fdf093a22
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 02:04:12 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_20829846_REVIEW_1654581850611.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions20829846 { static final reviewedTitles20829846 = const {"TRIXEL - Carrera 51B #80-58 Oficina 1203, Barranquilla, Atlántico, 080020, Colombia",
+"Industria Metalmecanica Lozano - 1-29 Zona Industrial Comditaca Ureña, C. 17, Ureña 5048, Táchira, Venezuela",
+"COOPERATIVA CELESTINA - 5X3M+3R6, Valencia 2001, Carabobo, Venezuela",
+"FUTURE TECHNOLOGY SAS - CR 29, Calle 81 Sur #N 40, Bogotá, Cundinamarca, 110111, Colombia",
+"Labcooper Servicio Técnico de celulares IPhone - Carrera 59 #130-28, Suba, Bogotá, 111111, Colombia",
+};
+}
