393cc5aca966194d909e4afd0e46b8b01fb09f0f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-06 22:38:50 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_6_28979417_REVIEW_1654569528583.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions28979417 { static final reviewedTitles28979417 = const {"Todo Torta - Juan Maria Lheritier 539, San Carlos Centro, Santa Fe, Argentina",
+"Kiosco - Av. Salvador María del Carril 2706, C1419 GZT, Buenos Aires, Argentina",
+"Good Sas - Carrera 35 #59 45, Bogotá, 111321, Colombia",
+"Cyberlink - Buenos Aires 1631, 3300, N3301IJH Posadas, Misiones, Argentina",
+"Daniel Morales Automotores - -Canelones, 91600 San Jacinto Departamento de Canelones, Uruguay",
+"Embassy of Argentina - Carrera 11B #97 56 Piso 4. Edificio, Ápice, Bogotá, 110221, Colombia",
+};
+}
