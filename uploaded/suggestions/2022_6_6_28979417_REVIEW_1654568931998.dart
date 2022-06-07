306cd266aa01254c206de3ff35f4d574128909d6
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-06 22:28:54 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_6_28979417_REVIEW_1654568931998.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions28979417 { static final reviewedTitles28979417 = const {"Todo Torta - Juan Maria Lheritier 539, San Carlos Centro, Santa Fe, Argentina",
+"Kiosco - Av. Salvador María del Carril 2706, C1419 GZT, Buenos Aires, Argentina",
+};
+}
