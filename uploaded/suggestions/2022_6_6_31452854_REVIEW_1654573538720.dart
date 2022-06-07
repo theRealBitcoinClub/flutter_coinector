f34fe31e5fb58e2b4c07242903f595b58f568afb
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-06 23:45:40 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_6_31452854_REVIEW_1654573538720.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions31452854 { static final reviewedTitles31452854 = const {"LA CASA DEL CEVICHE - Calle 52 #43 - 32, Medellín, La Candelaria, Medellín, Antioquia, 050012, Colombia",
+"C&D Company - Avenida Calle 32 ## 6A - 15, Bogotá, 110311, Colombia",
+"Stg Group Sas - Calle 24 #8 23, Cali, Valle del Cauca, 760044, Colombia",
+"Droguería Salud & Éxito - Carrera 73, Cali, Valle del Cauca, 760035, Colombia",
+"Yakara - Calle 72 Sur #80m-21, Bogotá, 110731, Colombia",
+"FABRICA DE ALITAS - Carrera 5 ## 70a-90, Bogotá, Cundinamarca, 110231, Colombia",
+};
+}
