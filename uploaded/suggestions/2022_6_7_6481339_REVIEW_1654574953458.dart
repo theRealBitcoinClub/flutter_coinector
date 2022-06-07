e45648ccbd709ccae36f7eb8071a9491d5592bb4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 00:09:15 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_6481339_REVIEW_1654574953458.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions6481339 { static final reviewedTitles6481339 = const {"PandaBTM - Avenida Carrera 7 #66 34 local 4, Bogotá, 110231, Colombia",
+"Hotel Elimar - a 12-85, Calle 28 ##12-1, Girardot, Cundinamarca, 252432, Colombia",
+"Servicios Odontológicos Diseño e Imágen - Calle 63 ## 13 - 12, Bogotá, 110231, Colombia",
+"IKI SUSHI & POKE - Carrera 29 #47 - 14, Bucaramanga, Santander, 680003, Colombia",
+"Kinean Worldwide - Bogotá, Bogota, 111611, Colombia",
+};
+}
