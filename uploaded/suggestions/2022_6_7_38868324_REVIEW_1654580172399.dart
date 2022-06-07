5afb46e62b6586cf00970dbb0b9c08177f517e05
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 01:36:14 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_38868324_REVIEW_1654580172399.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions38868324 { static final reviewedTitles38868324 = const {"Zipaquirá Empresas - calle 10 No 1B - 57, Zipaquirá, Cundinamarca, 250252, Colombia",
+"fisioesteticalalibertad - calle 22# 12 -- 40, la libertad, Cúcuta, Norte de Santander, 540005, Colombia",
+"Dido Veterinaria - AV PRADILLA # 1B -21, Chía, Cundinamarca, 250001, Colombia",
+};
+}
