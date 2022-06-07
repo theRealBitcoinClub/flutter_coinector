81de527fc0f0fe7c12a62b7c9d2ae06f485451ae
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-06 23:33:36 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_6_31452854_REVIEW_1654572814799.dart
@@ -0,0 +1,3 @@
+class AutoSuggestions31452854 { static final reviewedTitles31452854 = const {"LA CASA DEL CEVICHE - Calle 52 #43 - 32, Medellín, La Candelaria, Medellín, Antioquia, 050012, Colombia",
+};
+}
