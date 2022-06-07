0d703bdfb4ab97366c7a1c6db224a6791413e5d3
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 00:28:06 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_10035640_REVIEW_1654576084756.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions10035640 { static final reviewedTitles10035640 = const {"Galileo Restaurante Bar - Avenida 3 Este ## 14A-25, Cúcuta, Norte de Santander, 540003, Colombia",
+"Panda Express - Local 274, Cra. 49 #52-107, La Candelaria, Medellín, Antioquia, 050012, Colombia",
+"PandaBTM - Avenida Carrera 7 #66 34 local 4, Bogotá, 110231, Colombia",
+};
+}
