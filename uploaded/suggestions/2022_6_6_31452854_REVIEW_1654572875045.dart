2fa8ee1ae578dc85a9859a16802568b9ef0095e8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-06 23:34:37 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_6_31452854_REVIEW_1654572875045.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions31452854 { static final reviewedTitles31452854 = const {"LA CASA DEL CEVICHE - Calle 52 #43 - 32, Medellín, La Candelaria, Medellín, Antioquia, 050012, Colombia",
+"C&D Company - Avenida Calle 32 ## 6A - 15, Bogotá, 110311, Colombia",
+};
+}
