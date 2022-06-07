febb5ba0958b491553f328cf6df9a6a4df53c8c4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-06 22:56:48 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_6_28979417_REVIEW_1654570606258.dart
@@ -0,0 +1,17 @@
+class AutoSuggestions28979417 { static final reviewedTitles28979417 = const {"Todo Torta - Juan Maria Lheritier 539, San Carlos Centro, Santa Fe, Argentina",
+"Kiosco - Av. Salvador María del Carril 2706, C1419 GZT, Buenos Aires, Argentina",
+"Good Sas - Carrera 35 #59 45, Bogotá, 111321, Colombia",
+"Cyberlink - Buenos Aires 1631, 3300, N3301IJH Posadas, Misiones, Argentina",
+"Daniel Morales Automotores - -Canelones, 91600 San Jacinto Departamento de Canelones, Uruguay",
+"Embassy of Argentina - Carrera 11B #97 56 Piso 4. Edificio, Ápice, Bogotá, 110221, Colombia",
+"Athena Holding S.R.L. - Via Publio Virginio Marone, 13, 10024 Moncalieri TO, Italy",
+"barucshop - Calle 51 #73-134 Local 3640, Medellín, Laureles, Medellín, Antioquia, 050034, Colombia",
+"Matisse Hotel - Calle 67 #6-55, Bogotá, Cundinamarca, 110231, Colombia",
+"TAG SAS Internet Satelital - Carrera 1f #No. 40-195 OFICINA 611, Tunja, Boyacá, 150001, Colombia",
+"Doctor Ricardo De Lima - Megacentro Pinares. Torre 2. Consultorio 510, Calle 18 #12-75, Pereira, Risaralda, 660003, Colombia",
+"AVACOL AVALUOS DE COLOMBIA SAS - # 94a-61, Carrera 14 #, Oficina 205, Bogotá, 110221, Colombia",
+"Sala de Ventas Cerámica Italia S.A - Calle 15 ##2N-134, Cúcuta, Norte de Santander, 540005, Colombia",
+"Rodríguez & León - Whymper N27-70 y, Av. Francisco de Orellana, Quito 170517, Ecuador",
+"teknocome - carrera 55c#159 #10 int 11, Bogotá, 111156, Colombia",
+};
+}
