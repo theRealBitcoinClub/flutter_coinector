0937bf830e5e90cd4c8ffe0231eef36eb9a8c9ff
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 16:58:33 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_7196948_REVIEW_1654721911108.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions7196948 { static final reviewedTitles7196948 = const {"Moto mondo - Hanuševa ul., 10000, Zagreb, Croatia",
+"Caffe Bar Iskra - Ul. Šime Ljubića 12, 51000, Rijeka, Croatia",
+"Forum - Hosti 43a, 51000, Rijeka, Croatia",
+"Moto mondo - Hanuševa ul., 10000, Zagreb, Croatia",
+};
+}
