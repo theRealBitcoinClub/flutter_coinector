01adc8d832031e0898aa2255eb24681fefcccc60
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 18:05:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_53648309_REVIEW_1654725925988.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions53648309 { static final reviewedTitles53648309 = const {"Alo Alo 12 - Ul. Josipa Jovića 93, 21000, Split, Croatia",
+"Konoba Blato - Titov trg 8C, 51000, Rijeka, Croatia",
+"PO-MARK D.O.O. - Ul. Gustava Krkleca 40, 10000, Zagreb, Croatia",
+"Art Nails - Trg Vladimira Nazora, 10310, Ivanić Grad, Croatia",
+"VOYAGER TRAVEL AGENCY - Dolac 3/c, 51000, Rijeka, Croatia",
+"SPOT shopping park - Trogirska cesta 6, 21220, Plano, Croatia",
+"Pub "Bačva" - Dolac 8, 51000, Rijeka, Croatia",
+"Good Gate video produkcija - Obrovacka 16 Veliko Polje, 10010, Zagreb, Croatia",
+};
+}
