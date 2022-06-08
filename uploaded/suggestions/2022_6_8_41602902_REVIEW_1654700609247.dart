56a1cab80c8c9c5f27c9c8786a645ab3bd85c5f0
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 11:03:32 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_41602902_REVIEW_1654700609247.dart
@@ -0,0 +1,14 @@
+class AutoSuggestions41602902 { static final reviewedTitles41602902 = const {"BitLucky d.o.o. - 1st, floor, Riva 8, 51000, Rijeka, Croatia",
+"Rakhia bar - Ul. Andrije Medulića 5, 51000, Rijeka, Croatia",
+"Master’s Barber Shop - Ul. Andrije Medulića 6, 51000, Rijeka, Croatia",
+"DNK Garaža - Ul. Slavka Cindrića 9, 51000, Rijeka, Croatia",
+"Tools Milic Ltd. - Branch 2nd Split - Velebitska ul. 9, 21000, Split, Croatia",
+"Tools Milic Ltd. - Branch 2nd Split - Velebitska ul. 9, 21000, Split, Croatia",
+"Tools Milic - Branch first Malesnica - Ul. Mirka Viriusa 1, 10090, Zagreb, Croatia",
+"Tunel Bar - Školjić 12, 51000, Rijeka, Croatia",
+"Beach Bar "PHANAS" - Ul. Stara Voda bb, 51221, Paveki, Croatia",
+"Phanas Pub Rijeka est.2001 - Ul. Ivana Zajca 9, 51000, Rijeka, Croatia",
+"Khao Sun - Malešnica 14, 10000, Zagreb, Croatia",
+"Pet Jedan Tapas Bar - Riva 14A, 51000, Rijeka, Croatia",
+};
+}
