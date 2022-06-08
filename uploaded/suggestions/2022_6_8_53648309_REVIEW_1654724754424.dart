e02f580ca69f38beeaaea9b10c5b3d30c1548228
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 17:45:57 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_53648309_REVIEW_1654724754424.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions53648309 { static final reviewedTitles53648309 = const {"Alo Alo 12 - Ul. Josipa Jovića 93, 21000, Split, Croatia",
+"Konoba Blato - Titov trg 8C, 51000, Rijeka, Croatia",
+};
+}
