1459b73ffa43efa75d2ddacc1c9aafc70b4c81c8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 07:22:30 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_99991565_REVIEW_1654687347417.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions99991565 { static final reviewedTitles99991565 = const {"Heyday Authentic Store - R. da Irmandade 15, 2640-022 Santo Isidoro, Portugal",
+"Atlas-Agencia De Viagens E Turismo (Braga), Lda. - Praça Conde de Agrolongo 127, 4700-317 Braga, Portugal",
+"Explorer Investments SCR SA - Av. Eng. Duarte Pacheco 7 7A, 1070-100 Lisboa, Portugal",
+"EPICUR - Wine Boutique & Food - R. Alexandre Herculano 22A, 8000-204 Faro, Portugal",
+"Vegana By Tentúgal - R. de Santo Ildefonso 412 4300, 4300 Porto, Portugal",
+"TOPTOUR PORTUGAL - Azinhaga da Torre do Fato 33B, 1600-297 Lisboa, Portugal",
+"Átrio da Cidade - Av. 5 de Outubro 23A, 2560-270 Torres Vedras, Portugal",
+"Paviljoen ...aan het Water - Brielselaan 157, 3081 AC Rotterdam, Netherlands",
+};
+}
