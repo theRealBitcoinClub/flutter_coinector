140d4bbef8208083a1b165ac0368af813af8b179
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-07 22:13:46 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_7_84063047_REVIEW_1654654423458.dart
@@ -0,0 +1,12 @@
+class AutoSuggestions84063047 { static final reviewedTitles84063047 = const {"FISCAL ONLINE - Case Marcat Oradea - Aparatura Fiscala - Strada Independenței 11, Oradea 410065, Romania",
+"Csermobil GSM - Strada Gábor Áron 2, Vlăhița 535800, Romania",
+"NN FISCALPRINT SB S.R.L. - etaj 1, Str. Autogării 16, Sibiu 550135, Romania",
+"Taxi Albertini Galati - Str. AUREL VLAICU nr. 7 Corp C2, Et.1, Galați 800508, Romania",
+"Padrino Suceava - Strada Universității 15, Suceava 720225, Romania",
+"Auto Dany Drive - Strada Popa Stoica din Farcas 72, București, Romania",
+"Il Passo - Strada Republicii nr. 25, Brașov 500030, Romania",
+"Elcom Internaţional S.A. - Strada Crinilor 2, Bistrița, Romania",
+"Datacon Solutions - Strada Câmpului 91, Cluj-Napoca 400656, Romania",
+"Lounge Pub - Strada Ștefan cel Mare 22, Suceava 720005, Romania",
+};
+}
