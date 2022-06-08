34ba8179f3e6c0aa6b1e7c0252145fc5bf019663
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 07:16:41 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_99991565_REVIEW_1654686998772.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions99991565 { static final reviewedTitles99991565 = const {"Heyday Authentic Store - R. da Irmandade 15, 2640-022 Santo Isidoro, Portugal",
+"Atlas-Agencia De Viagens E Turismo (Braga), Lda. - Praça Conde de Agrolongo 127, 4700-317 Braga, Portugal",
+"Explorer Investments SCR SA - Av. Eng. Duarte Pacheco 7 7A, 1070-100 Lisboa, Portugal",
+};
+}
