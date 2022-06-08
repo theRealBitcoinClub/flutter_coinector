d89e4d35bd374cd27447b048cff97a12693c253c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 18:29:52 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_15659063_REVIEW_1654727389139.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions15659063 { static final reviewedTitles15659063 = const {"Mare art poklon galerija - Ul. Andrije Medulića 1C, 51000, Rijeka, Croatia",
+"Mare art poklon galerija - Ul. Andrije Medulića 1C, 51000, Rijeka, Croatia",
+"FOTO KURTI - Užarska ul. 20, 51000, Rijeka, Croatia",
+"Caffe-Bar "Corso" - Andrije Palmovića 27, 48312, Rasinja, Croatia",
+"Smiljana - Titov trg 12, 51000, Rijeka, Croatia",
+"McDonald's Rijeka - Trg 111 Brigade Hrvatske Vojske 1, 51000, Rijeka, Croatia",
+};
+}
