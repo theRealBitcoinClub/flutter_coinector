6ff7a6968f5a43de969b5c476b9064717ff0b897
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 18:24:20 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_15659063_REVIEW_1654727057212.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions15659063 { static final reviewedTitles15659063 = const {"Mare art poklon galerija - Ul. Andrije Medulića 1C, 51000, Rijeka, Croatia",
+"Mare art poklon galerija - Ul. Andrije Medulića 1C, 51000, Rijeka, Croatia",
+};
+}
