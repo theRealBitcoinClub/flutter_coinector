c32d9e4fa3f52543fbd847b61cd5595589d05d37
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 10:43:23 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_41602902_REVIEW_1654699400300.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions41602902 { static final reviewedTitles41602902 = const {"BitLucky d.o.o. - 1st, floor, Riva 8, 51000, Rijeka, Croatia",
+"Rakhia bar - Ul. Andrije Medulića 5, 51000, Rijeka, Croatia",
+"Master’s Barber Shop - Ul. Andrije Medulića 6, 51000, Rijeka, Croatia",
+};
+}
