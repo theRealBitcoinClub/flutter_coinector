6de9a04e7ed8d624af5067da43786bb28c24774a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 16:25:49 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_32812917_REVIEW_1654719946895.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions32812917 { static final reviewedTitles32812917 = const {"KARIĆ AUTOMOBILI - Rakovac ul. 60, 47000, Karlovac, Croatia",
+"Art Nails - Trg Vladimira Nazora, 10310, Ivanić Grad, Croatia",
+"Taxi Rijeka - VIP Car Service Rijeka - Trg kralja Tomislava 2, 51000, Rijeka, Croatia",
+"Youth Club Rijeka - Ul. Erazma Barčića 9A, 51000, Rijeka, Croatia",
+"MPS Hotnja doo, P5 - Volavje 29B, 10450, Volavje, Croatia",
+};
+}
