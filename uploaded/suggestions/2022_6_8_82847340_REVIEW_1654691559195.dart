5f85dfdf5f4500d7583ba5c5f83da5b481da5a6e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 08:32:41 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_82847340_REVIEW_1654691559195.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions82847340 { static final reviewedTitles82847340 = const {"Supermajice.hr - Šetalište Andrije Kačića Miošića 8A, 51000, Rijeka, Croatia",
+"Na Kantunu Tavern - Wenzelova ul. 4, 51000, Rijeka, Croatia",
+"Rijeka Clinical Hospital Center Sušak Locality - Ul. Tome Strižića 3, 51000, Rijeka, Croatia",
+};
+}
