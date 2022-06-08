9284c7fb52dc02d9294d13cee11c56dfbc063afe
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 16:14:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_19451704_REVIEW_1654719281860.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions19451704 { static final reviewedTitles19451704 = const {"Karić Automobili d.o.o. - Ul. Andrije Žaje 5, 10000, Zagreb, Croatia",
+"BAČANI ŽELJKO - Jablanska ul. 2, 10000, Zagreb, Croatia",
+"Prima 4 rooms - Verdijeva ul. 17, 51000, Rijeka, Croatia",
+};
+}
