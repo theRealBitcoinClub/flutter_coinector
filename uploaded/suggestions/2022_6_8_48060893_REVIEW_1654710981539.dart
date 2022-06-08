c8f42328c2af54d5839f409589a39b0b7700f3d4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 13:56:24 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_48060893_REVIEW_1654710981539.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions48060893 { static final reviewedTitles48060893 = const {"Big Blue Sport - Podan Glavice 2, 21420, Bol, Croatia",
+"Big Blue Cafe - Ul. Bolskih Pomoraca 15, 21420, Bol, Croatia",
+"Big Blue Bike&Beach Shop - Put Zlatnog Rata 46, 21420, Bol, Croatia",
+"OPG Grancigula - Rakovo Selo 67, 22000, Dubrava kod Šibenika, Croatia",
+};
+}
