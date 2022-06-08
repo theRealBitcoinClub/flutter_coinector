576eaa1fa771b24b405f03de765f2c462d973a30
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 17:48:26 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_53648309_REVIEW_1654724903716.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions53648309 { static final reviewedTitles53648309 = const {"Alo Alo 12 - Ul. Josipa Jovića 93, 21000, Split, Croatia",
+"Konoba Blato - Titov trg 8C, 51000, Rijeka, Croatia",
+"PO-MARK D.O.O. - Ul. Gustava Krkleca 40, 10000, Zagreb, Croatia",
+"Art Nails - Trg Vladimira Nazora, 10310, Ivanić Grad, Croatia",
+};
+}
