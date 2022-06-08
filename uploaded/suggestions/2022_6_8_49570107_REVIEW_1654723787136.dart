2e302d3952ecccd256fc0da169a708ecc636adce
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 17:29:50 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_49570107_REVIEW_1654723787136.dart
@@ -0,0 +1,16 @@
+class AutoSuggestions49570107 { static final reviewedTitles49570107 = const {"CHOCOLAND - Ul. Andrije Medulića 8, 51000, Rijeka, Croatia",
+"Big Box d.o.o. - Kumičićeva ul. 62A, 51000, Rijeka, Croatia",
+"Octopus Piercing & Tattoo Studio - prilaz Gjure Deželića 23, 10000, Zagreb, Croatia",
+"Slastičarnica "Slon by Moreno Debartoli" - Grivica ul. 4, 51000, Rijeka, Croatia",
+"Kaokakao - Ul. Andrije Štangera 44, 51410, Opatija, Croatia",
+"CAFFE BAR 2020 - Ul. Nikole Tesle 1A, 51000, Rijeka, Croatia",
+"Taxi transfers Croatia - Airport Taxi Sevices - Call Taxi Zagreb - Ul. Barutanski jarak 22/1, 10000, Zagreb, Croatia",
+"Muzej čokolade Zagreb (Chocolate Museum Zagreb) - Varšavska ul. 5, 10000, Zagreb, Croatia",
+"Konoba Tarsa - J. Kulfaneka, Strmica 10, 51000, Rijeka, Croatia",
+"Tiskara SUŠAK - Ul. Milutina Barača 54, 51000, Rijeka, Croatia",
+"Harat's Pub - Ul. pod Kaštelom 9, 51000, Rijeka, Croatia",
+"DVD OSIJEK TENJA - Ul. Vladka Mačeka 18, 31207, Tenja, Croatia",
+"Alo Alo 10 - Vukovarska ul. 207, 21000, Split, Croatia",
+"Alo Alo 3 - Ul. kralja Zvonimira 21, 22300, Knin, Croatia",
+};
+}
