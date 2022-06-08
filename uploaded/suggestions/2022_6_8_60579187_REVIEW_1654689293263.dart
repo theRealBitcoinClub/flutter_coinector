866714095a93bc7c09390716c9fe5ddbf6461c5d
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 07:54:55 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_60579187_REVIEW_1654689293263.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions60579187 { static final reviewedTitles60579187 = const {"Tools Milic Ltd. - Branch 3rd Zabok - Ul. Matije Gupca 115, 49000, Zabok, Croatia",
+"Konoba Ribica - Pulska ul. 30, 51000, Rijeka, Croatia",
+};
+}
