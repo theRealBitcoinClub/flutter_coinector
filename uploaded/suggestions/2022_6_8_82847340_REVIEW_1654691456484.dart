95ff39cb75dea786a56e4dd54e0b7d1f447f52a9
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 08:30:59 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_82847340_REVIEW_1654691456484.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions82847340 { static final reviewedTitles82847340 = const {"Supermajice.hr - Šetalište Andrije Kačića Miošića 8A, 51000, Rijeka, Croatia",
+"Na Kantunu Tavern - Wenzelova ul. 4, 51000, Rijeka, Croatia",
+};
+}
