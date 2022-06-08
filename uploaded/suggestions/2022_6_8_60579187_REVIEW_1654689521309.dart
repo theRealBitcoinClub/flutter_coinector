e32d6205c7553ee8db58ef562c1d4f3a2924cff4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-08 07:58:43 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_8_60579187_REVIEW_1654689521309.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions60579187 { static final reviewedTitles60579187 = const {"Tools Milic Ltd. - Branch 3rd Zabok - Ul. Matije Gupca 115, 49000, Zabok, Croatia",
+"Konoba Ribica - Pulska ul. 30, 51000, Rijeka, Croatia",
+"Bistro oštarija Fortica - Trg Matka Laginje 1, 51215, Kastav, Croatia",
+"Khao Sun - Ul. Slaviše Vajnera Čiče 11, 51000, Rijeka, Croatia",
+};
+}
