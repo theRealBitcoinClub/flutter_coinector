be5bd178d39ca92f06894e5f8fd44c0cd169fa82
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-06-08 20:49:18 -0400

    c

--- uploaded/suggestions/2022_6_8_26468718_REVIEW_1654721479579.dart
+++
@@ -1,5 +0,0 @@
-class AutoSuggestions26468718 { static final reviewedTitles26468718 = const {"PriMarius craft pivovara d.o.o. - Radnička cesta 37, 10000, Zagreb, Croatia",
-"PriMarius craft pivovara d.o.o. - Radnička cesta 37, 10000, Zagreb, Croatia",
-"Fan Shop HNK Rijeka - Ul. Pavla Rittera Vitezovića 1, 51000, Rijeka, Croatia",
-};
-}
--- uploaded/suggestions/2022_6_8_7196948_REVIEW_1654722045605.dart
+++
@@ -1,8 +0,0 @@
-class AutoSuggestions7196948 { static final reviewedTitles7196948 = const {"Moto mondo - Hanuševa ul., 10000, Zagreb, Croatia",
-"Caffe Bar Iskra - Ul. Šime Ljubića 12, 51000, Rijeka, Croatia",
-"Forum - Hosti 43a, 51000, Rijeka, Croatia",
-"Moto mondo - Hanuševa ul., 10000, Zagreb, Croatia",
-"Infinity Caffe & Night Bar - Vozišće 23, 51216, Viškovo, Croatia",
-"Caffe bar "SAMOVAR" - Trg Matije V. Flaciusa 2, 51000, Rijeka, Croatia",
-};
-}
