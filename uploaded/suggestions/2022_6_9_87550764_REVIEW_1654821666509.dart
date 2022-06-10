0e46b43254e5fdb992ac2732c923ca4bbe0e21b4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-09 20:41:10 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_9_87550764_REVIEW_1654821666509.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions87550764 { static final reviewedTitles87550764 = const {"Mladen Burec - Trg Stjepana Konzula 1, 10000, Zagreb, Croatia",
+"Tikvarici - Pakoštanska ul. 5, 10000, Zagreb, Croatia",
+"Municipium - Trg Riječke rezolucije 5, 51000, Rijeka, Croatia",
+"Blato 1902 - Ul. Vinka Benca 6, 51000, Rijeka, Croatia",
+};
+}
