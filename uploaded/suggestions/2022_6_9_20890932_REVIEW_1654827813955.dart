392d2932ad9297c3ac858995af15847c49689977
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-09 22:23:37 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_9_20890932_REVIEW_1654827813955.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions20890932 { static final reviewedTitles20890932 = const {"ShoeBeDo - City Center One, Vukovarska ul. 207, 21000, Split, Croatia",
+"Skechers - ZTC Rijeka, Zvonimirova ul. 3, 51000, Rijeka, Croatia",
+};
+}
