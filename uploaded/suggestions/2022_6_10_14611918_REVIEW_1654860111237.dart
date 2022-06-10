6f6339ab6b6d0d19a4a2820c27bbd4962dd4a05f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 07:21:54 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_14611918_REVIEW_1654860111237.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions14611918 { static final reviewedTitles14611918 = const {"Store - Shopping center Cvjetni, floor -1, Trg Petra Preradovića 4-6, 10000, Zagreb, Croatia",
+"Staklarstvo - Ul. Ivana Fistrovića, 44000, Sisak, Croatia",
+"Up Samobor - Giznik ul. 2, 10430, Samobor, Croatia",
+"Optika Mint - Krčelićeva ul. 27, 10000, Zagreb, Croatia",
+"Grand Auto Jankomir (Ford i Hyundai – prodaja i servis; Infiniti - servis) - Ljubljanska avenija 4, 10090, Zagreb, Croatia",
+};
+}
