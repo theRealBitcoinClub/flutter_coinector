3c19ca4cc5cdb29ca5b9b8ad2ed569bcf85e5782
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 07:19:41 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_14611918_REVIEW_1654859977630.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions14611918 { static final reviewedTitles14611918 = const {"Store - Shopping center Cvjetni, floor -1, Trg Petra Preradovića 4-6, 10000, Zagreb, Croatia",
+"Staklarstvo - Ul. Ivana Fistrovića, 44000, Sisak, Croatia",
+};
+}
