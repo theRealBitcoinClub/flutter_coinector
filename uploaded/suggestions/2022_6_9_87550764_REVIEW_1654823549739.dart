e1ce4faee1c7d3efee1092de0f47e4f4f8d04c6d
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-09 21:12:33 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_9_87550764_REVIEW_1654823549739.dart
@@ -0,0 +1,13 @@
+class AutoSuggestions87550764 { static final reviewedTitles87550764 = const {"Mladen Burec - Trg Stjepana Konzula 1, 10000, Zagreb, Croatia",
+"Tikvarici - Pakoštanska ul. 5, 10000, Zagreb, Croatia",
+"Municipium - Trg Riječke rezolucije 5, 51000, Rijeka, Croatia",
+"Blato 1902 - Ul. Vinka Benca 6, 51000, Rijeka, Croatia",
+"Auto centar MMS d.o.o. - Ul. Ante Starčevića 84, 34551, Lipik, Croatia",
+"Sally Atelier - Užarska ul. 17, 51000, Rijeka, Croatia",
+"Sally Atelier - Užarska ul. 17, 51000, Rijeka, Croatia",
+"SUN&SPA Wellness Baška - Ul. Emila Geistlicha 39, 51523, Baška, Croatia",
+"Skechers - LUMINI CENTAR, Ul. Grada Lipika 15, 42204, Donji Kneginec, Croatia",
+"CENTRO PLIN SAM D.O.O. - Pavlova ul. 4, 10430, Hrastina Samoborska, Croatia",
+""E-REVOLUCIJA" Vape Shop el. Cigarete i tekućine -Geekvape, Vaporesso, L&A Vape, Voopoo - Maksimirska cesta 118, 10000, Zagreb, Croatia",
+};
+}
