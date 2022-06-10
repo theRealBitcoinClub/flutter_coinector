5ceb5e6fe6251634be60f584d55dd78747511201
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 00:05:16 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_28909562_REVIEW_1654833912582.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions28909562 { static final reviewedTitles28909562 = const {"Skechers - Ulica Stoja 14A Max City, 1. kat, 52100, Pula, Croatia",
+"ShoeBeDo - Oporovečka Ul. 14, 10040, Zagreb, Croatia",
+};
+}
