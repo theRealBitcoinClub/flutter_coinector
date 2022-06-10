2225f6000ae74b9aa64133ec288d27a834d74f44
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-09 20:42:08 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_9_87550764_REVIEW_1654821725262.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions87550764 { static final reviewedTitles87550764 = const {"Mladen Burec - Trg Stjepana Konzula 1, 10000, Zagreb, Croatia",
+"Tikvarici - Pakoštanska ul. 5, 10000, Zagreb, Croatia",
+"Municipium - Trg Riječke rezolucije 5, 51000, Rijeka, Croatia",
+"Blato 1902 - Ul. Vinka Benca 6, 51000, Rijeka, Croatia",
+"Auto centar MMS d.o.o. - Ul. Ante Starčevića 84, 34551, Lipik, Croatia",
+};
+}
