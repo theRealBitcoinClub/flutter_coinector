0b706191e603c8e90170ade54c146291f6e5d302
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-11 17:21:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_11_12788981_REVIEW_1654982484286.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions12788981 { static final reviewedTitles12788981 = const {"Mauri Concept - Via Gerolamo Vegezzi 4, 6900 Lugano, Switzerland",
+"Dagorà - Lifestyle Innovation Hub - Galleria 1, Via Cantonale, 6928 Manno, Switzerland",
+};
+}
