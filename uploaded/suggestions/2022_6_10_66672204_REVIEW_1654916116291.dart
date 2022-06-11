f2f2cfeb9149cb1fae8db615f62831228d4db5cd
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 22:55:20 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_66672204_REVIEW_1654916116291.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions66672204 { static final reviewedTitles66672204 = const {"Yazaki Bulgaria EOOD - бул. Европа 43, 8600 Western Industrial Zone, Yambol, Bulgaria",
+"Museum of Illusions Sofia - бул. княгиня Мария Луиза 16, 1000 Old City Center, Sofia, Bulgaria",
+"Smerch - ul. Han Kubrat 4, 8600 g.k. General Vladimir Zaimov, Yambol, Bulgaria",
+};
+}
