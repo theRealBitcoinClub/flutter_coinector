f756fc9621b0d134e5d123eb3a4cd21eae5462d1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 22:38:35 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_16185866_REVIEW_1654915111327.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions16185866 { static final reviewedTitles16185866 = const {"Слим Компютърс ООД - ul. Georgi S. Rakovski 13, 8600 Yambol Center, Yambol, Bulgaria",
+"Monaco - bul. Vitosha 133, 1408 Ivan Vazov, Sofia, Bulgaria",
+};
+}
