c10c0a8d0b6cc276b12a70839d09ae6474e95d35
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 21:35:57 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_46363399_REVIEW_1654911353910.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions46363399 { static final reviewedTitles46363399 = const {"MAXX21 - JumpMAXX, LaserMAXX - Holzmanngasse 1, Floridsdorf 1210 Wien, Austria",
+"Maxx21 - Jedlersdorfer Str. 94, Floridsdorf 1210 Wien, Austria",
+};
+}
