00f01672a2d55c6d03d13024501a1797b1b33798
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-11 14:33:16 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_11_19831099_REVIEW_1654972392104.dart
@@ -0,0 +1,9 @@
+class AutoSuggestions19831099 { static final reviewedTitles19831099 = const {"PAPRIKA Lounge Bar - Piazza Nosetto 4C, 6500 Bellinzona, Switzerland",
+"Taqueria El Chilicuil - Corso Enrico Pestalozzi 12, 6900 Lugano, Switzerland",
+"Garage Golena SA - Centro Toyota Bellinzona - Via del Carmagnola 22- 24- 26, 6517 Arbedo, Switzerland",
+"Bar Osteria Indipendenza - Piazza Indipendenza 11, 6900 Lugano, Switzerland",
+"Gioielleria Preziosi - Viale Carlo Cattaneo 1, 6900 Lugano, Switzerland",
+"Pizza Burger Lugano - Via Luigi Lavizzari 5, 6900 Lugano, Switzerland",
+"Ottica Götte - Morottica SA - Via Pessina 8, 6900 Lugano, Switzerland",
+};
+}
