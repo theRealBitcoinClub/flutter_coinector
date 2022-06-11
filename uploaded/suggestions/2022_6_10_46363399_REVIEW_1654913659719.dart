1554b4dd5c4bb80c039304a27b7cd2ede0393a24
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 22:14:23 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_46363399_REVIEW_1654913659719.dart
@@ -0,0 +1,17 @@
+class AutoSuggestions46363399 { static final reviewedTitles46363399 = const {"MAXX21 - JumpMAXX, LaserMAXX - Holzmanngasse 1, Floridsdorf 1210 Wien, Austria",
+"Maxx21 - Jedlersdorfer Str. 94, Floridsdorf 1210 Wien, Austria",
+"LaserMAXX Bar - Roßauer Lände 45, Alsergrund 1090 Wien, Austria",
+"Restaurant-Bar Krahvogel - Anichstraße 12, Innenstadt 6020 Innsbruck, Austria",
+"WipeOut Austria Parndorf - Gewerbestraße 4, 7111 Parndorf, Austria",
+"ArtSmiles General & Cosmetic Dentistry - 4/45-49 Nind St, Southport QLD 4215, Australia",
+"Oh My Burger Antwerp - Graaf van Egmontstraat 47, 2000 Antwerpen, Belgium",
+"Doner & Gyros - ul. Hristo Botev 12, 5000 Veliko Tarnovo Center, Veliko Tarnovo, Bulgaria",
+"Arbat Guest Rooms - ul. Kapitan Pavel Gramadov 1, 5029 Arbanasi, Bulgaria",
+"Architect's house ( Дом на Архитекта) - ul. Velcho Dzhamdzhiyata 14, 5000 g.k. Varusha - south, Veliko Tarnovo, Bulgaria",
+"Happy Potter - ceramics and souvenirs - ul. Stefan Stambolov 53А, 5000 g.k. Varusha - north, Veliko Tarnovo, Bulgaria",
+"Fresh bar - ul. Borisova 14-162, 7000 Ruse Center, Ruse, Bulgaria",
+"bar Taboo - g.k. Krasna polyana 2 38A, 1373 ж.к. Красна поляна 2, Sofia, Bulgaria",
+"Kuche Ninja - Dog Grooming (подстригване на кучета) - Mladost 3, bl. 302, ent. 3 - inner yard, 1712 ж.к. Младост 3, Sofia, Bulgaria",
+"Club Cult - ul. Sveti Sveti Kiril I Metodiy 8, 8600 Yambol Center, Yambol, Bulgaria",
+};
+}
