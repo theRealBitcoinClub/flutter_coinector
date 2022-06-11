aa40a03b606e53f680fc6db548831d5dfe05ba0a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 22:40:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_16185866_REVIEW_1654915209412.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions16185866 { static final reviewedTitles16185866 = const {"Слим Компютърс ООД - ul. Georgi S. Rakovski 13, 8600 Yambol Center, Yambol, Bulgaria",
+"Monaco - bul. Vitosha 133, 1408 Ivan Vazov, Sofia, Bulgaria",
+"TATTOO Party Boat - Marina Nessebar, str.Monsenior A. Ronkali 6, 8231, 8231 Стария Град, Несебър, Bulgaria",
+};
+}
