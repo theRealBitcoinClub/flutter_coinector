18975023549aa2b71ac64afe1704fc9e5656cd93
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 21:44:52 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_46363399_REVIEW_1654911888870.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions46363399 { static final reviewedTitles46363399 = const {"MAXX21 - JumpMAXX, LaserMAXX - Holzmanngasse 1, Floridsdorf 1210 Wien, Austria",
+"Maxx21 - Jedlersdorfer Str. 94, Floridsdorf 1210 Wien, Austria",
+"LaserMAXX Bar - Roßauer Lände 45, Alsergrund 1090 Wien, Austria",
+"Restaurant-Bar Krahvogel - Anichstraße 12, Innenstadt 6020 Innsbruck, Austria",
+"WipeOut Austria Parndorf - Gewerbestraße 4, 7111 Parndorf, Austria",
+"ArtSmiles General & Cosmetic Dentistry - 4/45-49 Nind St, Southport QLD 4215, Australia",
+};
+}
