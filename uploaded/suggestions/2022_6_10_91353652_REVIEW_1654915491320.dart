16b618eb633b6a564d8d6d80127fa3825a57f09f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-10 22:44:55 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_10_91353652_REVIEW_1654915491320.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions91353652 { static final reviewedTitles91353652 = const {"Barbershop Old Capital - ul. Todor Balina 9a, 5006 Rayon Zapaden, Veliko Tarnovo, Bulgaria",
+"Ресторант Swiss - ul. Vasil Aprilov 56 А, 9700 Shumen Center, Shumen, Bulgaria",
+};
+}
