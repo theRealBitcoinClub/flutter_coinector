5005580884d60815698a93bcceea4c4f119801e8
TRBC - The Real Bitcoin Club <trbc@bitcoinmap.cash>
2022-06-11 11:16:27 -0400

    c

--- uploaded/suggestions/2022_6_10_66672204_REVIEW_1654916162274.dart
+++
@@ -1,6 +0,0 @@
-class AutoSuggestions66672204 { static final reviewedTitles66672204 = const {"Yazaki Bulgaria EOOD - бул. Европа 43, 8600 Western Industrial Zone, Yambol, Bulgaria",
-"Museum of Illusions Sofia - бул. княгиня Мария Луиза 16, 1000 Old City Center, Sofia, Bulgaria",
-"Smerch - ul. Han Kubrat 4, 8600 g.k. General Vladimir Zaimov, Yambol, Bulgaria",
-"Swiss Hotel - ul. Vasil Aprilov 56, 9705 Shumen Center, Shumen, Bulgaria",
-};
-}
