eba65f14afcc48a25763b2351c2c2c553937b2c2
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-12 11:31:36 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_12_98042329_REVIEW_1655047892250.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions98042329 { static final reviewedTitles98042329 = const {"Pizza Luciana - Ženklava 160, 742 67 Ženklava, Czechia",
+"vybaveniprouklid.cz - Bečovská 939, 104 00 Praha 22, Czechia",
+};
+}
