26e6e0f231f8ba16b1a71419dc32dd36c1e65dcd
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-12 10:36:15 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_12_13361998_REVIEW_1655044570385.dart
@@ -0,0 +1,12 @@
+class AutoSuggestions13361998 { static final reviewedTitles13361998 = const {"Internesto Apartments - Veselá 164/14, 602 00 Brno-střed-Brno-město, Czechia",
+"Hotel a restaurace Praděd Rýmařov - nám. Svobody 12, 795 01 Rýmařov, Czechia",
+"Rest - Nová Přímětická 792/57, Přímětice, 669 04 Znojmo, Czechia",
+"KáDéčko Bar & Grill - Sokolská 412, 463 42 Hodkovice nad Mohelkou, Czechia",
+"Nest Escape - Hybešova 29, 602 00 Brno-Brno-střed, Czechia",
+"Elektro - Lumír Pavlík - 1. máje 124, 340 04 Železná Ruda, Czechia",
+"Střelecka Škola Fišer - 26, Koněvova 316, 130 00 Praha 3-Žižkov, Czechia",
+"Džemy Kvasnička - Českobrodská 367, Hostavice, 198 00 Praha-Praha 14, Czechia",
+"Hospůdka Rasovna - Pražská 417, 276 01 Mělník, Czechia",
+"Generali Česká pojišťovna - Malinovského nám. 4, 602 00 Brno-Brno-střed, Czechia",
+};
+}
