4a023135ff15e637dbea913cc93a0a12e08dcb85
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-12 01:46:19 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_12_86578974_REVIEW_1655012774793.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions86578974 { static final reviewedTitles86578974 = const {"Nida Needles Tailor - Unit 17, Market Hall, The Mall Wood Green, 159 High Road, London N22 6YQ, UK",
+"Orcs Nest - 6 Earlham Street, London WC2H 9RY, UK",
+"Praxis Bubenberg 8+11 - Bubenbergpl. 8, 3011 Bern, Switzerland",
+};
+}
