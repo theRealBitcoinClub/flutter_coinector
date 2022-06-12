31529b44099f276b303c4d0435574f1f764cbb0b
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-12 02:06:17 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_12_23238096_REVIEW_1655013972698.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions23238096 { static final reviewedTitles23238096 = const {"Midgard Bar - 53, Jaromírova 245, 128 00 Praha 2-Nusle, Czechia",
+"STAVITELSTVÍ ČERVENÝ - Žitná 1480/3, 621 00 Brno-Brno-Řečkovice a Mokrá Hora, Czechia",
+"Life invest as - Na Laurové, 150 00 Praha 5, Czechia",
+};
+}
