a3ea62b9f8952ee1f957ee0271131b7cf956e09e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-12 01:19:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_12_4688629_REVIEW_1655011179960.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions4688629 { static final reviewedTitles4688629 = const {"The Flower Stand Chelsea - 241 Fulham Road, London SW3 6HY, UK",
+"BEST Antivirus by SSG Limited - SSG Limited, 128 City Road, London EC1V 2NX, UK",
+"P&V - 180 Borough High Street, London SE1 1LH, UK",
+"Amazon Hub store - 60 Holborn Viaduct, London EC1A 2FD, UK",
+"Wick Liquor Ejuice Co. - Unit 12 Darwell. Park, Mica Close, Tamworth B77 4DR, UK",
+};
+}
