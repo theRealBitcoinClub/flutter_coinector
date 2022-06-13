67b620c99fe90a36a2cd71a54930b618153a9137
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-13 09:31:46 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_13_30540315_REVIEW_1655127105188.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions30540315 { static final reviewedTitles30540315 = const {"KAM PHONE - Uhelná 4, 669 02 Znojmo, Czechia",
+"Ubytování Lumik - Slunná 72, 790 01 Jeseník, Czechia",
+};
+}
