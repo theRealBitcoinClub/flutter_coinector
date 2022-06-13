784d1a65c4087ec5db7a264aba3514a1efd63bd2
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-13 09:01:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_13_12626849_REVIEW_1655125272500.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions12626849 { static final reviewedTitles12626849 = const {"HopTaxi - Americká 17, 301 00 Plzeň 3, Czechia",
+"TO SK Mělník - 80, Pražská 416, 276 01 Mělník, Czechia",
+"Zmrzlina Opatia - Kamenice 136, 547 01 Náchod, Czechia",
+"Tržnice - Unnamed Road, 741 01 Nový Jičín, Czechia",
+};
+}
