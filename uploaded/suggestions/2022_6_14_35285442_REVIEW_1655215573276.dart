cc9bbad50d37ee67523f1083ee42d26ce312b3e1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 10:06:14 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_35285442_REVIEW_1655215573276.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions35285442 { static final reviewedTitles35285442 = const {"Airsoft klub Ptuj - Ob Grajeni 1, 2250 Ptuj, Slovenia",
+"La Cantina - Pohorska ulica 60, 2000 Maribor, Slovenia",
+};
+}
