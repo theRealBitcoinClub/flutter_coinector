c5275110471402a9eb0ad0a60f18336488ebf662
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 07:27:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_21653504_REVIEW_1655206020953.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions21653504 { static final reviewedTitles21653504 = const {"ASTRO CAFFE - Vojkova cesta 49, 5000 Nova Gorica, Slovenia",
+"Bar & Gostinica PADELCA - Vogrsko 7, 5293 Volčja Draga, Slovenia",
+};
+}
