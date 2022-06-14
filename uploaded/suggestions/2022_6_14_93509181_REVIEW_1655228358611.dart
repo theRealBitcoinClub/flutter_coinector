d0b26458e274e38a5312b2edf4722d44b56da1c3
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 13:39:20 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_93509181_REVIEW_1655228358611.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions93509181 { static final reviewedTitles93509181 = const {"Estetika Fabjan d.o.o. - Nova Gorica - Vipavska cesta 58, 5000 Nova Gorica, Slovenia",
+"GOLD STORE - PE CELJE - Ljubljanska c. 1B, 3000 Celje, Slovenia",
+"Breza caffe - Celovška cesta 179b, 1000 Ljubljana, Slovenia",
+"GOLD STORE - PE BTC - Šmartinska c. 152, 1000 Ljubljana, Slovenia",
+"Barbarella BTC Bistro - Ameriška ulica 13, 1000 Ljubljana, Slovenia",
+};
+}
