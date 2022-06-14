299ac94cb59b22aeb136bd5080047ba3110ef82d
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 14:38:51 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_59424357_REVIEW_1655231929974.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions59424357 { static final reviewedTitles59424357 = const {"Cacao BTC - Letališka cesta 11, 1000 Ljubljana, Slovenia",
+"Cacao Ljubljana - Petkovškovo nabrežje 3, 1000 Ljubljana, Slovenia",
+};
+}
