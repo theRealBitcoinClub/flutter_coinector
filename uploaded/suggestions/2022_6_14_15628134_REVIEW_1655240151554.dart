5f572c6096de70801aa817ae2e1c06ad00a7c8d4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 16:55:53 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_15628134_REVIEW_1655240151554.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions15628134 { static final reviewedTitles15628134 = const {"Gerry Weber - Wolfova ulica 6, 1000 Ljubljana, Slovenia",
+"Tkanina d.o.o. - Cankarjeva - Cankarjeva cesta 4, 1000 Ljubljana, Slovenia",
+};
+}
