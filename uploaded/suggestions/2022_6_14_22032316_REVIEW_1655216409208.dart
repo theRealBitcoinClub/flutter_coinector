8e1f9303076da72a9f1aed0baab81a4e7cd8e01e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 10:20:10 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_22032316_REVIEW_1655216409208.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions22032316 { static final reviewedTitles22032316 = const {"Masim Caffe - Ulica XXX. divizije 21, 5000 Nova Gorica, Slovenia",
+"Molek.si d.o.o. - Vrtnarija 3, 1360 Vrhnika, Slovenia",
+};
+}
