dd2fb6bb06f136060dc7efeae6ab7818f8040ebc
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-13 23:31:52 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_13_70374098_REVIEW_1655177511314.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions70374098 { static final reviewedTitles70374098 = const {"Eko resort pod Veliko planino - 44b, Godič, 1242 Stahovica, Slovenia",
+"Eko resort pod Veliko planino - 44b, Godič, 1242 Stahovica, Slovenia",
+};
+}
