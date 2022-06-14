8ae1886788f7f56077812940b4f29a7f7f5af911
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 14:48:26 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_62036211_REVIEW_1655232504886.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions62036211 { static final reviewedTitles62036211 = const {"Cacao Ljubljana - Petkovškovo nabrežje 3, 1000 Ljubljana, Slovenia",
+"Bungalow Portorož - Obala 18a, 6320 Portorož, Slovenia",
+};
+}
