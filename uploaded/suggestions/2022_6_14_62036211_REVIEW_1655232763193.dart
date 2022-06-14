7f16acd6d889ebbd7252a593f689f4e1a2e76fe6
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 14:52:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_62036211_REVIEW_1655232763193.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions62036211 { static final reviewedTitles62036211 = const {"Cacao Ljubljana - Petkovškovo nabrežje 3, 1000 Ljubljana, Slovenia",
+"Bungalow Portorož - Obala 18a, 6320 Portorož, Slovenia",
+"SIMPLE - Koper Park center - Ankaranska cesta 2, 6000 Koper - Capodistria, Slovenia",
+};
+}
