f737d97619d4c107244a8a3a8664cc6e9c1ecccf
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 17:46:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_13293638_REVIEW_1655243162945.dart
@@ -0,0 +1,16 @@
+class AutoSuggestions13293638 { static final reviewedTitles13293638 = const {"Avtodeli Potokar - Tovarniška cesta 4a, 5270 Ajdovščina, Slovenia",
+"Steklarstvo Potokar - Cesta Franceta Prešerna, 4270 Jesenice, Slovenia",
+"Hotenjka, D.O.O. - Tržaška c. 513b, 1351 Brezovica pri Ljubljani, Slovenia",
+"Avtodeli Potokar - Tovarniška ulica 7, 8270 Krško, Slovenia",
+"Avtodeli Potokar, Sežana - Cesta na Lenivec 34, 6210 Sežana, Slovenia",
+"Avtodeli Potokar - Dunajska c. 421, 1000 Ljubljana, Slovenia",
+"Atlantis Dive Center - 1000, Šmartinska c. 152, 1000 Ljubljana, Slovenia",
+"Avto center- Car Wash DIAMANT - Ulica Jožeta Jame 12, 1210 Ljubljana - Šentvid, Slovenia",
+"SIMPLE - Koper Mercator - Dolinska cesta 1a, 6000 Koper - Capodistria, Slovenia",
+"SIMPLE Frizerski salon - Spodnji Plavž 5, 4270 Jesenice, Slovenia",
+"SIMPLE - Logatec Mercator - Tržaška cesta 3, 1370 Logatec, Slovenia",
+"SIMPLE - Sežana Spar - Cesta na Lenivec 5, 6210 Sežana, Slovenia",
+"SIMPLE - Koper Park center - Ankaranska cesta 2, 6000 Koper - Capodistria, Slovenia",
+"Avtodeli Potokar - Gojače 5e, 5262 Črniče, Slovenia",
+};
+}
