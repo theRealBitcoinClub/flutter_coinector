99982c3a7f29873427e404444d687e196d6bee91
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 14:23:40 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_93644892_REVIEW_1655231019053.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions93644892 { static final reviewedTitles93644892 = const {"Avtocenter AvtoStop - Devova ulica 18, 1000 Ljubljana, Slovenia",
+"Tattoo & Piercing Studio CULT - Šmartinska c. 152, 1000 Ljubljana, Slovenia",
+};
+}
