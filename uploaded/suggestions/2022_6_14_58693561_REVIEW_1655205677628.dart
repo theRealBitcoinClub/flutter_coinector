09291f18092109993aa96c6b639a568e417ea435
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 07:21:19 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_58693561_REVIEW_1655205677628.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions58693561 { static final reviewedTitles58693561 = const {"Venera Shop - Sex shop Ljubljana - Vošnjakova ulica 5, 1000 Ljubljana, Slovenia",
+"Locap Group - Your Local Accommodation Point - Letoviška pot 11, 6320 Portorož, Slovenia",
+"SkyEye Bitcoin ATM - Obrežna ulica 33, 2000 Maribor, Slovenia",
+"Čisto Belo, čistilnica in pralnica perila, d.o.o. - Gradis, Letališka cesta 33 E, 1000 Ljubljana, Slovenia",
+"Laplac & Pizza Fabrika Ptuj - Rajšpova ulica 26, 2250 Ptuj, Slovenia",
+"LAM Care - Žanova ulica 3, 4000 Kranj, Slovenia",
+};
+}
