e6393b5d7ad37e571f2ab1259e9c7679e7b8d84f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-13 23:46:36 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_13_70374098_REVIEW_1655178395623.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions70374098 { static final reviewedTitles70374098 = const {"Eko resort pod Veliko planino - 44b, Godič, 1242 Stahovica, Slovenia",
+"Eko resort pod Veliko planino - 44b, Godič, 1242 Stahovica, Slovenia",
+"Takos - Pobreška cesta 18, 2000 Maribor, Slovenia",
+"Rona Caffe - Šuceva ulica 25, 4000 Kranj, Slovenia",
+"Avtodeli Potokar - Dunajska c. 421, 1000 Ljubljana, Slovenia",
+"SOBA ROOM - Trg mladih 1, 1000 Ljubljana, Slovenia",
+};
+}
