caf9f80300bd00263d1b4ae2ca776c7bd0381c35
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 19:11:52 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_35567300_REVIEW_1655248310481.dart
@@ -0,0 +1,15 @@
+class AutoSuggestions35567300 { static final reviewedTitles35567300 = const {"Kava Bar - BS, Pot na Orle 2, 1000 Ljubljana, Slovenia",
+"ID: Factory Hairgroup - Kolodvorska ulica 12, 1000 Ljubljana, Slovenia",
+"M Caffe - Dalmatinova ulica 4a, 1000 Ljubljana, Slovenia",
+"Frizerski studio Red Scissors - Tržaška cesta 35, 6230 Postojna, Slovenia",
+"Boutique - Šmartinska c. 152, 1000 Ljubljana, Slovenia",
+"City cafe - Šmartinska c. 152g, 1000 Ljubljana, Slovenia",
+"List biro oprema d.o.o. - Litijska cesta 47, 1000 Ljubljana, Slovenia",
+"Tozd - 1000, Gallusovo nabrežje 27, 1000 Ljubljana, Slovenia",
+"SIMPLE - Nova Gorica Qlandia - Cesta 25. junija 1a, 5000 Nova Gorica, Slovenia",
+"Frizerski salon Helena Pogačar s.p. - Gmajnica 295, 1218 Komenda, Slovenia",
+"Dvorana Krim - Šport Ljubljana - Ob dolenjski železnici 50, 1000 Ljubljana, Slovenia",
+"SIMPLE - Novo mesto Supernova - Otoška cesta 5, 8000 Novo mesto, Slovenia",
+"SIMPLE - Krško, Supernova Qlandia - Cesta krških žrtev 141, 8270 Krško, Slovenia",
+};
+}
