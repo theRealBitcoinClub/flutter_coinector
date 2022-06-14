53026eff3541e56930084330354cc8dcb1523374
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 16:15:57 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_10850411_REVIEW_1655237756219.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions10850411 { static final reviewedTitles10850411 = const {"Kolesarski center Bauer trgovina, zastopstvo in posredovanje, d.o.o., Ljubljana - Šmartinska c. 152, 1000 Ljubljana, Slovenia",
+"MORRO SARTORIALE LJUBLJANA - Ameriška ulica 8, 1000 Ljubljana, Slovenia",
+"TIP&TAP - Šmartinska c. 106, 1000 Ljubljana, Slovenia",
+"NŠ PLAC - Celovška cesta 177 Šiška, 1000 Ljubljana, Slovenia",
+"Toskana delikatesa- enota Gurman, Koseze - Vodnikova cesta 187, 1000 Ljubljana, Slovenia",
+"Gold Store Pe Kranj - Koroška cesta 4, 4000 Kranj, Slovenia",
+};
+}
