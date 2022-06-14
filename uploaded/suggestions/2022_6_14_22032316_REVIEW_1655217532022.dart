9c8c26e4607ad0ee42b42644bd444633738d5837
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 10:38:53 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_22032316_REVIEW_1655217532022.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions22032316 { static final reviewedTitles22032316 = const {"Masim Caffe - Ulica XXX. divizije 21, 5000 Nova Gorica, Slovenia",
+"Molek.si d.o.o. - Vrtnarija 3, 1360 Vrhnika, Slovenia",
+"Tuš supermarket Grosuplje - Gasilska Cesta 1, 1290 Grosuplje, Slovenia",
+"Buzz - Šmartinska c. 152g, 1000 Ljubljana, Slovenia",
+"Elektro team servis Peter Čelesnik s.p. - Hrastulje 44, 8275 Škocjan, Slovenia",
+};
+}
