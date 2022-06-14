b688b3f8923b9ff86fadd3cce30f07110bb16c2e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 13:13:55 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_23516356_REVIEW_1655226833347.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions23516356 { static final reviewedTitles23516356 = const {"GOLD STORE - PE KOPER - Pristaniška ulica 2, 6000 Koper - Capodistria, Slovenia",
+"GOLD STORE - PE MARIBOR - Cafova ulica 4, 2000 Maribor, Slovenia",
+};
+}
