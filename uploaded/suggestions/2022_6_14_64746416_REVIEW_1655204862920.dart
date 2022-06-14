9fdc5349fada9de5f6d7b68b5029a283e387a4da
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 07:07:44 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_64746416_REVIEW_1655204862920.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions64746416 { static final reviewedTitles64746416 = const {"Biljard klub Direkt - Tbilisijska ulica 83, 1000 Ljubljana, Slovenia",
+"Kozlovna Poštna - Poštna ulica 12, 2000 Maribor, Slovenia",
+};
+}
