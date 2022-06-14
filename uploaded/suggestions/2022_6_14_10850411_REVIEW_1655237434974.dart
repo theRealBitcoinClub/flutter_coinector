f332b6aa14713ff2873207e15b36d9e169ca1e07
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 16:10:36 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_10850411_REVIEW_1655237434974.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions10850411 { static final reviewedTitles10850411 = const {"Kolesarski center Bauer trgovina, zastopstvo in posredovanje, d.o.o., Ljubljana - Šmartinska c. 152, 1000 Ljubljana, Slovenia",
+"MORRO SARTORIALE LJUBLJANA - Ameriška ulica 8, 1000 Ljubljana, Slovenia",
+"TIP&TAP - Šmartinska c. 106, 1000 Ljubljana, Slovenia",
+"NŠ PLAC - Celovška cesta 177 Šiška, 1000 Ljubljana, Slovenia",
+};
+}
