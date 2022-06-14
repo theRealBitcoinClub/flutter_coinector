7a9b4e09bb84520aa369ebe33cbb9e11e8a8d4db
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 07:29:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_21653504_REVIEW_1655206166467.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions21653504 { static final reviewedTitles21653504 = const {"ASTRO CAFFE - Vojkova cesta 49, 5000 Nova Gorica, Slovenia",
+"Bar & Gostinica PADELCA - Vogrsko 7, 5293 Volčja Draga, Slovenia",
+"Žito maloprodaja trgovsko podjetje na veliko in malo, d.o.o., poslovna enota Potojoča trgovina arkade - Tržnica, Pogačarjev trg 1, 1000 Ljubljana, Slovenia",
+"Domas - Kont družba za svetovanje in storitve d.o.o., Ljubljana - Rožna Dolina, cesta XIII 5, 1000 Ljubljana, Slovenia",
+};
+}
