d1d8cca87d5e3462246ff0ca3a117b0a5e785501
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-14 06:53:49 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_14_15451170_REVIEW_1655204027821.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions15451170 { static final reviewedTitles15451170 = const {"Tenis društvo ob Savi - Tenis drustvo, ob Savi, 1000 Ljubljana, Slovenia",
+"Kavarna Zlato Polje - Koroška cesta 30, 4000 Kranj, Slovenia",
+};
+}
