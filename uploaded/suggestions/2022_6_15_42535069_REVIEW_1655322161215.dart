19979d657d34aa384ff62293faaaf576a1d5a3d1
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-15 15:42:42 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_15_42535069_REVIEW_1655322161215.dart
@@ -0,0 +1,11 @@
+class AutoSuggestions42535069 { static final reviewedTitles42535069 = const {"F. Dvor, services and trade D.O.O. - Ob železnici 14, 1000 Ljubljana, Slovenia",
+"Ljubljanski Brivec by Mič styling - HOTEL UNION - Miklošičeva cesta 1, 1000 Ljubljana, Slovenia",
+"YOUNG MIČ STYLING - Trubarjeva cesta 28, 1000 Ljubljana, Slovenia",
+"Mič Styling - Poljanska - Poljanska cesta 13, 1000 Ljubljana, Slovenia",
+"Venera Shop, podjetje za trgovino in storitve, d.o.o. - Seškova cesta 9, 1215 Medvode, Slovenia",
+"SIMPLE - Jurčkova Cesta 223, 1000 Ljubljana, Slovenia",
+"SIMPLE - Nova Gorica Qlandia - Cesta 25. junija 1a, 5000 Nova Gorica, Slovenia",
+"Venera Shop, podjetje za trgovino in storitve, d.o.o. - Šuceva ulica 25, 4000 Kranj, Slovenia",
+"Venera Shop, podjetje za trgovino in storitve, d.o.o. - Šaleška cesta 19a, 3320 Velenje, Slovenia",
+};
+}
