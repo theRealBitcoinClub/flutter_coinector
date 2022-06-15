9427b5d8a1254497cd41bd759973063b915fb22d
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-15 16:13:06 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_15_70636605_REVIEW_1655323985163.dart
@@ -0,0 +1,8 @@
+class AutoSuggestions70636605 { static final reviewedTitles70636605 = const {"Venera Shop - Sex shop Domžale - Unnamed Road, 1230, 1230 Domžale, Slovenia",
+"Takos - Pobreška cesta 18, 2000 Maribor, Slovenia",
+"Venera Shop, podjetje za trgovino in storitve, d.o.o. - Prešernova cesta 53, 6310 Izola, Slovenia",
+"Venera Shop, podjetje za trgovino in storitve, d.o.o. - Ulica Gradnikove brigade 53, 5000 Nova Gorica, Slovenia",
+"Venera shop - Sex shop Murska Sobota - Nemčavci 1d, 9000 Murska Sobota, Slovenia",
+"Venera shop - Sex Shop Maribor - Cesta proletarskih brigad 100, 2000 Maribor, Slovenia",
+};
+}
