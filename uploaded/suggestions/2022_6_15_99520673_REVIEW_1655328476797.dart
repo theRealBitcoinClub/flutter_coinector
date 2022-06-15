48c4485a9b0278253817cb994fc2c6f11eb21bea
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-15 17:27:58 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_15_99520673_REVIEW_1655328476797.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions99520673 { static final reviewedTitles99520673 = const {"Leskovački dvor - Dunajska c. 57, 1000 Ljubljana, Slovenia",
+"Žito, Delikatesa Koseze - Vodnikova cesta 187, 1000 Ljubljana, Slovenia",
+};
+}
