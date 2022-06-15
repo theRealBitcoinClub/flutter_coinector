36ee1f1e85f7e2efdd70e03cd0d4774e6ff89d11
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-15 16:33:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_15_76677314_REVIEW_1655325191782.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions76677314 { static final reviewedTitles76677314 = const {"Kino Komuna - Kino Komuna, Kino Komuna, Tomšičeva ulica 2, 1000 Ljubljana, Slovenia",
+"SIMPLE - Novo mesto Mercator - Ljubljanska cesta 47, 8000 Novo mesto, Slovenia",
+};
+}
