0e177343c5fe49e4b57fcbd622b1c276d2ecc476
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-15 17:58:24 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_15_40946544_REVIEW_1655330302809.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions40946544 { static final reviewedTitles40946544 = const {"Kavarna Zvezda - Wolfova ulica 14, 1000 Ljubljana, Slovenia",
+"Fitnesshop - Rakuševa ulica 10, 1000 Ljubljana, Slovenia",
+};
+}
