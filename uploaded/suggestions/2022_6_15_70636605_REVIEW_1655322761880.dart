98dc2d513bbf306433e15385ca35060758d0bf33
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-15 15:52:43 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_15_70636605_REVIEW_1655322761880.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions70636605 { static final reviewedTitles70636605 = const {"Venera Shop - Sex shop Domžale - Unnamed Road, 1230, 1230 Domžale, Slovenia",
+"Takos - Pobreška cesta 18, 2000 Maribor, Slovenia",
+};
+}
