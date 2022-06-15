a9343b38fb39df273b41179bbe50acad8fcf2fa3
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-15 14:49:05 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_15_42535069_REVIEW_1655318943478.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions42535069 { static final reviewedTitles42535069 = const {"F. Dvor, services and trade D.O.O. - Ob železnici 14, 1000 Ljubljana, Slovenia",
+"Ljubljanski Brivec by Mič styling - HOTEL UNION - Miklošičeva cesta 1, 1000 Ljubljana, Slovenia",
+};
+}
