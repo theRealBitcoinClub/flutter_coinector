a1fb6821e8fef494b110a36ad947bc292a669f43
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 07:17:05 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_54269816_REVIEW_1655378223573.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions54269816 { static final reviewedTitles54269816 = const {"Lagev bar - Ulica dr. Vrbnjaka 10a, 9000 Murska Sobota, Slovenia",
+"Ag Gantar - Kranjska cesta 22, 4202 Naklo, Slovenia",
+"Galerija Kofi Design - Kersnikova ulica 10, 1000 Ljubljana, Slovenia",
+"Škrnic'lj restavracija - Andrej Belovič s.p. - Šmartinska c. 152, 1000 Ljubljana, Slovenia",
+"Quicksilver Store - Mestni trg 16, 1000 Ljubljana, Slovenia",
+};
+}
