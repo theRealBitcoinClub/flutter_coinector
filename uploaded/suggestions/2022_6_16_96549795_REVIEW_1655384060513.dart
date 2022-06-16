97157a9f27a30c5ca3d3dc6b9600df7779b67c2c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 08:54:22 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_96549795_REVIEW_1655384060513.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions96549795 { static final reviewedTitles96549795 = const {"ALEJA - Rakuševa ulica 1, 1000 Ljubljana, Slovenia",
+"Soba pobega MindMaze, Escape Room Ljubljana - Trubarjeva cesta 24, Ground floor, 1000 Ljubljana, Slovenia",
+};
+}
