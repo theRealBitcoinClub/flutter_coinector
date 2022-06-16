10ac19850ab002914bd02ee904a4a380bd6835f7
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 11:09:46 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_81611601_REVIEW_1655392184945.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions81611601 { static final reviewedTitles81611601 = const {"TISHLER EYEWEAR - Cesta svobode 15, 4260 Bled, Slovenia",
+"POTOKAR d.o.o., p.e. Vrhnika - Cankarjev trg 4, 1360 Vrhnika, Slovenia",
+"VAPE ON SHOP - Trubarjeva cesta 46, 1000 Ljubljana, Slovenia",
+};
+}
