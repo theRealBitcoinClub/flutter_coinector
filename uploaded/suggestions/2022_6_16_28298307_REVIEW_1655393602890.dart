591030c62919511eaa143ef4411997ea6a3bd041
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 11:33:24 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_28298307_REVIEW_1655393602890.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions28298307 { static final reviewedTitles28298307 = const {"Takos - Pobreška cesta 18, 2000 Maribor, Slovenia",
+"Doget d.o.o. - Miklošičeva cesta 38, 1000 Ljubljana, Slovenia",
+"Bazaar Smoking shop - Gregorčičeva ulica 6, 4000 Kranj, Slovenia",
+"Klub Rečnih Kapitanov - Opekarska 1 (Pod Prulskim mostom), 1000 Ljubljana, Slovenia",
+};
+}
