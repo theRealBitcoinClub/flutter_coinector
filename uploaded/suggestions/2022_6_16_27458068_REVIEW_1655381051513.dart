28c7639be2231a15ad757eef28e297f92baf0573
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 08:04:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_27458068_REVIEW_1655381051513.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions27458068 { static final reviewedTitles27458068 = const {"Club SEVEN - Šmartno - Brod, 1133 Ljubljana - Brod, Slovenia",
+"Club SEVEN - Šmartno - Brod, 1133 Ljubljana - Brod, Slovenia",
+"Restavracija Dobri Čuk - Stegne 23a, 1000 Ljubljana, Slovenia",
+"Prosti čas Kamnik - Šutna 37, 1241 Kamnik, Slovenia",
+"Lečnik, urarstvo in trgovina, d.o.o. - Stari trg 14, 1000 Ljubljana, Slovenia",
+};
+}
