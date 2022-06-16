fe5da4f5284349ff79e0189305e846f8b76a9df7
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 11:28:53 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_28298307_REVIEW_1655393331732.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions28298307 { static final reviewedTitles28298307 = const {"Takos - Pobreška cesta 18, 2000 Maribor, Slovenia",
+"Doget d.o.o. - Miklošičeva cesta 38, 1000 Ljubljana, Slovenia",
+};
+}
