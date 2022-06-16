e12a6b3984c471e377d9363d46c77a547942f965
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 07:38:00 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_19897112_REVIEW_1655379478397.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions19897112 { static final reviewedTitles19897112 = const {"OOOPS boutique d.o.o. - Slovenska cesta 30, 1000 Ljubljana, Slovenia",
+"Karin podjetje za trgovino, storitve in zastopanje, d.o.o., Ljubljana-Brezovica, Tržaška 438 - Tržaška c. 438, 1351 Brezovica pri Ljubljani, Slovenia",
+"Restavracija in pizzeria B2 - Tržaška c. 200, 1000 Ljubljana, Slovenia",
+};
+}
