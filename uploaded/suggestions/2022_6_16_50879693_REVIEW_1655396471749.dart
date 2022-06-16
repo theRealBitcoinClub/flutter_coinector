f26b350755e6c1a1f342e349b6fe1d141ef8bd20
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 12:21:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_50879693_REVIEW_1655396471749.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions50879693 { static final reviewedTitles50879693 = const {"Trappa pizzeria&bar - Koprska ulica 94, 1000 Ljubljana, Slovenia",
+"PraveStvari - Dunajska c. 122, 1000 Ljubljana, Slovenia",
+"Mk Gril, priprava in dostava hrane, Miomir Kostić s.p. - Bratovševa ploščad 12, 1000 Ljubljana, Slovenia",
+};
+}
