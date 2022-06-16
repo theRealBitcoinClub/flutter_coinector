e7f1378c9178fcd8d51b758dc8e0781854e18351
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-16 12:32:14 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_16_50879693_REVIEW_1655397132233.dart
@@ -0,0 +1,11 @@
+class AutoSuggestions50879693 { static final reviewedTitles50879693 = const {"Trappa pizzeria&bar - Koprska ulica 94, 1000 Ljubljana, Slovenia",
+"PraveStvari - Dunajska c. 122, 1000 Ljubljana, Slovenia",
+"Mk Gril, priprava in dostava hrane, Miomir Kostić s.p. - Bratovševa ploščad 12, 1000 Ljubljana, Slovenia",
+"Fotokopirnica Gallus, Kristina Kogelnik s.p. - Grajska ulica 5, 2000 Maribor, Slovenia",
+"Maribor INN hotel - Karantanska ulica 26, 2000 Maribor, Slovenia",
+"Um - ha, gostinske storitve, d.o.o. - BTC tržnica sever, Italijanska ulica 7, 1000 Ljubljana, Slovenia",
+"Čebelarstvo Vasič - Litijska cesta 12, 1000 Ljubljana, Slovenia",
+"Kava bar mi Srša Patricija s.p. - Ruska ulica 9, 2000 Maribor, Slovenia",
+"Kavarnica Rokaj - Usnjarska ulica 3, 2000 Maribor, Slovenia",
+};
+}
