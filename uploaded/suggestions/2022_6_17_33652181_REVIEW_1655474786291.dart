0955b5ca39bbbac7769f5eee27f7bf230ba80184
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-17 10:06:28 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_17_33652181_REVIEW_1655474786291.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions33652181 { static final reviewedTitles33652181 = const {"Tuš supermarket C&C Sežana - Partizanska c. 71, 6210 Sežana, Slovenia",
+"Tuš supermarket Žalec - Ulica heroja Staneta 8, 3310 Žalec, Slovenia",
+};
+}
