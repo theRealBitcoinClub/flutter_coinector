b321a5ef79b4d0e49957d4485c78507f3d3b5f6c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-17 09:59:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_17_8064581_REVIEW_1655474350684.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions8064581 { static final reviewedTitles8064581 = const {"Tuš supermarket Planet Maribor - Studenci - Na Poljanah 18, 2000 Maribor, Slovenia",
+"Tuš supermarket Rače - Ljubljanska cesta 13, 2327 Rače, Slovenia",
+};
+}
