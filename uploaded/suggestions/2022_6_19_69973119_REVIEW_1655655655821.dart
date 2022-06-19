790d6a74b26c666d086f0e3d5318235a7585ccd2
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-19 12:20:59 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_19_69973119_REVIEW_1655655655821.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions69973119 { static final reviewedTitles69973119 = const {"The Fifth Element studio - Barjanska cesta 62, 1000 Ljubljana, Slovenia",
+"Big Bang Jesenice - Fužinska cesta 8, 4270 Jesenice, Slovenia",
+};
+}
