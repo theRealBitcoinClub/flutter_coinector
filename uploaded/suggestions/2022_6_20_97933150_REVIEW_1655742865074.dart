209aa810cec98df04918ccb696ac97fea4804267
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-20 12:34:27 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_20_97933150_REVIEW_1655742865074.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions97933150 { static final reviewedTitles97933150 = const {"Big Bang Ptuj - Ormoška 15 Nakupovalni center Qlandia, 2250 Ptuj, Slovenia",
+"SkyEye Bitcoin ATM - Gasilska ulica 10, 1292 Ig, Slovenia",
+"Big Bang Kranj, Stop Shop - Cesta Staneta Žagarja 71, 4000 Kranj, Slovenia",
+"Big Bang - Pobreška cesta 18, 2000 Maribor, Slovenia",
+};
+}
