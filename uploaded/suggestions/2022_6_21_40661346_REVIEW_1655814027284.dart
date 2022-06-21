32d1d0d36e2513fba6a2db09ed40827360977b6e
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-21 08:20:29 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_21_40661346_REVIEW_1655814027284.dart
@@ -0,0 +1,6 @@
+class AutoSuggestions40661346 { static final reviewedTitles40661346 = const {"AcuModerna - reopens end of May, June 2020 - Vipavska cesta 23a, 5000 Nova Gorica, Slovenia",
+"Ošterija pri oljki - Šmartinska c. 152g, 1000 Ljubljana, Slovenia",
+"Big Bang - Otoška cesta 5 Nakupovalni center Qlandia, 8000 Novo mesto, Slovenia",
+"Big Bang Ljubljana, Rudnik - Park Center - Jurčkova Cesta 228, 1000 Ljubljana, Slovenia",
+};
+}
