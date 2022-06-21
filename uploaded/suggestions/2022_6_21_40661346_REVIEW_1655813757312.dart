37a68e6d824cba14946cf76b06341374720d13f7
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-21 08:15:59 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_21_40661346_REVIEW_1655813757312.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions40661346 { static final reviewedTitles40661346 = const {"AcuModerna - reopens end of May, June 2020 - Vipavska cesta 23a, 5000 Nova Gorica, Slovenia",
+"Ošterija pri oljki - Šmartinska c. 152g, 1000 Ljubljana, Slovenia",
+};
+}
