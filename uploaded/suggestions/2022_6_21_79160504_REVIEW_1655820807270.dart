fae80bd926b57af7a8e372975dfb488698ca506c
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-21 10:13:29 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_21_79160504_REVIEW_1655820807270.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions79160504 { static final reviewedTitles79160504 = const {"Big Bang Kamnik, Qlandia - Domžalska cesta 3, 1241 Kamnik, Slovenia",
+"Qulto :: qoffee & qulinaris - Slovenska ulica 9, 2000 Maribor, Slovenia",
+};
+}
