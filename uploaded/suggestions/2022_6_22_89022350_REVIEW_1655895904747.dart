5339ae43c8bed05e676434fec259d070be8766d4
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 07:05:06 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_89022350_REVIEW_1655895904747.dart
@@ -0,0 +1,14 @@
+class AutoSuggestions89022350 { static final reviewedTitles89022350 = const {"Superdry - Planet Tuš, Ankaranska cesta 2, 6000 Koper, Slovenia",
+"Parfois - Jurčkova Cesta 223, 1000 Ljubljana, Slovenia",
+"Sportina let, družba za trgovino in zastopanje d.o.o., poslovna enota Mega Factory Outlet - Jurčkova Cesta 224, 1000 Ljubljana, Slovenia",
+"Pikanet - Lendavska ulica, 9000 Murska Sobota, Slovenia",
+"Sportina Group - Sportina Outlet - Ankaranska cesta 3a, 6000 Koper, Slovenia",
+"Sportina - Megamarket Interspar Vič, Jamova cesta 105, 1000 Ljubljana, Slovenia",
+"Sportina let, družba za trgovino in zastopanje d.o.o., poslovna enota Outlet Kranj - Cesta Staneta Žagarja 71, 4000 Kranj, Slovenia",
+"XYZ - Mariborska cesta 100, 3000 Celje, Slovenia",
+"Sportina - Cesta 1. maja 77, 4000 Kranj, Slovenia",
+"Arkadia Shopping Center Domžale - Virska cesta 21, 1233 Dob, Slovenia",
+"Parfois - Pobreška cesta 18, 2000 Maribor, Slovenia",
+"Sportina Group - S/O Sportina - Ljubljanska cesta 47, 8000 Novo mesto, Slovenia",
+};
+}
