ac412402a2b3f39e4b1ebdca70e1328c14b907d8
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 07:38:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_34777538_REVIEW_1655897880974.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions34777538 { static final reviewedTitles34777538 = const {"BUZZ - Otoška cesta 5, 8000 Novo mesto, Slovenia",
+"Vinska klet podutik - Gradnikova ulica 8, 1000 Ljubljana, Slovenia",
+"Fast food Twins - Ulica Jožeta Jame 12, 1210 Ljubljana - Šentvid, Slovenia",
+"Bit Computer Engineering, Matjaž Dobovičnik SpA - Prežihova ulica 3, 2392 Ravne na Koroškem, Slovenia",
+"Agil d.o.o. - Brnčičeva ulica 11, 1231 Ljubljana, Slovenia",
+};
+}
