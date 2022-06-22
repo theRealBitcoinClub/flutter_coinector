95383de3257bbcfde802655be691186208b802e5
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 07:28:45 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_34777538_REVIEW_1655897323615.dart
@@ -0,0 +1,5 @@
+class AutoSuggestions34777538 { static final reviewedTitles34777538 = const {"BUZZ - Otoška cesta 5, 8000 Novo mesto, Slovenia",
+"Vinska klet podutik - Gradnikova ulica 8, 1000 Ljubljana, Slovenia",
+"Fast food Twins - Ulica Jožeta Jame 12, 1210 Ljubljana - Šentvid, Slovenia",
+};
+}
