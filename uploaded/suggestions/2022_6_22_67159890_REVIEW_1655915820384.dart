6ed8192fd83f7b6dfae79854e654c015209e5da6
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 12:37:02 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_67159890_REVIEW_1655915820384.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions67159890 { static final reviewedTitles67159890 = const {"Tilen Rep Hair Studio - Brnčičeva ulica 13, 1231 Ljubljana - Črnuče, Slovenia",
+"Center za lažje učenje in življenje, Mojca Stojkovič s.p., izobraževanje in svetovanje - Partizanska c. 27, 6210 Sežana, Slovenia",
+"Amanzi, trgovina na drobno, Urban Lagoja s.p. - Glavni trg 18, 1234 Mengeš, Slovenia",
+"Avto Tertinek, proizvodno, trgovsko, gostinsko in storitveno podjetje, d.o.o. - Borštnikova ulica 138, 2000 Maribor, Slovenia",
+"Bella Caffe - Industrijska ulica 44, 2230 Lenart v Slov. goricah, Slovenia",
+};
+}
