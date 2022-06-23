3481b715f6a81d6e9e78614f24ac130d65795ab0
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 21:09:21 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_75008984_REVIEW_1655946558852.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions75008984 { static final reviewedTitles75008984 = const {"Temza svetovanje s področja osebnosti in medsebojnih odnosov d.o.o. - Dunajska c. 158, 1000 Ljubljana, Slovenia",
+"KluBar - Slovenski trg 7, 4000 Kranj, Slovenia",
+"KAR=IN I Prodajna galerija I ljubljana - Tavčarjeva ulica 3, 1000 Ljubljana, Slovenia",
+"Pohištvo il Ambienti d.o.o. - Obrtna ulica 11, 9000 Murska Sobota, Slovenia",
+"Trgovina Galerija IL Ambienti - Ameriška ulica 8, 1000 Ljubljana, Slovenia",
+};
+}
