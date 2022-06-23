232869f375fbeae848f0b39279e316074a958258
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 21:48:04 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_73575509_REVIEW_1655948881718.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions73575509 { static final reviewedTitles73575509 = const {"AVTOKLINIKA MARIBOR - Spodnje Dobrenje 42a, 2211 Pesnica pri Mariboru, Slovenia",
+"Grafični Atelje- Grafične, ključavničarske in poslovne storitve Branko Brajdih s.p. - Partizanska c. 82, 6210 Sežana, Slovenia",
+"Naturopatski center Saeka izobraževanje, prodaja in zastopništvo d.o.o. - Vojkova ulica 28, 5271 Vipava, Slovenia",
+"Mobimania.si - Gosposka ulica 21, 2000 Maribor, Slovenia",
+"AVTO ŠAJN, čiščenje in poliranje vozil. Darko Solina s.p. - Krčevina pri Vurbergu 141, 2250 Ptuj, Slovenia",
+};
+}
