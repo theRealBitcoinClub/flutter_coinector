312e13e54f7254be051cf053f1d090b534fcd43f
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 21:43:51 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_73575509_REVIEW_1655948628806.dart
@@ -0,0 +1,4 @@
+class AutoSuggestions73575509 { static final reviewedTitles73575509 = const {"AVTOKLINIKA MARIBOR - Spodnje Dobrenje 42a, 2211 Pesnica pri Mariboru, Slovenia",
+"Grafični Atelje- Grafične, ključavničarske in poslovne storitve Branko Brajdih s.p. - Partizanska c. 82, 6210 Sežana, Slovenia",
+};
+}
