31c44c65c1a74aba863bd663a923a379f72b9e6a
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 22:29:13 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_27657473_REVIEW_1655951350907.dart
@@ -0,0 +1,7 @@
+class AutoSuggestions27657473 { static final reviewedTitles27657473 = const {"Extraordinary. Ljubljana - Novi trg 6, 1000 Ljubljana, Slovenia",
+"Trikot Pizza - 9000 Nemčavci, Slovenia",
+"Oskar Bar and Cafe - Litijska cesta 148, 1000 Ljubljana, Slovenia",
+"Lekarna PRI KRANJSKEM ORLU - Glavni trg 21, 4000 Kranj, Slovenia",
+"E5 storitve, trgovina in svetovanje, D. O. O. - Maistrova ulica 20 A, 2250 Ptuj, Slovenia",
+};
+}
