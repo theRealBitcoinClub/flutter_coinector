5300660ac915a0b992daa985fff7dba9ede25944
therealbitcoinclub <trbc@bitcoinmap.cash>
2022-06-22 22:32:31 -0400

    Add Suggestions

---
+++ uploaded/suggestions/2022_6_22_27657473_REVIEW_1655951549472.dart
@@ -0,0 +1,10 @@
+class AutoSuggestions27657473 { static final reviewedTitles27657473 = const {"Extraordinary. Ljubljana - Novi trg 6, 1000 Ljubljana, Slovenia",
+"Trikot Pizza - 9000 Nemčavci, Slovenia",
+"Oskar Bar and Cafe - Litijska cesta 148, 1000 Ljubljana, Slovenia",
+"Lekarna PRI KRANJSKEM ORLU - Glavni trg 21, 4000 Kranj, Slovenia",
+"E5 storitve, trgovina in svetovanje, D. O. O. - Maistrova ulica 20 A, 2250 Ptuj, Slovenia",
+"ANVINA, d.o.o., Vrhnika - Tržaška cesta 8, 1360 Vrhnika, Slovenia",
+"Anvina d.o.o., PE Borovnica - Paplerjeva ulica 8, 1353 Borovnica, Slovenia",
+"Anvina d.o.o., proizvodnja, storitve, trženje - Cankarjev trg 6, 1360 Vrhnika, Slovenia",
+};
+}
